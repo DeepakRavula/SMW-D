9877b0c370419639c62c89cc583e43c3e327e957
SenG <senguttuvang@gmail.com>
2016-04-29 16:31:53 +0530

    Fixing access for all menu

--- common/config/web.php
+++ common/config/web.php
@@ -18,6 +18,11 @@ $config = [
             'customer/*',
             'site/*',
             'admin/*',
+			'user/*',
+			'program/*',
+			'lesson/*',
+			'invoice/*',
+			'location/*',
             'system-information/*',
             // The actions listed here will be allowed to everyone including guests.
             // So, 'admin/*' should not appear here in the production, of course.
