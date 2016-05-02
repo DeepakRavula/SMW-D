14dab5005c7ca4f4146d5c32c1566af18fedb73a
SenG <senguttuvang@gmail.com>
2016-05-02 21:21:51 +0530

    log out issue and dashboard uplon login fixed by whitelisting the respective controllers.

--- common/config/web.php
+++ common/config/web.php
@@ -26,6 +26,8 @@ $config = [
 			'location/*',
             'system-information/*',
             'gii/*',
+            'sign-in/*',
+            'timeline-event/*',
             // The actions listed here will be allowed to everyone including guests.
             // So, 'admin/*' should not appear here in the production, of course.
             // But in the earlier stages of your development, you may probably want to
