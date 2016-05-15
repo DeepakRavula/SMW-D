d21ff52d939af2bc560e58dfe3cb63aae95bf12f
SenG <senguttuvang@gmail.com>
2016-05-15 17:03:43 +0530

    integrated schedule in calender

--- backend/controllers/ScheduleController.php
+++ backend/controllers/ScheduleController.php
@@ -36,14 +36,39 @@ class ScheduleController extends Controller
     {
         /* $teacherAvailability = ArrayHelper::map(TeacherAvailability::find()->all(), 'id', 'teacher_id as name');*/
         $teacherAvailability = (new \yii\db\Query())
-            ->select(['ta.teacher_id', 'concat(up.firstname,\' \',up.lastname) as name'])
+            ->select(['ta.teacher_id as id', 'concat(up.firstname,\' \',up.lastname) as name'])
             ->from('teacher_availability_day ta')
             ->join('Join', 'user_profile up', 'up.user_id = ta.teacher_id')
             ->where('ta.location_id = :location_id AND ta.day = DATE_FORMAT(now(),\'%w\')', [':location_id'=>1])
             ->all();
         
+        $events = array();
+        $student = array();
         
-		return $this->render('index', ['teacherAvailability'=>$teacherAvailability]);
+        foreach($teacherAvailability as $teacher){
+            
+            $studentAvailability = (new \yii\db\Query())
+                ->select(['q.teacher_id as id', 'concat(s.first_name,\' \',s.last_name) as name, ed.from_time, ed.to_time'])
+                ->from('enrolment e')
+                ->join('Join', 'qualification q', 'q.id = e.qualification_id')
+                ->join('Join', 'enrolment_schedule_day ed', 'ed.enrolment_id = e.id')
+                ->join('Join', 'student s', 's.id = e.student_id')
+                ->where('q.teacher_id = :teacher_id AND ed.day = DATE_FORMAT(now(),\'%w\')', [':teacher_id'=>$teacher['id']])
+                ->all();
+                
+            foreach($studentAvailability as $studentAvailability){
+                $student['title'] = $studentAvailability['name'];
+                $student['start'] = date("D M d Y").' '.$studentAvailability["from_time"];
+                $student['end'] = date("D M d Y").' '.$studentAvailability["to_time"];
+                $student['allDay'] = false;
+                $student['resources'] = $studentAvailability['id']; 
+                
+                array_push($events, $student);
+            } 
+            
+        }
+        
+		return $this->render('index', ['teacherAvailability'=>$teacherAvailability, 'events'=>$events]);
     }
 
 }
--- backend/views/schedule/index.php
+++ backend/views/schedule/index.php
@@ -18,7 +18,7 @@ $(document).ready(function() {
   var d = date.getDate();
   var m = date.getMonth();
   var y = date.getFullYear();
-
+  
   $('#calendar').fullCalendar({
     header: {
       left: 'prev,next today',
@@ -29,80 +29,7 @@ $(document).ready(function() {
     editable: true,
     droppable: true,
     resources:  <?php echo Json::encode($teacherAvailability); ?>,
-    events: [{
-      title: '[AM] Karen Johnson (Guitar)',
-      start: new Date(y, m, d, 10, 15),
-      end: new Date(y, m, d, 11, 45),
-      allDay: false,
-      resources: ['1', '2']
-    }, {
-      title: '[AM] Jake Attal (Piano)',
-      start: new Date(y, m, d, 11, 0),
-      end: new Date(y, m, d, 12, 30),
-      allDay: false,
-      resources: '150'
-    }, {
-      title: '[PM] Karen Johnson (Violin)',
-      start: new Date(y, m, d, 13, 0),
-	   end: new Date(y, m, d, 14, 30),
-      allDay: false,
-      resources: '152'
-    }, {
-      title: '[PM] Jake Attal (Guitar)',
-      start: new Date(y, m, d, 15, 0),
-      end: new Date(y, m, d, 16, 30),
-      allDay: false,
-      resources: ['153', '154']
-    }, {
-      id: 777,
-      title: '[PM] Tiffany Kong (Piano)',
-      start: new Date(y, m, d, 17, 0),
-      end: new Date(y, m, d, 18, 30),
-      allDay: false,
-      resources: ['155']
-    }, {
-      id: 999,
-      title: '[PM] Kimberly Aquino (Guitar)',
-      start: new Date(y, m, d, 19, 0),
-      end: new Date(y, m, d, 20, 30),
-      allDay: false,
-      resources: '155'
-    }, {
-      
-       title: 'Nicholas Hanns (Guitar)',
-      start: new Date(y, m, d, 21, 0),
-      end: new Date(y, m, d, 22, 30),
-      allDay: false,
-      resources: '153'
-    },{
-      
-      title: 'Rita Fabian (Piano)',
-      start: new Date(y, m, d, 23, 0),
-      end: new Date(y, m, d, 00, 0),
-      allDay: false,
-      resources: '155'
-    }, {
-      
-       title: '[AM] Vanessa Pryia (Guitar)',
-      start: new Date(y, m, d, 3, 0),
-      end: new Date(y, m, d, 4, 30),
-      allDay: false,
-      resources: '155'
-    },{
-      
-       title: '[AM] Paul Faggil (Piano)',
-      start: new Date(y, m, d, 4, 0),
-      end: new Date(y, m, d, 5, 30),
-      allDay: false,
-      resources: '151'
-    },{
-     
-       title: '[AM] Giselle Andrews (Guitar)',
-      start: new Date(y, m, d, 6, 0),
-      end: new Date(y, m, d, 7, 30),
-      allDay: false,
-      resources: '152'
-    }],
+    events: <?php echo Json::encode($events); ?>,
     // the 'ev' parameter is the mouse event rather than the resource 'event'
     // the ev.data is the resource column clicked upon
     selectable: true,
