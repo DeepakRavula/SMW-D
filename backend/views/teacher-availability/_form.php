26a7b997c9498d1faced21accccf1c48b8ee3dbd
SenG <senguttuvang@gmail.com>
2016-05-17 14:31:47 +0530

    CRUD was generated for teacher-availability

---
+++ backend/views/teacher-availability/_form.php
@@ -0,0 +1,36 @@
+<?php
+
+use common\models\User;
+use common\models\Location;
+use common\models\TeacherAvailability;
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\TeacherAvailability */
+/* @var $form yii\bootstrap\ActiveForm */
+?>
+
+<div class="teacher-availability-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?php echo $form->errorSummary($model); ?>
+
+    <?php echo $form->field($model, 'teacher_id')->dropDownList(ArrayHelper::map(User::findByRole(User::ROLE_TEACHER),'id','userProfile.fullName')) ?>
+
+    <?php echo $form->field($model, 'location_id')->dropDownList(ArrayHelper::map(Location::find()->all(), 'id', 'name')); ?>
+
+    <?php echo $form->field($model, 'day')->dropdownList(TeacherAvailability::getWeekdaysList());?>
+
+    <?php echo $form->field($model, 'from_time');?>
+
+    <?php echo $form->field($model, 'to_time');?>
+
+    <div class="form-group">
+        <?php echo Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/teacher-availability/_search.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\TeacherAvailabilitySearch */
+/* @var $form yii\bootstrap\ActiveForm */
+?>
+
+<div class="teacher-availability-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?php echo $form->field($model, 'id') ?>
+
+    <?php echo $form->field($model, 'teacher_id') ?>
+
+    <?php echo $form->field($model, 'location_id') ?>
+
+    <?php echo $form->field($model, 'day') ?>
+
+    <?php echo $form->field($model, 'from_time') ?>
+
+    <?php // echo $form->field($model, 'to_time') ?>
+
+    <div class="form-group">
+        <?php echo Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?php echo Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
