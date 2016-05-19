8a3c3436981b49ceaee84c5ff35d62fca997d0e0
SenG <senguttuvang@gmail.com>
2016-05-19 20:15:57 +0530

    Addind customer name dropdown based on location in student create page.

--- backend/views/student/_form.php
+++ backend/views/student/_form.php
@@ -11,7 +11,10 @@ use yii\bootstrap\ActiveForm;
 ?>
 
 <div class="student-form">
-
+	<?php
+	$session = Yii::$app->session;
+	$locationId = $session->get('location_id');
+	?>
     <?php $form = ActiveForm::begin(); ?>
 
     <?php echo $form->errorSummary($model); ?>
@@ -22,7 +25,13 @@ use yii\bootstrap\ActiveForm;
 
     <?php echo $form->field($model, 'birth_date')->widget(\yii\jui\DatePicker::classname())  ?>
 
-    <?php echo $form->field($model, 'customer_id')->dropDownList(ArrayHelper::map(User::findByRole(User::ROLE_CUSTOMER),'id','userProfile.fullName')) ?>
+    <?php echo $form->field($model, 'customer_id')->dropDownList(ArrayHelper::map(
+			User::find()
+				->join('INNER JOIN', 'rbac_auth_assignment', 'user_id = id')
+				->andWhere(['rbac_auth_assignment.item_name' => 'customer'])
+				->andWhere(['location_id' => $locationId])
+				->all(),
+			'id','userProfile.fullName')) ?>
 
     <div class="form-group">
         <?php echo Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
