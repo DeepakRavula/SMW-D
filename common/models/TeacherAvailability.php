44e74cbf636358236d04ad763b93306c3f8ee193
SenG <senguttuvang@gmail.com>
2016-05-20 15:16:53 +0530

    depdrop of teacher name based on location.

--- common/models/TeacherAvailability.php
+++ common/models/TeacherAvailability.php
@@ -68,4 +68,8 @@ class TeacherAvailability extends \yii\db\ActiveRecord
     {
         return $this->hasOne(Location::className(), ['id' => 'location_id']);
     }
+
+	public function IsAvailableAtLocation($location_id){
+		return $this->find()->where(['teacher_id' => $this->teacher_id,'location_id' => $location_id])->exists();
+	}
 }
