e3bb5da53c489097438f3cf642e02bf4b8bb05f4
SenG <senguttuvang@gmail.com>
2016-05-25 22:17:38 +0530

    schedule issue fixed

--- backend/controllers/ScheduleController.php
+++ backend/controllers/ScheduleController.php
@@ -38,10 +38,11 @@ class ScheduleController extends Controller
         
         /* $teacherAvailability = ArrayHelper::map(TeacherAvailability::find()->all(), 'id', 'teacher_id as name');*/
         $teacherAvailability = (new \yii\db\Query())
-            ->select(['distinct(ta.teacher_id) as id', 'concat(up.firstname,\' \',up.lastname) as name'])
+            ->select(['distinct(ul.user_id) as id', 'concat(up.firstname,\' \',up.lastname) as name'])
             ->from('teacher_availability_day ta')
-            ->join('Join', 'user_profile up', 'up.user_id = ta.teacher_id')
-            ->where('ta.location_id = :location_id', [':location_id'=>Yii::$app->session->get('location_id')])
+            ->join('Join', 'user_location ul', 'ul.id = ta.teacher_location_id')
+            ->join('Join', 'user_profile up', 'up.user_id = ul.user_id')
+            ->where('ul.location_id = :location_id', [':location_id'=>Yii::$app->session->get('location_id')])
             ->orderBy('id desc')
             ->all();
         
