3668ed9faae934f683a2aab8c1e846df3226b07d
SenG <senguttuvang@gmail.com>
2016-06-07 18:17:15 +0530

    alert message added in country

--- backend/controllers/CountryController.php
+++ backend/controllers/CountryController.php
@@ -63,6 +63,10 @@ class CountryController extends Controller
         $model = new Country();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
+			Yii::$app->session->setFlash('alert', [
+            	'options' => ['class' => 'alert-success'],
+            	'body' => 'Country has been created successfully'
+        ]); 
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('create', [
@@ -82,6 +86,10 @@ class CountryController extends Controller
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
+			Yii::$app->session->setFlash('alert', [
+            	'options' => ['class' => 'alert-success'],
+            	'body' => 'Country has been updated successfully'
+        ]); 
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('update', [
@@ -99,7 +107,10 @@ class CountryController extends Controller
     public function actionDelete($id)
     {
         $this->findModel($id)->delete();
-
+        Yii::$app->session->setFlash('alert', [
+          	'options' => ['class' => 'alert-success'],
+           	'body' => 'Country has been deleted successfully'
+        ]); 
         return $this->redirect(['index']);
     }
 
