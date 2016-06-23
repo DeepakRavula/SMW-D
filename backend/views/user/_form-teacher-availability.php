d21f55ee959cf71aef240e5508d9ccea253cabb2
SenG <senguttuvang@gmail.com>
2016-06-23 12:41:32 +0530

    adds prompt message in teacher availability form.

--- backend/views/user/_form-teacher-availability.php
+++ backend/views/user/_form-teacher-availability.php
@@ -7,16 +7,17 @@ use kartik\time\TimePicker;
 /* @var $model common\models\TeacherAvailability */
 /* @var $form yii\bootstrap\ActiveForm */
 ?>
+<div class="row-fluid">
 <div class="col-md-12">
 	<h4 class="pull-left m-r-20">Teachers Availability</h4>
 	<a href="#" class="availability text-add-new"><i class="fa fa-plus-circle"></i> Add availability</a>
 	<div class="clearfix"></div>
 </div>
 <div class="teacher-availability-create row-fluid">
-	<div class="teacher-availability-form form-well form-well-smw">
+	<!--<div class="teacher-availability-form form-well form-well-smw">-->
 		<div class="row">
 			<div class="col-md-2">
-				<?php echo $form->field($model, 'teacherAvailabilityDay')->dropdownList(TeacherAvailability::getWeekdaysList()); ?>
+				<?php echo $form->field($model, 'teacherAvailabilityDay')->dropdownList(TeacherAvailability::getWeekdaysList(),['prompt' => 'select day']); ?>
 			</div>
 			<div class="col-md-2">
 				<?php echo $form->field($model, 'fromTime')->widget(TimePicker::classname(), []); ?>
@@ -26,6 +27,5 @@ use kartik\time\TimePicker;
 			</div>
 
 		</div>
-	</div>
 </div>
-<div class="clearfix"></div>
+</div>
