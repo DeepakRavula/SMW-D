cab031addf0607c52cfc10982588daa1c70250a2
SenG <senguttuvang@gmail.com>
2016-07-02 14:28:22 +0530

    Reimplements user search

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -26,9 +26,9 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="user-index">
     <?php yii\widgets\Pjax::begin(['id' => 'lesson-index']); ?>
-        <?php echo $this->render('_search', ['model' => $searchModel]); ?>
         <?php echo GridView::widget([
             'dataProvider' => $dataProvider,
+        	'filterModel' => $searchModel,
             'rowOptions' => function ($model, $key, $index, $grid) use ($roleName){
                 $u= \yii\helpers\StringHelper::basename(get_class($model));
                 $u= yii\helpers\Url::toRoute(['/'.strtolower($u).'/view']);
@@ -41,39 +41,41 @@ $this->params['breadcrumbs'][] = $this->title;
 				'class' => 'yii\grid\SerialColumn',
 				'header' => 'Serial No.',
 			],
-                'userProfile.firstname',
-                'userProfile.lastname',
-                'email',
-                [
-                    'label' => 'Role',
-                    'value' => function($data) {
-                        $roles = \Yii::$app->authManager->getRolesByUser($data->id);
-                            foreach($roles as $roles){
-                                return $roles->description;
-                            }
-                    },
-                ],
-                [
-                    'label' => 'Phone',
-                    'value' => function($data) {
-                        return ! empty($data->phoneNumber->number) ? $data->phoneNumber->number : null;
-                    },
-                ],
-                //['class' => 'yii\grid\ActionColumn'],
-            ],
-        ]); ?>
-    <?php yii\widgets\Pjax::end(); ?>
-    <div class="p-l-20 m-b-20">
+			[
+				'attribute' => 'firstname',
+				'label' => 'First Name',
+				'value' => function($data){
+					return ! empty($data->userProfile->firstname) ? $data->userProfile->firstname : null;
+				}
+			],
+			[
+				'attribute' => 'lastname',
+				'label' => 'Last Name',
+				'value' => function($data){
+					return ! empty($data->userProfile->lastname) ? $data->userProfile->lastname : null;
+				}
+			],
+			'email',
+			[
+				'label' => 'Phone',
+				'value' => function($data) {
+					return ! empty($data->phoneNumber->number) ? $data->phoneNumber->number : null;
+				},
+			],
+		],
+	]); ?>
+<?php yii\widgets\Pjax::end(); ?>
+<div class="p-l-20 m-b-20">
 <?php echo Html::a(Yii::t('backend', 'Add '), ['create', 'User[role_name]' => $searchModel->role_name], ['class' => 'btn btn-success pull-left m-r-20']) ?>
 
-	<?php if($searchModel->role_name === User::ROLE_CUSTOMER):?>
-        <?php echo Html::a(Yii::t('backend', 'Delete All Customers', [
-            'modelClass' => 'User',
-            ]), 
-            ['delete-all'], 
-            ['class' => 'btn btn-danger pull-left']) 
-        ?>
-    <?php endif;?>
-    <div class="clearfix"></div>
-    </div>
+<?php if($searchModel->role_name === User::ROLE_CUSTOMER):?>
+	<?php echo Html::a(Yii::t('backend', 'Delete All Customers', [
+		'modelClass' => 'User',
+		]), 
+		['delete-all'], 
+		['class' => 'btn btn-danger pull-left']) 
+	?>
+<?php endif;?>
+<div class="clearfix"></div>
+</div>
 </div>
