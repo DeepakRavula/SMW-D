22ffe0cf6de4f9183045ca050dfb89bb5e63ed5a
SenG <senguttuvang@gmail.com>
2016-07-06 15:05:46 +0530

    Add Error notification at the top of the page in create / update forms

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -28,7 +28,8 @@ use yii\bootstrap\Tabs;
 
 </style>
 
-<div class="user-form"> 
+<div class="user-form">
+    <div id="notification" style="display: none;" class="alert-danger alert fade in"></div>
 
 	<?php $form = ActiveForm::begin(['id' => 'dynamic-form']); ?>
 	<?php
@@ -142,4 +143,12 @@ use yii\bootstrap\Tabs;
 		e.preventDefault();
 		$(this).tab('show');
 	})
+    $("#dynamic-form").on("afterValidate", function (event, messages) {
+       $('#notification').html("Form has some errors. Please fix and try again.").fadeIn();
+        console.log('aftervalidate');
+      // Now you can work with messages by accessing messages variable
+      var attributes = $(this).data().attributes; // to get the list of attributes that has been passed in attributes property
+      var settings = $(this).data().settings; // to get the settings
+    });
+
 </script>
