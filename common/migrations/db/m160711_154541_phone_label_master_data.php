434231ea9d8922606cae4de568d646e43cdd0f99
SenG <senguttuvang@gmail.com>
2016-07-11 22:51:38 +0530

    Skeleton code for Migration master datas like tax,location,phonelabel and 'rbac' related tabel

---
+++ common/migrations/db/m160711_154541_phone_label_master_data.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+class m160711_154541_phone_label_master_data extends Migration
+{
+    public function up()
+    {
+		$sql = file_get_contents(dirname(__FILE__).'/' . get_class($this) . '_' . __FUNCTION__ . '.sql');
+        
+		return $this->execute($sql);
+
+    }
+
+    public function down()
+    {
+        echo "m160711_154541_phone_label_master_data cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
