701e1288c8d7ca255b08fd840bbe12bd2ad089a7
SenG <senguttuvang@gmail.com>
2016-07-13 13:13:27 +0530

    removes delete button from student profile

--- backend/views/student/_profile.php
+++ backend/views/student/_profile.php
@@ -24,14 +24,6 @@ use yii\helpers\Html;
 	<div class="student-view">
 		<div class="col-md-12 action-btns">
 			<?php echo Html::a('<i class="fa fa-pencil"></i> Edit', ['update', 'id' => $model->id], ['class' => 'm-r-20']) ?>
-			<?php
-			echo Html::a('<i class="fa fa-remove"></i> Delete', ['delete', 'id' => $model->id], [
-				'data' => [
-					'confirm' => 'Are you sure you want to delete this item?',
-					'method' => 'post',
-				],
-			])
-			?>
 	    </div>
 	    <div class="clearfix"></div>
 	</div>
