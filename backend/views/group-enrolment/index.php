9a92dce235fca7fc9b3e17c30bc59449d7e25f3a
SenG <senguttuvang@gmail.com>
2016-07-15 11:16:14 +0530

    generates CRUD for grroup enrolment entity

---
+++ backend/views/group-enrolment/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\search\GroupEnrolmentSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Group Enrolments';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="group-enrolment-index">
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?php echo Html::a('Create Group Enrolment', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'course_id',
+            'student_id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
---
+++ common/models/GroupEnrolment.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "group_enrolment".
+ *
+ * @property string $id
+ * @property integer $course_id
+ * @property integer $student_id
+ */
+class GroupEnrolment extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'group_enrolment';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['course_id', 'student_id'], 'required'],
+            [['course_id', 'student_id'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'course_id' => 'Course ID',
+            'student_id' => 'Student ID',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return \common\models\query\GroupEnrolmentQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\GroupEnrolmentQuery(get_called_class());
+    }
+}
