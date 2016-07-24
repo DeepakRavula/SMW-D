e3ccfa168a154aae44abcc2903d9dff89cacefda
SenG <senguttuvang@gmail.com>
2016-07-24 12:45:57 +0530

    Revert "access calendar for non-logged users"
    This reverts commit 08bfc31d129ad634cc1aed33e5e094efec835619.

--- common/config/web.php
+++ common/config/web.php
@@ -45,7 +45,6 @@ $config = [
 			'group-enrolment/*',
 			'holiday/*',
 			'professional-development-day/*',
-			'calendar/*'
             // The actions listed here will be allowed to everyone including guests.
             // So, 'admin/*' should not appear here in the production, of course.
             // But in the earlier stages of your development, you may probably want to
