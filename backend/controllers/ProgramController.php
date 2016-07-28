06e90e51a34cacca7bf914b6032361ab004b17de
SenG <senguttuvang@gmail.com>
2016-07-28 16:05:39 +0530

    saves program type

--- backend/controllers/ProgramController.php
+++ backend/controllers/ProgramController.php
@@ -86,6 +86,9 @@ class ProgramController extends Controller
     {
         $model = new Program();
 		$model->status = Program::STATUS_ACTIVE;
+		$request = Yii::$app->request;
+		$programRequest = $request->post('Program');
+		$model->type = $programRequest['type'];
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
 			Yii::$app->session->setFlash('alert', [
         	    'options' => ['class' => 'alert-success'],
