98a15b4c421efcc266b537db7bc13ca767ac9913
SenG <senguttuvang@gmail.com>
2016-08-01 22:39:54 +0530

    displays credit payment tansaction in customer's accounts tab

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -187,11 +187,9 @@ class UserController extends Controller {
 		]);
 		$paymentDataProvider = new ActiveDataProvider([
 			'query' => Allocation::find()
-				->joinWith(['payment p' => function($query) use($id){
-					$query->where(['p.user_id' => $id]);
-				}])
-				->andWhere(['not',['allocation.payment_id' => Payment::PAYMENT_CREDIT]])
-				->orWhere(['allocation.invoice_id' => Payment::PAYMENT_CREDIT])
+				->joinWith('payment p')
+				->where(['p.user_id' => $id])
+				->orWhere(['not',['and',['allocation.payment_id' => Payment::PAYMENT_CREDIT],['<>','allocation.invoice_id',Payment::PAYMENT_CREDIT]]])
 		]);
  		$paymentModel = new Payment();
 		if ($paymentModel->load(Yii::$app->request->post())) {
