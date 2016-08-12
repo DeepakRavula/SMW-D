f08d00c3ab074f22514820b37318a5906f1b8183
SenG <senguttuvang@gmail.com>
2016-08-12 21:34:34 +0530

    implements customer's account transaction

--- backend/views/user/_account.php
+++ backend/views/user/_account.php
@@ -10,63 +10,67 @@ use yii\bootstrap\Modal;
 ?>
 <?php
 $invoiceCredits = Invoice::find()
-		->invoiceCredit($model->id)
-		->all();
+	->invoiceCredit($model->id)
+	->all();
 
 $results = [];
 foreach($invoiceCredits as $invoiceCredit){
-	$lastInvoicePayment = $invoiceCredit->invoicePayments;
-	$lastInvoicePayment = end($lastInvoicePayment);
-	$paymentDate = \DateTime::createFromFormat('Y-m-d H:i:s',$lastInvoicePayment->payment->date);
-	$results[] = [
-		'id' => $invoiceCredit->id,
-		'date' => $paymentDate->format('d-m-Y'),
-		'amount' => abs($invoiceCredit->balance),
-		'source' => 'Invoice',
-		'type' => 'invoice'
-	];
+$lastInvoicePayment = $invoiceCredit->invoicePayments;
+$lastInvoicePayment = end($lastInvoicePayment);
+$paymentDate = \DateTime::createFromFormat('Y-m-d H:i:s',$lastInvoicePayment->payment->date);
+$results[] = [
+	'id' => $invoiceCredit->id,
+	'date' => $paymentDate->format('d-m-Y'),
+	'total' => $invoiceCredit->total,
+	'paid' => $invoiceCredit->invoicePaymentTotal,
+	'owing' => -abs($invoiceCredit->invoiceBalance),
+	'source' => 'Invoice Credit',
+];
 }
 
 $openingBalancePaymentModel = Payment::find()
-				->where([
-					'user_id' => $model->id,
-					'payment_method_id' => [PaymentMethod::TYPE_ACCOUNT_ENTRY, ],
-			])->one();
-	
-		$remainingOpeningBalance = 0;
-		if(! empty($openingBalancePaymentModel->id)){
-			$openingBalanceCreditsUsed = Payment::find()
-					->joinWith(['invoicePayment ip' => function($query) use($model){
-						$query->where(['ip.invoice_id' => Payment::TYPE_OPENING_BALANCE_CREDIT]);	
-					}])
-					->where(['user_id' => $model->id])
-					->sum('amount');
+	->where([
+		'user_id' => $model->id,
+		'payment_method_id' => [PaymentMethod::TYPE_ACCOUNT_ENTRY, ],
+	])->one();
 
-			$remainingOpeningBalance = $openingBalancePaymentModel->amount + $openingBalanceCreditsUsed;
-		}
-		
+$remainingOpeningBalance = 0;
+if(! empty($openingBalancePaymentModel->id)){
+	$openingBalanceCreditsUsed = Payment::find()
+			->joinWith(['invoicePayment ip' => function($query) use($model){
+				$query->where(['ip.invoice_id' => Payment::TYPE_OPENING_BALANCE_CREDIT]);	
+			}])
+			->where(['user_id' => $model->id])
+			->sum('amount');
+
+	$remainingOpeningBalance = $openingBalancePaymentModel->amount + $openingBalanceCreditsUsed;
+}
+
 if($remainingOpeningBalance > 0){
 	$paymentDate = \DateTime::createFromFormat('Y-m-d H:i:s',$openingBalancePaymentModel->date);
 	$results[] = [
-			'id' => $openingBalancePaymentModel->id,
-			'date' => $paymentDate->format('d-m-Y'),
-			'amount' => abs($remainingOpeningBalance),
-			'source' => 'Opening Balance',
-			'type' => 'account_entry'
-		];
+		'id' => $openingBalancePaymentModel->id,
+		'date' => $paymentDate->format('d-m-Y'),
+		'total' => 0,
+		'paid' => 0, 
+		'owing' => -abs($remainingOpeningBalance),
+		'source' => 'Opening Balance',
+	];
 }
 
 $proFormaInvoiceCredits = Invoice::find()->alias('i')
-		->select(['i.id', 'i.date', 'SUM(p.amount) as credit'])
-		->joinWith(['invoicePayments ip' => function($query){
-			$query->joinWith(['payment p' => function($query){
-			}]);
-		}])
-		->where(['i.type' => Invoice::TYPE_PRO_FORMA_INVOICE, 'i.user_id' => $model->id])
-		->groupBy('i.id')
-		->all();
-		
+	->select(['i.id', 'i.date', 'SUM(p.amount) as credit', 'total'])
+	->joinWith(['invoicePayments ip' => function($query){
+		$query->joinWith(['payment p' => function($query){
+		}]);
+	}])
+	->where(['i.type' => Invoice::TYPE_PRO_FORMA_INVOICE, 'i.user_id' => $model->id])
+	->groupBy('i.id')
+	->all();
+
 foreach($proFormaInvoiceCredits as $proFormaInvoiceCredit){
+//$lastInvoicePayment = $invoiceCredit->invoicePayments;
+//$lastInvoicePayment = end($lastInvoicePayment);
 	if($proFormaInvoiceCredit->credit <= 0){
 		continue;
 	}
@@ -74,63 +78,95 @@ foreach($proFormaInvoiceCredits as $proFormaInvoiceCredit){
 	$results[] = [
 		'id' => $proFormaInvoiceCredit->id,
 		'date' => $paymentDate->format('d-m-Y'),
-		'amount' => $proFormaInvoiceCredit->credit,
+		'total' => $proFormaInvoiceCredit->total,
+		'paid' => $proFormaInvoiceCredit->total,
+		'owing' => -abs($proFormaInvoiceCredit->credit),
 		'source' => 'Pro-forma Invoice',
-		'type' => 'pro_forma_invoice'
 	];
 }
 
 $newInvoices = Invoice::find()
-		->joinWith(['invoicePayments ip' => function($query){
-			$query->where(['ip.id' => null]);
-		}])
-		->where(['user_id' => $model->id])
-		->all();
+	->joinWith(['invoicePayments ip' => function($query){
+		$query->where(['ip.id' => null]);
+	}])
+	->where(['user_id' => $model->id, 'type' => Invoice::TYPE_INVOICE])
+	->all();
 
-	foreach($newInvoices as $newInvoice){
+foreach($newInvoices as $newInvoice){
 	$invoiceDate = \DateTime::createFromFormat('Y-m-d H:i:s',$newInvoice->date);
 		$results[] = [
 			'id' => $newInvoice->id,
 			'date' => $invoiceDate->format('d-m-Y'),
-			'amount' => $newInvoice->total,
+			'total' => $newInvoice->total,
+			'paid' => $newInvoice->invoicePaymentTotal,
+			'owing' => $newInvoice->invoiceBalance,
 			'source' => 'Invoice',
-			'type' => 'invoice'
 		];	
+}
+
+$invoices = Invoice::find()->alias('i')
+->joinWith(['invoicePayments ip' => function($query){
+	$query->joinWith(['payment p' => function($query){
+	}]);
+}])
+->where(['i.type' => Invoice::TYPE_INVOICE, 'i.user_id' => $model->id])
+->all();
+if(! empty($invoices)){
+	foreach($invoices as $invoice){
+		if($invoice->total > $invoice->invoicePaymentTotal){
+			$invoiceDate = \DateTime::createFromFormat('Y-m-d H:i:s',$invoice->date);
+	$results[] = [
+		'id' => $invoice->id,
+		'date' => $invoiceDate->format('d-m-Y'),
+		'total' => $invoice->total,
+		'paid' => $invoice->invoicePaymentTotal,
+		'owing' => $invoice->invoiceBalance,
+		'source' => 'Invoice',
+	];
+		}
 	}
+}
+
 $creditDataProvider = new ArrayDataProvider([
-    'allModels' => $results,
-    'sort' => [
-        'attributes' => ['id', 'date', 'amount', 'source'],
-    ],
+'allModels' => $results,
+'sort' => [
+	'attributes' => ['id', 'date', 'total', 'source','paid','owing'],
+],
 ]);
 ?>
 <?php
 echo GridView::widget([
-	'dataProvider' => $creditDataProvider,
-    'rowOptions'   => function ($model, $key, $index, $grid) {
-        return [
-			'data-amount' => $model['amount'], 
-			'data-id' => $model['id'],
-			'data-source' => $model['type']
-		];
-    },
-	'columns' => [
-		[
-		'label' => 'Id', 
-		'value' => 'id',
-		],
-		[
-		'label' => 'Source', 
-		'value' => 'source',
-		],
-		[
-		'label' => 'Date', 
-		'value' => 'date',
-		],
-		[
-		'label' => 'Credit',
-		'value' => 'amount',
-		]
-    ]
+'dataProvider' => $creditDataProvider,
+'rowOptions'   => function ($model, $key, $index, $grid) {
+	return [
+		'data-id' => $model['id'],
+	];
+},
+'columns' => [
+	[
+	'label' => 'Id', 
+	'value' => 'id',
+	],
+	[
+	'label' => 'Source', 
+	'value' => 'source',
+	],
+	[
+	'label' => 'Date', 
+	'value' => 'date',
+	],
+	[
+	'label' => 'Total',
+	'value' => 'total',
+	],
+	[
+	'label' => 'Paid',
+	'value' => 'paid',
+	],
+	[
+	'label' => 'Owing',
+	'value' => 'owing',
+	]
+]
 ]);
 ?>
