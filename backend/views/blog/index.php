0618c65dd3f2db25f25c1bb1d6a79cbc1f628c34
SenG <senguttuvang@gmail.com>
2016-08-30 11:44:51 +0530

    generates crud code for blog entity

---
+++ backend/views/blog/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Blogs';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="blog-index">
+
+
+    <p>
+        <?php echo Html::a('Create Blog', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'user_id',
+            'title:ntext',
+            'content:ntext',
+            'date',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
---
+++ backend/views/blog/view.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Blog */
+
+$this->title = $model->title;
+$this->params['breadcrumbs'][] = ['label' => 'Blogs', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="blog-view">
+
+    <p>
+        <?php echo Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?php echo Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?php echo DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'user_id',
+            'title:ntext',
+            'content:ntext',
+            'date',
+        ],
+    ]) ?>
+
+</div>
