f22c18bd8b1b15a379f422af55f77e9f769a539f
SenG <senguttuvang@gmail.com>
2016-08-31 14:42:04 +0530

    updates invoice total after deleting a line item

--- backend/controllers/InvoiceController.php
+++ backend/controllers/InvoiceController.php
@@ -409,6 +409,7 @@ class InvoiceController extends Controller {
 		$invoiceModel = Invoice::findOne(['id' => $invoiceId]);
 		$invoiceModel->subTotal -= $lineItemModel->amount;
 		$invoiceModel->tax -= $lineItemModel->tax_rate;
+		$invoiceModel->total = $invoiceModel->subTotal + $invoiceModel->tax;
 		$invoiceModel->save();
 		$lineItemModel->delete();
 		Yii::$app->session->setFlash('alert', [
