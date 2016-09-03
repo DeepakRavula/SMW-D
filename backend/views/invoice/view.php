d37468f6123aecbe58b75b752605bf8928e6fcf8
SenG <senguttuvang@gmail.com>
2016-09-03 11:59:02 +0530

    implements 'go-back' button in pro-forma invoice view page

--- backend/views/invoice/view.php
+++ backend/views/invoice/view.php
@@ -8,10 +8,10 @@ use yii\bootstrap\Tabs;
 /* @var $this yii\web\View */
 /* @var $model common\models\Invoice */
 
-$this->title = (int) $model->type === InvoiceSearch::TYPE_PRO_FORMA_INVOICE ? 'Pro-forma Invoice' : 'Invoice';
+$this->title = (int) $model->type === InvoiceSearch::TYPE_PRO_FORMA_INVOICE ? 'Pro-forma Invoice <a href="#" class="go-back text-add-new f-s-14 m-r-10"></a>' : 'Invoice';
 $this->params['subtitle'] = Html::a('<i class="fa fa-pencil"></i> Edit', ['update', 'id' => $model->id], ['class' => 'btn btn-primary btn-sm']);
 $this->params['breadcrumbs'][] = ['label' => 'Invoices', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title. '#' .$model->id;
+$this->params['breadcrumbs'][] = $this->title. 'invoice/index' .$model->id;
 ?>
 <style>
   .invoice-view .logo>img{
@@ -70,3 +70,8 @@ $paymentContent =  $this->render('_payment', [
 ]);?>
 </div>
 </div>
+<script>
+    jQuery(document).ready(function(){
+    $('.go-back').html('<a href="javascript: history.back()">Go Back</a>');
+    });
+</script>
