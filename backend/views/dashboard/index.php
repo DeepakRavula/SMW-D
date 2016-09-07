faf10e2fa9cd9cac5b75bad97eb3f0adc5fd298a
SenG <senguttuvang@gmail.com>
2016-09-07 16:32:05 +0530

    implements monthly revenue line chart in dashboard

--- backend/views/dashboard/index.php
+++ backend/views/dashboard/index.php
@@ -1,5 +1,9 @@
 <?php
 /* @var $this yii\web\View */
+
+use miloschuman\highcharts\Highcharts;
+use common\models\Invoice;
+
 ?>
 <div class="col-md-12">
     <h3>Dashboard</h3>
@@ -36,7 +40,7 @@
         <div class="col-md-3">
             <div class="box box-primary box-solid">
                 <div class="box-header with-border">
-                  <h3 class="box-title">Enrolments</h3>
+                  <h3 class="box-title">Enrolled Students</h3>
                   <!-- /.box-tools -->
                 </div>
                 <!-- /.box-header -->
@@ -76,4 +80,33 @@
           <!-- /.box -->
         </div>
     </div>
+</div>
+<div class="col-md-6">
+<?php
+	$start = new DateTime('first day of this month - 5 months');
+	$end = new DateTime();
+	$interval = new DateInterval('P1M');
+
+	$date_period = new DatePeriod($start, $interval, $end);
+	$months = [];
+	foreach ($date_period as $dates) {
+		array_push($months, $dates->format('M'));
+	}
+
+	
+;?>
+	<?= Highcharts::widget([
+   'options' => [
+      'title' => ['text' => 'Monthly Revenue'],
+      'xAxis' => [
+         'categories' => $months
+      ],
+      'yAxis' => [
+         'title' => ['text' => 'Income'],
+      ],
+      'series' => [
+         ['name' => 'month', 'data' => [10500, 25800, 42275, 22389.5, 35000, 55555]]
+      ]
+   ]
+]);?>
 </div>
