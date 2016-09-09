63cf3a7abc7c62363222f1e7a3c5ac998f26356b
SenG <senguttuvang@gmail.com>
2016-09-09 15:22:20 +0530

    implements dashboard group enrolments widget

--- backend/controllers/DashboardController.php
+++ backend/controllers/DashboardController.php
@@ -6,6 +6,8 @@ use Yii;
 use common\models\Invoice;
 use common\models\Enrolment;
 use common\models\Payment;
+use common\models\GroupCourse;
+use common\models\Student;
 use backend\models\search\DashboardSearch;
 
 class DashboardController extends \yii\web\Controller
@@ -27,28 +29,43 @@ class DashboardController extends \yii\web\Controller
         $locationId = Yii::$app->session->get('location_id');
         $invoiceTotal = Invoice::find()
                         ->where(['location_id' => $locationId])
-                        ->andWhere(['between','date', $searchModel->fromDate->format('Y-m-d'), $searchModel->toDate->format('Y-m-d')])
+                        ->andWhere(['between', 'date', $searchModel->fromDate->format('Y-m-d'), $searchModel->toDate->format('Y-m-d')])
                         ->sum('subTotal');
         $invoiceTaxTotal = Invoice::find()
                         ->where(['location_id' => $locationId])
                         ->andWhere(['between','date', $searchModel->fromDate->format('Y-m-d'), $searchModel->toDate->format('Y-m-d')])
                         ->sum('tax');
         $enrolments = Enrolment::find()
+                    ->where(['location_id' => $locationId])                
+                    ->andWhere(['between','renewal_date', $searchModel->fromDate->format('Y-m-d'), $searchModel->toDate->format('Y-m-d')])
+                    ->count('student_id');
+        $groupEnrolments = GroupCourse::find()
+                    ->joinWith(['groupEnrolments'])
                     ->where(['location_id' => $locationId])
-                    ->andWhere(['>','renewal_date', $currentDate->format('Y-m-d')])
-                    ->count('id');
+                    ->andWhere(['between', 'end_date', $searchModel->fromDate->format('Y-m-d'), $searchModel->toDate->format('Y-m-d')])
+                    ->count('group_enrolment.student_id');
         $payments = Payment::find()
                     ->joinWith(['invoice i' => function($payments) use($locationId) {                        
                             $payments->where(['i.location_id' => $locationId]);                        
                     }])
                     ->andWhere(['between','payment.date', $searchModel->fromDate->format('Y-m-d'), $searchModel->toDate->format('Y-m-d')])
                     ->sum('payment.amount');
-        $students = Enrolment::find()
-                    ->where(['location_id' => $locationId])
-                    ->andWhere(['>','renewal_date', $currentDate->format('Y-m-d')])
-                    ->count('distinct student_id');
+         $students = Student::find()
+                    ->joinWith(['groupEnrolments'=>function($query) {
+                         $query->joinWith('groupCourse'); 
+                    }])
+                    ->joinWith('enrolment')
+                    ->andWhere(['OR',[
+                        'NOT', ['enrolment.student_id' => null]],['NOT', ['group_enrolment.student_id' => null]
+                        ]]) 
+                    ->andWhere(['OR',
+                        ['between','enrolment.renewal_date', $searchModel->fromDate->format('Y-m-d'), $searchModel->toDate->format('Y-m-d')],
+                        ['between','group_course.end_date', $searchModel->fromDate->format('Y-m-d'), $searchModel->toDate->format('Y-m-d')]
+                        ])
+                    ->distinct(['group_enrolment.student_id','enrolment.student_id'])
+                    ->count();
         
-        return $this->render('index', ['searchModel' => $searchModel, 'invoiceTotal' => $invoiceTotal, 'invoiceTaxTotal' => $invoiceTaxTotal, 'enrolments' => $enrolments, 'payments' => $payments, 'students' => $students]);
+        return $this->render('index', ['searchModel' => $searchModel, 'invoiceTotal' => $invoiceTotal, 'invoiceTaxTotal' => $invoiceTaxTotal, 'enrolments' => $enrolments, 'groupEnrolments' => $groupEnrolments, 'payments' => $payments, 'students' => $students]);
     }
 
 }
