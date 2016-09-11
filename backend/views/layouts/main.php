46aff170ee557460bfe9340eeff3fd8aa4de2419
kijanmaharjan <kijan.maharjan@gmail.com>
2016-09-11 15:14:44 +0545

    remove tabs auto scroll code

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -48,16 +48,7 @@ $(document).ready(function(){
             }
         });
     });
-    // Javascript to enable link to tab
-      var url = document.location.toString();
-      if (url.match('#')) {
-          $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
-      } 
-  
-      // Change hash for page-reload
-      $('.nav-tabs a').on('shown.bs.tab', function (e) {
-          window.location.hash = e.target.hash;
-      })
+    
 });
 </script>
 
