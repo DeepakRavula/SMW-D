f95591403c8c268418ed0c618eb077b77d9df1d9
SenG <senguttuvang@gmail.com>
2016-09-17 10:36:31 +0530

    refactors group course print page

--- backend/views/course/_print.php
+++ backend/views/course/_print.php
@@ -1,9 +1,6 @@
 <?php
 
-use yii\helpers\Html;
 use yii\grid\GridView;
-use backend\models\search\InvoiceSearch;
-use common\models\ItemType;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Invoice */
@@ -12,10 +9,36 @@ $this->title = $model->id;
 ?>
 
 <div class="invoice-view p-10">
-    <div class="row-fluid">
-        <h4 class="m-0 f-w-400"><strong><?= 'Lessons'?> </strong></h4>
+    <div class="row-fluid user-details-wrapper">
+        <div class="row-fluid p-10">
+            <h4 class="m-0 f-w-400"><strong><?= 'Lessons'?> </strong></h4>
+        </div> 
+        <div class="col-md-2 pull-left">
+            <?= 'Teacher Name: ' ?> <?= $model->teacher->publicIdentity; ?>
+        </div>
+        <div class="col-md-2 pull-right">
+            <?= 'Program Name: ' ?> <?= $model->program->name; ?>
+        </div>    	   
     </div>
-    <div class="row invoice-info m-t-20">      
+    <div class="row-fluid p-10">
+        <div class="col-md-2 pull-left">
+            <?= 'Start Date: ' ?> <?= Yii::$app->formatter->asDate($model->startDate); ?>
+        </div> 
+        <div class="col-md-2 pull-left">
+            <?= 'End Date: ' ?> <?= Yii::$app->formatter->asDate($model->endDate); ?>
+        </div>        
+        <div class="col-md-2 pull-right">
+            <?= 'Time: ' ?> <?php 
+            $fromTime = \DateTime::createFromFormat('H:i:s', $model->fromTime);
+            echo $fromTime->format('h:i A');?>	
+        </div>
+        <div class="col-md-2 pull-right">
+            <?= 'Durartion: ' ?> <?php 
+            $length = \DateTime::createFromFormat('H:i:s', $model->duration);
+            echo $length->format('H:i'); ?>
+        </div>  
+    </div> 
+    <div class="row-fluid p-10">      
     <?php yii\widgets\Pjax::begin(['id' => 'lesson-index']); ?>
         <?php echo GridView::widget([
         'dataProvider' => $lessonDataProvider,
@@ -26,9 +49,9 @@ $this->title = $model->id;
         'headerRowOptions' => ['class' => 'bg-light-gray' ],
         'columns' => [
             [
-				'label' => 'Program Name',
+				'label' => 'Teacher Name',
 				'value' => function($data) {
-					return ! empty($data->course->program->name) ? $data->course->program->name : null;
+					return ! empty($data->teacher->publicIdentity) ? $data->teacher->publicIdentity : null;
                 },
 			],
 			[
