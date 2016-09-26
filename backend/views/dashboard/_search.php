7fd5e0c5f7d35c592dd95388b1476937c31d6785
SenG <senguttuvang@gmail.com>
2016-09-26 16:27:05 +0530

    customizes date range picker in dashboard

--- backend/views/dashboard/_search.php
+++ backend/views/dashboard/_search.php
@@ -24,11 +24,20 @@ use kartik\daterange\DateRangePicker;;
    echo DateRangePicker::widget([
     'model'=>$model,
     'attribute'=>'dateRange',
-    'convertFormat'=>true,    
-    'pluginOptions'=>[ 
-            'locale'=>[        
+    'convertFormat'=>true, 
+    'initRangeExpr' => true,
+    'pluginOptions' => [
+        'ranges' => [
+            Yii::t('kvdrp', "Today") => ["moment().startOf('day')", "moment()"],
+            Yii::t('kvdrp', "Yesterday") => ["moment().startOf('day').subtract(1,'days')", "moment().endOf('day').subtract(1,'days')"],
+            Yii::t('kvdrp', "Last {n} Days", ['n' => 7]) => ["moment().startOf('day').subtract(6, 'days')", "moment()"],
+            Yii::t('kvdrp', "Last {n} Days", ['n' => 30]) => ["moment().startOf('day').subtract(29, 'days')", "moment()"],
+            Yii::t('kvdrp', "This Month") => ["moment().startOf('month')", "moment().endOf('month')"],
+            Yii::t('kvdrp', "Last Month") => ["moment().subtract(1, 'month').startOf('month')", "moment().subtract(1, 'month').endOf('month')"],
+        ],         
+        'locale'=>[        
             'format'=>'d-m-Y'
-        ]
+        ],
     ]
     ]);
    ?>
