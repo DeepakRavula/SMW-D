2154fee51d517a25b62099648800513b1d08b63a
SenG <senguttuvang@gmail.com>
2016-09-28 13:04:32 +0530

    implements draft lesson conflict in lesson review page.

--- common/models/Lesson.php
+++ common/models/Lesson.php
@@ -148,6 +148,17 @@ class Lesson extends \yii\db\ActiveRecord
 				'duration' => $teacherLesson->course->duration
 			];
 		}
+		$draftLessons = self::find()
+			->where(['courseId' => $this->courseId, 'status' => self::STATUS_DRAFTED])
+			->andWhere(['not', ['id' => $this->id]])
+			->all();
+		foreach($draftLessons as $draftLesson) {
+			$otherLessons[] = [
+				'id' => $draftLesson->id,
+				'date' =>  $draftLesson->date,
+				'duration' => $draftLesson->course->duration
+			];
+		}
 		foreach($otherLessons as $otherLesson){
 			$timebits = explode(':',$otherLesson['duration']);
 			$intervals[] = new DateRangeInclusive(new \DateTime($otherLesson['date']), new \DateTime($otherLesson['date']), new \DateInterval('PT' . $timebits[0] . 'H' . $timebits[1] .'M'), $otherLesson['id']);
