079251968819f2d7e371ceb35fed2e61b737cbf9
kijanmaharjan <kijan.maharjan@gmail.com>
2016-09-30 12:50:38 +0545

    group course print design

--- backend/views/course/view.php
+++ backend/views/course/view.php
@@ -7,7 +7,9 @@ use common\models\Enrolment;
 /* @var $model common\models\GroupCourse */
 
 $this->title = 'Group Course Details';
-$this->params['goback'] = Html::a('<i class="fa fa-angle-left fa-2x"></i>', ['index'], ['class' => 'go-back text-add-new f-s-14 m-r-10']);
+$this->params['goback'] = Html::a('<i class="fa fa-angle-left fa-2x"></i>', ['index'], ['class' => 'go-back text-add-new f-s-14 m-r-10 m-t-0']);
+$this->params['action-button'] = Html::a('<i class="fa fa-print"></i> Print', ['print', 'id' => $model->id], ['class' => 'btn btn-default pull-left', 'target'=>'_blank']); 
+
 ?>
 <div class="group-course-view">
 	<div class="row-fluid user-details-wrapper">
@@ -17,15 +19,15 @@ $this->params['goback'] = Html::a('<i class="fa fa-angle-left fa-2x"></i>', ['in
 	<div class="col-md-2" data-toggle="tooltip" data-placement="bottom" title="Teacher Name">
         	<i class="fa fa-graduation-cap"></i> <?php echo $model->teacher->publicIdentity; ?>
     </div>
-    <div class="col-md-2" data-toggle="tooltip" data-placement="bottom" title="Rate">
+    <div class="col-md-1" data-toggle="tooltip" data-placement="bottom" title="Rate">
     	<i class="fa fa-money"></i> <?php echo $model->program->rate; ?>
     </div>
-	<div class="col-md-2" data-toggle="tooltip" data-placement="bottom" title="Duration">
+	<div class="col-md-1" data-toggle="tooltip" data-placement="bottom" title="Duration">
     	<i class="fa fa-calendar"></i> <?php 
 		$length = \DateTime::createFromFormat('H:i:s', $model->duration);
 		echo $length->format('H:i'); ?>
     </div>
-	<div class="col-md-2 hand" data-toggle="tooltip" data-placement="bottom" title="Time">
+	<div class="col-md-1 p-0 hand" data-toggle="tooltip" data-placement="bottom" title="Time">
 		<i class="fa fa-clock-o"></i> <?php 
 		$fromTime = \DateTime::createFromFormat('H:i:s', $model->fromTime);
 		echo $fromTime->format('h:i A');?>	
@@ -33,17 +35,11 @@ $this->params['goback'] = Html::a('<i class="fa fa-angle-left fa-2x"></i>', ['in
 	<div class="col-md-2 hand" data-toggle="tooltip" data-placement="bottom" title="Start Date">
 			<i class="fa fa-calendar"></i> <?php echo Yii::$app->formatter->asDate($model->startDate)?>	
 	</div>
-		<div class="row-fluid">
 	<div class="col-md-2 hand" data-toggle="tooltip" data-placement="bottom" title="End Date">
 			<i class="fa fa-calendar"></i> <?php echo Yii::$app->formatter->asDate($model->endDate)?>	
 	</div>
-		</div>
     <div class="clearfix"></div>
 </div>
-</div>
-<div class="row-fluid p-10">
-    <?= Html::a('<i class="fa fa-print"></i> Print', ['print', 'id' => $model->id], ['class' => 'btn btn-default pull-left', 'target'=>'_blank',]) ?>  
-    <div class="clearfix"></div>
 </div>
 <div class="tabbable-panel">
      <div class="tabbable-line">
