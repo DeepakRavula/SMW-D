7c12d9aac11725518b418b238a64a8f697f85185
kijanmaharjan <kijan.maharjan@gmail.com>
2016-09-30 15:14:24 +0545

    lesson schedule table view

--- backend/views/enrolment/view.php
+++ backend/views/enrolment/view.php
@@ -12,15 +12,15 @@ $this->title = $model->student->fullName;
 	<div class="col-md-2" data-toggle="tooltip" data-placement="bottom" title="Teacher Name">
         	<i class="fa fa-graduation-cap"></i> <?= $model->course->teacher->publicIdentity; ?>
     </div>
-    <div class="col-md-2" data-toggle="tooltip" data-placement="bottom" title="Rate">
+    <div class="col-md-1" data-toggle="tooltip" data-placement="bottom" title="Rate">
     	<i class="fa fa-money"></i> <?= $model->course->program->rate; ?>
     </div>
-	<div class="col-md-2" data-toggle="tooltip" data-placement="bottom" title="Duration">
+	<div class="col-md-1" data-toggle="tooltip" data-placement="bottom" title="Duration">
     	<i class="fa fa-calendar"></i> <?php 
 		$length = \DateTime::createFromFormat('H:i:s', $model->course->duration);
 		echo $length->format('H:i'); ?>
     </div>
-	<div class="col-md-2 hand" data-toggle="tooltip" data-placement="bottom" title="Time">
+	<div class="col-md-1 p-0 hand" data-toggle="tooltip" data-placement="bottom" title="Time">
 		<i class="fa fa-clock-o"></i> <?php 
 		$fromTime = \DateTime::createFromFormat('H:i:s', $model->course->fromTime);
 		echo $fromTime->format('h:i A');?>	
