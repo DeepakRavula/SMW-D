5074f5c35f56838ec242babdd20f611a970a0929
kijanmaharjan <kijan.maharjan@gmail.com>
2016-09-30 16:01:58 +0545

    edit button placement in enrolment view

--- backend/views/enrolment/view.php
+++ backend/views/enrolment/view.php
@@ -13,7 +13,7 @@ $this->title = $model->student->fullName;
     <?= Html::a('<i class="fa fa-print"></i> Print', ['course/print', 'id' => $model->course->id], ['class' => 'btn btn-default pull-left', 'target'=>'_blank',]) ?>  
     <?= Html::a('<i class="fa fa-envelope-o"></i> Email Lessons', ['send-mail', 'id' => $model->id], ['class' => 'btn btn-default pull-left  m-l-20',]) ?>
 	<?php if((int) $model->course->program->type !== (int) Program::TYPE_GROUP_PROGRAM) : ?>
-		<?php echo Html::a('<i class="fa fa-pencil"></i> Edit', ['update', 'id' => $model->id], ['class' => ' m-l-20']) ?>
+		<?php $this->params['action-button'] = Html::a('<i class="fa fa-pencil"></i> Edit', ['update', 'id' => $model->id], ['class' => ' m-l-20 btn btn-sm btn-primary']) ?>
 	<?php endif; ?>
     <div class="clearfix"></div>
 </div>
