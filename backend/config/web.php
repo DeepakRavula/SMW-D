fd2687e0295796c8c3b0e763db0b407978d03e35
SenG <senguttuvang@gmail.com>
2016-10-02 19:10:08 +0530

    sets dashboard access to staff member.

--- backend/config/web.php
+++ backend/config/web.php
@@ -120,7 +120,7 @@ $config = [
                 'roles' => ['administrator', 'staffmember'],
             ],
 			[
-                'controllers'=>['schedule','student', 'release-notes', 'lesson', 'invoice','timeline-event','enrolment','teacher-availability','group-course','group-lesson','group-enrolment', 'payment', 'course'],
+                'controllers'=>['schedule','student', 'release-notes', 'lesson', 'invoice','timeline-event','enrolment','teacher-availability','group-course','group-lesson','group-enrolment', 'payment', 'course', 'dashboard'],
                 'allow' => true,
                 'roles' => ['staffmember'],
             ],
