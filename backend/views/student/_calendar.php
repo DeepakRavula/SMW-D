b3e085c8f06e786c36b169fb106e07f905a1648d
SenG <senguttuvang@gmail.com>
2016-10-19 11:48:34 +0530

    skeleton code for calendar logic.

--- backend/views/student/_calendar.php
+++ backend/views/student/_calendar.php
@@ -1,3 +1,61 @@
+<?php
+use common\models\Location;
+use common\models\TeacherAvailability;
+use common\models\Lesson;
+
+?>
+<?php
+$location = Location::findOne($id=Yii::$app->session->get('location_id'));
+	$from_time = (new \DateTime($location->from_time))->format('H:i:s');
+	$to_time = (new \DateTime($location->to_time))->format('H:i:s');
+	$locationId			 = Yii::$app->session->get('location_id');
+		$teachersWithClass	 = TeacherAvailability::find()
+			->select(['user_location.user_id as id', "CONCAT(user_profile.firstname, ' ', user_profile.lastname) as name"])
+			->distinct()
+			->joinWith(['userLocation' => function($query) use($locationId, $model) {
+				$query->joinWith(['userProfile' => function($query) use($model){
+					$query->joinWith(['lesson' => function($query) use($model){
+						$query->where(['teacherId' => $model->teacherId])
+							->andWhere(['NOT', ['lesson.status' => [Lesson::STATUS_CANCELED, Lesson::STATUS_DRAFTED]]]);
+					}]);
+				}])
+				->where(['user_location.location_id' => $locationId]);
+			}])
+			->orderBy(['teacher_availability_day.id' => SORT_DESC])
+			->one();
+
+		$activeTeachers = [
+			'id' => $teachersWithClass->id,
+			'name' => $teachersWithClass->name,
+		];
+
+		$lessons =[];
+        $lessons = Lesson::find()
+			->joinWith(['course' => function($query) {
+			    $query->andWhere(['locationId' => Yii::$app->session->get('location_id')]);
+			}])
+			->where(['teacherId' => $model->teacherId])
+            ->andWhere(['NOT', ['lesson.status' => [Lesson::STATUS_CANCELED, Lesson::STATUS_DRAFTED]]])
+            ->all();
+       $events = [];
+        foreach ($lessons as &$lesson) {
+            $toTime = new \DateTime($lesson->date);
+            $length = explode(':', $lesson->duration);
+		    $toTime->add(new \DateInterval('PT' . $length[0] . 'H' . $length[1] . 'M'));
+            if ((int) $lesson->course->program->type === (int) Program::TYPE_GROUP_PROGRAM) {
+                $title = $lesson->course->program->name . ' ( ' . $lesson->course->getEnrolmentsCount() . ' ) ';
+            } else {
+            	$title = $lesson->enrolment->student->fullName . ' ( ' .$lesson->course->program->name . ' ) ';
+			}
+            $events[]= [
+                'resources' => $lesson->teacherId,
+                'title' => $title,
+                'start' => $lesson->date,
+                'end' => $toTime->format('Y-m-d H:i:s'),
+            ];
+        }
+        unset($lesson);
+?>
 <div class="schedule-index">
 <div id='calendar' class="p-10"></div>
 </div>
--- backend/views/student/_form-private-course.php
+++ backend/views/student/_form-private-course.php
@@ -9,8 +9,7 @@ use kartik\time\TimePicker;
 use kartik\date\DatePicker;
 use kartik\depdrop\DepDrop;
 use yii\helpers\Url;
-use common\models\Location;
-use common\models\TeacherAvailability;
+
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Enrolment */
@@ -47,7 +46,6 @@ use common\models\TeacherAvailability;
 					'depends' => ['course-programid'],
 					'placeholder' => 'Select...',
 					'url' => Url::to(['/course/teachers']),
-					'onchange'=>'this.form.submit()',
 				]
 			]);
 			?>
@@ -65,65 +63,13 @@ use common\models\TeacherAvailability;
 	<?php ActiveForm::end(); ?>
 
 </div>
-<?php
-	$location = Location::findOne($id=Yii::$app->session->get('location_id'));
-	$from_time = (new \DateTime($location->from_time))->format('H:i:s');
-	$to_time = (new \DateTime($location->to_time))->format('H:i:s');
-	$locationId			 = Yii::$app->session->get('location_id');
-		$teachersWithClass	 = TeacherAvailability::find()
-			->select(['user_location.user_id as id', "CONCAT(user_profile.firstname, ' ', user_profile.lastname) as name"])
-			->distinct()
-			->joinWith(['userLocation' => function($query) use($locationId) {
-				$query->joinWith(['userProfile' => function($query) use($model){
-					$query->joinWith(['lesson' => function($query) use($model){
-						$query->where(['teacherId' => $model->teacherId])
-							->andWhere(['NOT', ['lesson.status' => [Lesson::STATUS_CANCELED, Lesson::STATUS_DRAFTED]]]);
-					}]);
-				}])
-				->where(['user_location.location_id' => $locationId]);
-			}])
-			->orderBy(['teacher_availability_day.id' => SORT_DESC])
-			->one();
-
-		$activeTeachers[] = [
-			'id' => $teacherWithClass->id,
-			'name' => $teacherWithClass->name,
-		];
-
-		$lessons =[];
-        $lessons = Lesson::find()
-			->joinWith(['course' => function($query) {
-			    $query->andWhere(['locationId' => Yii::$app->session->get('location_id')]);
-			}])
-			->where(['teacherId' => $model->teacherId])
-            ->andWhere(['NOT', ['lesson.status' => [Lesson::STATUS_CANCELED, Lesson::STATUS_DRAFTED]]])
-            ->all();
-       $events = [];
-        foreach ($lessons as &$lesson) {
-            $toTime = new \DateTime($lesson->date);
-            $length = explode(':', $lesson->duration);
-		    $toTime->add(new \DateInterval('PT' . $length[0] . 'H' . $length[1] . 'M'));
-            if ((int) $lesson->course->program->type === (int) Program::TYPE_GROUP_PROGRAM) {
-                $title = $lesson->course->program->name . ' ( ' . $lesson->course->getEnrolmentsCount() . ' ) ';
-            } else {
-            	$title = $lesson->enrolment->student->fullName . ' ( ' .$lesson->course->program->name . ' ) ';
-			}
-            $events[]= [
-                'resources' => $lesson->teacherId,
-                'title' => $title,
-                'start' => $lesson->date,
-                'end' => $toTime->format('Y-m-d H:i:s'),
-            ];
-        }
-        unset($lesson);
-		
-			
-?>
-<?php $form = ActiveForm::begin(); ?>
+<div id="calendar" class="row">
     <?php echo $this->render('_calendar', [
-		'from_time' => $from_time,
-		'to_time' =>  $to_time,
-		'teachersWithClass' => $teachersWithClass,
-		'events' => $events,
+		'model' =>  $model,
     ]) ?>
-<?php ActiveForm::end(); ?>
+</div>
+<script type="text/javascript">
+   	$('#course-teacherid').change(function() {
+	  $('#calendar').show();
+	});
+</script>
