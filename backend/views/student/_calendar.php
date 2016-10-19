9feb6a96d1b91d304a55460471b0052196fc6a20
SenG <senguttuvang@gmail.com>
2016-10-19 10:59:43 +0530

    skeleton code for private course enrolment.

---
+++ backend/views/student/_calendar.php
@@ -0,0 +1,71 @@
+<div class="schedule-index">
+<div id='calendar' class="p-10"></div>
+</div>
+<script type="text/javascript">
+$(document).ready(function() {
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,resourceDay'
+    },
+	titleFormat: 'DD-MMM-YYYY, dddd',
+    defaultView: 'resourceDay',
+    minTime: "<?php echo $from_time; ?>",
+    maxTime: "<?php echo $to_time; ?>",
+    slotDuration: "00:15:01",
+    editable: false,
+    resources:  <?php echo Json::encode($teachersWithClass); ?>,
+    events: <?php echo Json::encode($events); ?>,
+    eventClick: function(event) {
+        $(location).attr('href', event.url);
+    },
+    // the 'ev' parameter is the mouse event rather than the resource 'event'
+    // the ev.data is the resource column clicked upon
+    selectable: true,
+    selectHelper: true,
+    select: function(start, end, ev) {
+      console.log(start);
+      console.log(end);
+      console.log(ev.data); // resources
+    },
+    dayClick: function(date, allDay, jsEvent, view) {
+        if (allDay) {
+            // Clicked on the entire day
+            $('#calendar').fullCalendar('changeView', 'resourceDay');
+            $('#calendar').fullCalendar('gotoDate', date);
+            $('#calendar').fullCalendar(
+                {
+                    resources:  <?php echo Json::encode($teachersWithClass); ?>,
+                    events: <?php echo Json::encode($events); ?>,
+                }
+            );
+        }
+    },
+    eventAfterAllRender: function (view, element) {
+        var date = new Date($('#calendar').fullCalendar('getDate'));
+        var count = 0;
+        $('#calendar').fullCalendar('clientEvents', function(event) {
+            var startTime = new Date(event.start);
+            var eventDate = startTime.getDate() + "/" + startTime.getMonth() + "/" + startTime.getFullYear();
+            var currentDate = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+            if(eventDate == currentDate) {
+               count++;
+            }
+        });
+
+        if(count==0){
+            $('#myflashinfo').html("No lessons scheduled for the day").fadeIn().delay(1000).fadeOut();
+        }
+    },
+  });
+    $(".fc-button-month, .fc-button-prev, .fc-button-next, .fc-button-today").click(function(){
+        $(".fc-view-month .fc-event").hide();
+    })
+});
+</script>
--- backend/views/student/_form-private-course.php
+++ backend/views/student/_form-private-course.php
@@ -9,6 +9,8 @@ use kartik\time\TimePicker;
 use kartik\date\DatePicker;
 use kartik\depdrop\DepDrop;
 use yii\helpers\Url;
+use common\models\Location;
+use common\models\TeacherAvailability;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Enrolment */
@@ -18,6 +20,16 @@ use yii\helpers\Url;
 <div class="enrolment-form form-well form-well-smw">
 	<?php $form = ActiveForm::begin(); ?>
     <div class="row">
+		<div class="col-md-4">
+			<?php
+			echo $form->field($model, 'duration')->widget(TimePicker::classname(), [
+				'pluginOptions' => [
+					'showMeridian' => false,
+					'defaultTime' => date('H:i', strtotime('00:30')),
+				]
+			]);
+			?>
+		</div>
 		<div class="col-md-4">
 			<?php echo $form->field($model, 'programId')->dropDownList(
 					ArrayHelper::map(Program::find()
@@ -34,20 +46,8 @@ use yii\helpers\Url;
 				'pluginOptions' => [
 					'depends' => ['course-programid'],
 					'placeholder' => 'Select...',
-					'url' => Url::to(['/course/teachers'])
-				]
-			]);
-			?>
-		</div>
-		<div class="col-md-4">
-			<?php
-			// Dependent Dropdown
-			echo $form->field($model, 'day')->widget(DepDrop::classname(), [
-				'options' => ['id' => 'course-day'],
-				'pluginOptions' => [
-					'depends' => ['course-teacherid'],
-					'placeholder' => 'Select...',
-					'url' => Url::to(['/teacher-availability/available-days'])
+					'url' => Url::to(['/course/teachers']),
+					'onchange'=>'this.form.submit()',
 				]
 			]);
 			?>
@@ -55,56 +55,75 @@ use yii\helpers\Url;
 	</div>
 	<div class="row">
 		<div class="col-md-4">
-			<?php
-			// Dependent Dropdown
-			echo $form->field($model, 'fromTime')->widget(DepDrop::classname(), [
-				'options' => ['id' => 'course-fromtime'],
-				'pluginOptions' => [
-					'depends' => ['course-teacherid', 'course-day'],
-					'placeholder' => 'Select...',
-					'url' => Url::to(['/teacher-availability/available-hours'])
-				]
-			]);
-			?>
-		</div>
-		<div class="col-md-4">
-			<?php
-			echo $form->field($model, 'duration')->widget(TimePicker::classname(), [
-				'pluginOptions' => [
-					'showMeridian' => false,
-					'defaultTime' => date('H:i', strtotime('00:30')),
-				]
-			]);
-			?>
-		</div>
-		<div class="col-md-4">
-			<?php 
-            if($model->isNewRecord){
-                $model->startDate = (new \DateTime())->format('d-m-Y');
-            }
-            ?>
-			<?php
-			echo $form->field($model, 'startDate')->widget(DatePicker::classname(), [
-				'type' => DatePicker::TYPE_COMPONENT_APPEND,
-                'options' => [
-                    'value' => $model->startDate,                      
-                    ],
-       				'pluginOptions' => [
-					'format' => 'dd-mm-yyyy',
-					'todayHighlight' => true,
-					'autoclose' => true
-				]
-			]);
-			?>
-		</div>
-		<div class="col-md-4">
         	<?= $form->field($model, 'paymentFrequency')->radioList(Enrolment::paymentFrequencies())?>
 		</div>
 	</div>
     <div class="form-group">
 		<?php echo Html::submitButton(Yii::t('backend', 'Preview Lessons'), ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
     </div>
-
+<div class="clearfix"></div>
 	<?php ActiveForm::end(); ?>
 
 </div>
+<?php
+	$location = Location::findOne($id=Yii::$app->session->get('location_id'));
+	$from_time = (new \DateTime($location->from_time))->format('H:i:s');
+	$to_time = (new \DateTime($location->to_time))->format('H:i:s');
+	$locationId			 = Yii::$app->session->get('location_id');
+		$teachersWithClass	 = TeacherAvailability::find()
+			->select(['user_location.user_id as id', "CONCAT(user_profile.firstname, ' ', user_profile.lastname) as name"])
+			->distinct()
+			->joinWith(['userLocation' => function($query) use($locationId) {
+				$query->joinWith(['userProfile' => function($query) use($model){
+					$query->joinWith(['lesson' => function($query) use($model){
+						$query->where(['teacherId' => $model->teacherId])
+							->andWhere(['NOT', ['lesson.status' => [Lesson::STATUS_CANCELED, Lesson::STATUS_DRAFTED]]]);
+					}]);
+				}])
+				->where(['user_location.location_id' => $locationId]);
+			}])
+			->orderBy(['teacher_availability_day.id' => SORT_DESC])
+			->one();
+
+		$activeTeachers[] = [
+			'id' => $teacherWithClass->id,
+			'name' => $teacherWithClass->name,
+		];
+
+		$lessons =[];
+        $lessons = Lesson::find()
+			->joinWith(['course' => function($query) {
+			    $query->andWhere(['locationId' => Yii::$app->session->get('location_id')]);
+			}])
+			->where(['teacherId' => $model->teacherId])
+            ->andWhere(['NOT', ['lesson.status' => [Lesson::STATUS_CANCELED, Lesson::STATUS_DRAFTED]]])
+            ->all();
+       $events = [];
+        foreach ($lessons as &$lesson) {
+            $toTime = new \DateTime($lesson->date);
+            $length = explode(':', $lesson->duration);
+		    $toTime->add(new \DateInterval('PT' . $length[0] . 'H' . $length[1] . 'M'));
+            if ((int) $lesson->course->program->type === (int) Program::TYPE_GROUP_PROGRAM) {
+                $title = $lesson->course->program->name . ' ( ' . $lesson->course->getEnrolmentsCount() . ' ) ';
+            } else {
+            	$title = $lesson->enrolment->student->fullName . ' ( ' .$lesson->course->program->name . ' ) ';
+			}
+            $events[]= [
+                'resources' => $lesson->teacherId,
+                'title' => $title,
+                'start' => $lesson->date,
+                'end' => $toTime->format('Y-m-d H:i:s'),
+            ];
+        }
+        unset($lesson);
+		
+			
+?>
+<?php $form = ActiveForm::begin(); ?>
+    <?php echo $this->render('_calendar', [
+		'from_time' => $from_time,
+		'to_time' =>  $to_time,
+		'teachersWithClass' => $teachersWithClass,
+		'events' => $events,
+    ]) ?>
+<?php ActiveForm::end(); ?>
