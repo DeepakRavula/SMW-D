519943a34f744ae5da76fbb7e9618834a8aa00da
SenG <senguttuvang@gmail.com>
2016-10-22 10:04:18 +0530

    removes "Day" view in enrolment calendar.

--- backend/views/student/_calendar.php
+++ backend/views/student/_calendar.php
@@ -23,7 +23,7 @@ $(document).ready(function() {
     header: {
       left: 'prev,next today',
       center: 'title',
-      right: 'agendaWeek,agendaDay'
+      right: 'agendaWeek'
     },
 	allDaySlot:false,
 	slotDuration: '00:15:00',
