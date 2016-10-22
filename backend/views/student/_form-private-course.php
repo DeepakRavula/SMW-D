0244b43c4c96fd56c4e1791ead826d31046d5a51
SenG <senguttuvang@gmail.com>
2016-10-22 21:00:55 +0530

    changes enrolment UI

--- backend/views/student/_form-private-course.php
+++ backend/views/student/_form-private-course.php
@@ -1,45 +1,57 @@
 <?php
+
 use common\models\Course;
 use drsdre\wizardwidget\WizardWidget;
+use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Enrolment */
 /* @var $form yii\bootstrap\ActiveForm */
 ?>
 <?php
-$programDetailContent = $this->render('_private-course-basic-detail', [
-    'model' => new Course(),
-]);
+$form					 = ActiveForm::begin([
+		'id' => 'enrolment-form'
+	]);
+?>
 
-$calendarContent = $this->render('_calendar',[
-    'model' => new Course(),
-]);
+<?php
+$programDetailContent	 = $this->render('_private-course-basic-detail',
+	[
+	'model' => new Course(),
+	'form' => $form,
+	]);
+
+$calendarContent = $this->render('_calendar', [
+	'model' => new Course(),
+	'form' => $form,
+	]);
 ?>
 <?php
-$wizard_config = [
-    'id' => 'stepwizard',
-    'steps' => [
-        1 => [
-            'title' => 'Step 1 - Choose Program',
-            'icon' => 'glyphicon glyphicon-music',
-            'content' => $programDetailContent,
-            'buttons' => [
-                'next' => [
-                    'title' => 'Next',
-                    'options' => [
-                        'class' => 'disabled'
-                    ],
-                 ],
-             ],
-        ],
-        2 => [
-            'title' => 'Step 2 - Choose Teacher',
-            'icon' => 'glyphicon glyphicon-education',
-            'content' => $calendarContent,
-        ],
-    ],
+$wizard_config	 = [
+	'id' => 'stepwizard',
+	'steps' => [
+		1 => [
+			'class' => 'step-1',
+			'title' => 'Step 1 - Choose Program',
+			'icon' => 'glyphicon glyphicon-music',
+			'content' => $programDetailContent,
+			'buttons' => [
+				'next' => [
+					'title' => 'Next',
+					'options' => [
+						'class' => 'disabled'
+					],
+				],
+			],
+		],
+		2 => [
+			'title' => 'Step 2 - Choose Teacher',
+			'icon' => 'glyphicon glyphicon-education',
+			'content' => $calendarContent,
+		],
+	],
 ];
 ?>
 <?= WizardWidget::widget($wizard_config); ?>
+<?php ActiveForm::end(); ?>
 
-	
--- backend/views/student/_private-course-basic-detail.php
+++ backend/views/student/_private-course-basic-detail.php
@@ -2,14 +2,10 @@
 
 use common\models\Program;
 use common\models\Enrolment;
-use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
-use yii\bootstrap\ActiveForm;
 use kartik\time\TimePicker;
 use kartik\date\DatePicker;
-use kartik\depdrop\DepDrop;
-use yii\helpers\Url;
-use wbraganca\selectivity\SelectivityWidget;
+use common\models\Location;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Enrolment */
@@ -17,22 +13,21 @@ use wbraganca\selectivity\SelectivityWidget;
 ?>
 <?php
 $privatePrograms = ArrayHelper::map(Program::find()
-		->active()
-		->where(['type' => Program::TYPE_PRIVATE_PROGRAM])
-		->all(),
-	'id', 'name')
+			->active()
+			->where(['type' => Program::TYPE_PRIVATE_PROGRAM])
+			->all(), 'id', 'name')
 ?>
 <div class="enrolment-form form-well form-well-smw">
-<?php $form = ActiveForm::begin(); ?>
     <div class="row">
 		<div class="col-md-4">
 			<?php
-			echo $form->field($model, 'startDate')->widget(DatePicker::classname(), [
+			echo $form->field($model, 'startDate')->widget(DatePicker::classname(),
+				[
 				'type' => DatePicker::TYPE_COMPONENT_APPEND,
-					'options' => [
-						'value' => (new \DateTime())->format('d-m-Y'),
-					],
-       				'pluginOptions' => [
+				'options' => [
+					'value' => (new \DateTime())->format('d-m-Y'),
+				],
+				'pluginOptions' => [
 					'format' => 'dd-mm-yyyy',
 					'todayHighlight' => true,
 					'autoclose' => true
@@ -42,7 +37,8 @@ $privatePrograms = ArrayHelper::map(Program::find()
 		</div>
 		<div class="col-md-4">
 			<?php
-			echo $form->field($model, 'duration')->widget(TimePicker::classname(), [
+			echo $form->field($model, 'duration')->widget(TimePicker::classname(),
+				[
 				'pluginOptions' => [
 					'showMeridian' => false,
 					'defaultTime' => date('H:i', strtotime('00:30')),
@@ -51,29 +47,62 @@ $privatePrograms = ArrayHelper::map(Program::find()
 			?>
 		</div>
 		<div class="col-md-4">
-			<?=
-				$form->field($model, 'programId')->widget(SelectivityWidget::classname(), [
-					'pluginOptions' => [
-						'allowClear' => true,
-						'multiple' => false,
-						'items' => $privatePrograms,
-						'placeholder' => 'Select Program'
-					]
-				]);
+			<?php
+			echo $form->field($model, 'programId')->dropDownList(
+				ArrayHelper::map(Program::find()
+						->active()
+						->where(['type' => Program::TYPE_PRIVATE_PROGRAM])
+						->all(), 'id', 'name'), ['prompt' => 'Select..']);
 			?>
 		</div>
+
 	</div>
 	<div class="row">
 		<div class="col-md-4">
-        	<?= $form->field($model, 'paymentFrequency')->radioList(Enrolment::paymentFrequencies())?>
+<?= $form->field($model, 'paymentFrequency')->radioList(Enrolment::paymentFrequencies()) ?>
 		</div>
 	</div>
-	<?php ActiveForm::end(); ?>
 
 </div>
+<link type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.1/fullcalendar.min.css" rel="stylesheet">
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.0.1/fullcalendar.min.js"></script>
 <?php
-    $session = Yii::$app->session;
-	$session->set('startDate', $model->startDate);
-	$session->set('duration', $model->duration);
-	$session->set('programId', $model->programId);
+$locationId		 = Yii::$app->session->get('location_id');
+$location		 = Location::findOne(['id' => $locationId]);
+$from_time		 = (new \DateTime($location->from_time))->format('H:i:s');
+$to_time		 = (new \DateTime($location->to_time))->format('H:i:s');
 ?>
+<script>
+
+    $(document).ready(function () {
+        $('#stepwizard_step2_save').click(function () {
+            $('#enrolment-form').submit();
+        });
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+            var target = $(e.target).attr("href") // activated tab
+            loadCalendar();
+        });
+    });
+    function loadCalendar() {
+        $('#calendar').fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'agendaWeek'
+            },
+            allDaySlot: false,
+            slotDuration: '00:15:00',
+            titleFormat: 'DD-MMM-YYYY, dddd',
+            defaultView: 'agendaWeek',
+            minTime: "<?php echo $from_time; ?>",
+            maxTime: "<?php echo $to_time; ?>",
+            selectConstraint: 'businessHours',
+            eventConstraint: 'businessHours',
+            businessHours: [],
+            allowCalEventOverlap: true,
+            overlapEventsSeparate: true,
+            events: [],
+        });
+    }
+</script>
+
