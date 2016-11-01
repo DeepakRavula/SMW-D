7f89ea39eb40867e93178a4f53288f4fa7ae6bf1
SenG <senguttuvang@gmail.com>
2016-11-01 11:01:06 +0530

    adds tax status in line item model rule.

--- common/models/InvoiceLineItem.php
+++ common/models/InvoiceLineItem.php
@@ -34,18 +34,23 @@ class InvoiceLineItem extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['unit', 'amount', 'description'], 'required', 'when' => function ($model, $attribute) {
+            [['unit', 'amount', 'description', 'tax_status'], 'required', 'when' => function ($model, $attribute) {
 					return (int) $model->item_type_id === ItemType::TYPE_MISC;
 				}
 			],
-            [['invoice_id', 'item_id'], 'integer'],
-            [['amount'], 'number'],
-			[['isRoyalty', 'invoice_id', 'item_id','item_type_id', 'tax_code', 'tax_status', 'tax_type', 'tax_rate'], 'safe'],
-			[['isRoyaltyExempted'], 'boolean'],
-			[['unit'], 'integer', 'when' => function ($model, $attribute) {
-				return (int) $model->item_type_id === ItemType::TYPE_MISC;
-			}
+            [['amount'], 'number', 'when' => function ($model, $attribute) {
+					return (int) $model->item_type_id === ItemType::TYPE_MISC;
+				}
 			],
+			[['isRoyaltyExempted'], 'boolean', 'when' => function ($model, $attribute) {
+					return (int) $model->item_type_id === ItemType::TYPE_MISC;
+				}
+			],
+			[['unit'], 'integer', 'when' => function ($model, $attribute) {
+					return (int) $model->item_type_id === ItemType::TYPE_MISC;
+				}
+			],
+			[['isRoyalty', 'invoice_id', 'item_id', 'item_type_id', 'tax_code', 'tax_status', 'tax_type', 'tax_rate'], 'safe'],
 		];
     }
 
