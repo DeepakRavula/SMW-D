aeb2267da74b28b461d3c666c8f963b2c603ee56
SenG <senguttuvang@gmail.com>
2016-11-04 21:06:52 +0530

    migration for vacation entity.

---
+++ common/migrations/db/m161104_152951_add_vacation_up.sql
@@ -0,0 +1,17 @@
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `vacation`
+-- ----------------------------
+DROP TABLE IF EXISTS `vacation`;
+CREATE TABLE `vacation` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `studentId` int(10) unsigned NOT NULL,
+  `fromDate` timestamp NOT NULL,
+  `toDate` timestamp NOT NULL,
+  `isConfirmed` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 
+
+SET FOREIGN_KEY_CHECKS = 1;
