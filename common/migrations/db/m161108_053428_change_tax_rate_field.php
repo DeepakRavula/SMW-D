a1dd23ed7cf221152809de8dac1fca4a37adbe14
SenG <senguttuvang@gmail.com>
2016-11-08 11:08:16 +0530

    migration for invoice line item "tax_rate" field.

---
+++ common/migrations/db/m161108_053428_change_tax_rate_field.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\db\Migration;
+
+class m161108_053428_change_tax_rate_field extends Migration
+{
+    public function up()
+    {
+		$sql = file_get_contents(dirname(__FILE__).'/'.get_class($this).'_'.__FUNCTION__.'.sql');
+
+        return $this->execute($sql);
+    }
+
+    public function down()
+    {
+        echo "m161108_053428_change_tax_rate_field cannot be reverted.\n";
+
+        return false;
+    }
+}
