633e6492cdf528b09b42cd61fed1b3431ee8aad8
SenG <senguttuvang@gmail.com>
2016-11-09 12:40:38 +0530

    removes deleted lesson event in enrolment calendar.

--- backend/controllers/TeacherAvailabilityController.php
+++ backend/controllers/TeacherAvailabilityController.php
@@ -179,6 +179,7 @@ class TeacherAvailabilityController extends Controller
                 $query->andWhere(['locationId' => Yii::$app->session->get('location_id')]);
             }])
             ->where(['lesson.teacherId' => $id])
+			->notDeleted()
             ->andWhere(['NOT', ['lesson.status' => [Lesson::STATUS_CANCELED, Lesson::STATUS_DRAFTED]]])
             ->all();
         $events = [];
