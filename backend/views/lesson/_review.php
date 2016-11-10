fe1e914a037609bccd6cc1f502cd3da0521b9ae2
SenG <senguttuvang@gmail.com>
2016-11-10 15:35:26 +0530

    shows teacher's availabilities in lesson review page.

--- backend/views/lesson/_review.php
+++ backend/views/lesson/_review.php
@@ -3,6 +3,8 @@ use yii\helpers\Html;
 use common\models\Program;
 use yii\helpers\Url;
 use kartik\grid\GridView;
+use common\models\TeacherAvailability;
+use yii\data\ActiveDataProvider;
 
 $this->title = 'Review Lessons';
 ?>
@@ -30,6 +32,56 @@ $this->title = 'Review Lessons';
         $fromTime = \DateTime::createFromFormat('H:i:s', $courseModel->fromTime);
         echo $fromTime->format('h:i A'); ?>	
 	</div>
+	</div>
+	<div class="clearfix"></div>
+	<div class="row">
+		<?php
+		$locationId = Yii::$app->session->get('location_id');
+		$query = TeacherAvailability::find()
+			->joinWith('userLocation')
+			->where(['user_id' => $courseModel->teacherId, 'location_id' => $locationId]);
+		$teacherAvailabilityDataProvider = new ActiveDataProvider([
+			'query' => $query,
+		]);
+		?>
+		<?php yii\widgets\Pjax::begin() ?>
+		<h4>Availabilities </h4>
+		<?php
+		echo GridView::widget([
+			'dataProvider' => $teacherAvailabilityDataProvider,
+			'options' => ['class' => 'col-md-5'],
+			'tableOptions' => ['class' => 'table table-bordered'],
+			'headerRowOptions' => ['class' => 'bg-light-gray'],
+			'columns' => [
+				[
+					'label' => 'Day',
+					'value' => function ($data) {
+						if (!empty($data->day)) {
+							$dayList = TeacherAvailability::getWeekdaysList();
+							$day	 = $dayList[$data->day];
+
+							return !empty($day) ? $day : null;
+						}
+
+						return null;
+					},
+				],
+				[
+					'label' => 'From Time',
+					'value' => function ($data) {
+						return !empty($data->from_time) ? Yii::$app->formatter->asTime($data->from_time) : null;
+					},
+				],
+				[
+					'label' => 'To Time',
+					'value' => function ($data) {
+						return !empty($data->to_time) ? Yii::$app->formatter->asTime($data->to_time) : null;
+					},
+				],
+			],
+		]);
+		?>
+		<?php \yii\widgets\Pjax::end(); ?>
 	</div>
 	<div class="clearfix"></div>	
 	<?php
