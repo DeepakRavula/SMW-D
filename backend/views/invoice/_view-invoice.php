25755aec57c9136851eea376b99743876f60df7b
SenG <senguttuvang@gmail.com>
2016-11-12 13:03:34 +0530

    sets link for customer on customer name in invoice view page

--- backend/views/invoice/_view-invoice.php
+++ backend/views/invoice/_view-invoice.php
@@ -72,7 +72,11 @@ use kartik\switchinput\SwitchInput;
         <div class="col-sm-8 invoice-col m-b-20 p-0">
           <div class="row m-t-10">
             <div class="col-xs-8">
-              <h4 class="m-0 f-w-400"><strong><?= isset($customer->publicIdentity) ? $customer->publicIdentity : null?></strong></h4>
+                <h4 class="m-0 f-w-400">
+                    <a href="/user/view?UserSearch%5Brole_name%5D=customer&id=<?php echo $customer->id ?>#student">
+                        <strong><?= isset($customer->publicIdentity) ? $customer->publicIdentity : null?></strong>
+                    </a>
+                </h4>
               <div class="text-gray">
 	   		<?php
                 $addresses = $customer->addresses;
