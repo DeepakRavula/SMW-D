bd9131ca84894d1844c20e68a66b98677338b14a
SenG <senguttuvang@gmail.com>
2016-12-10 13:17:33 +0530

    migration script for class room entiry.

---
+++ common/migrations/db/m161210_074346_add_class_room.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\db\Migration;
+
+class m161210_074346_add_class_room extends Migration
+{
+    public function up()
+    {
+		$sql = file_get_contents(dirname(__FILE__).'/'.get_class($this).'_'.__FUNCTION__.'.sql');
+
+        return $this->execute($sql);
+    }
+
+    public function down()
+    {
+        echo "m161210_074346_add_class_room cannot be reverted.\n";
+
+        return false;
+    }
+}
---
+++ common/migrations/db/m161210_074346_add_class_room_up.sql
@@ -0,0 +1,16 @@
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `classroom`
+-- ----------------------------
+BEGIN;
+DROP TABLE IF EXISTS `vacation`;
+CREATE TABLE `classroom` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
