cd04835342501e941ec177e6fcb60490dfe808c2
SenG <senguttuvang@gmail.com>
2016-12-28 11:56:30 +0530

    styles student note listing.

--- backend/views/student/note/view.php
+++ backend/views/student/note/view.php
@@ -1,7 +1,7 @@
 <?php
 
 use common\models\Note;
-use yii\grid\GridView;
+use yii\widgets\ListView;
 use yii\bootstrap\Modal;
 
 /* @var $this yii\web\View */
@@ -29,45 +29,9 @@ Modal::end();
 	'id' => 'student-note-listing',
 	'timeout' => 6000,
 ]) ?>
-<?php
-echo GridView::widget([
-    'dataProvider' => $noteDataProvider,
-    'options' => ['class' => 'col-md-12'],
-    'tableOptions' => ['class' => 'table table-bordered'],
-    'headerRowOptions' => ['class' => 'bg-light-gray'],
-    'columns' => [
-		[
-			'label' => 'Content',
-			'value' => function($data) {
-				return !empty($data->content) ? $data->content : null;
-			}
-		],
-		[
-			'label' => 'Created User',
-			'value' => function($data) {
-				return !empty($data->createdUser->publicIdentity) ? $data->createdUser->publicIdentity : null;
-			}
-		],
-		[
-			'label' => 'Updated User',
-			'value' => function($data) {
-				return !empty($data->updatedUser->publicIdentity) ? $data->updatedUser->publicIdentity : null;
-			}
-		],
-		[
-			'label' => 'CreatedOn',
-			'value' => function($data) {
-				return !empty($data->createdOn) ? Yii::$app->formatter->asDate($data->createdOn) : null;
-			}
-		],
-		[
-			'label' => 'UpdatedOn',
-			'value' => function($data) {
-				return !empty($data->updatedOn) ? Yii::$app->formatter->asDate($data->updatedOn) : null;
-			}
-		],
-    ],
-]);
-?>
+<?php echo ListView::widget([
+	'dataProvider' =>  $noteDataProvider,
+	'itemView' => '_view',
+]); ?>
 </div>
 <?php \yii\widgets\Pjax::end(); ?>
