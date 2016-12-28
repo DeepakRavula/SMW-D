f1540ba121a299ef20d447d831eb807a8d957019
SenG <senguttuvang@gmail.com>
2016-12-28 12:43:57 +0530

    migration for add discount and type in invoice line item table.

---
+++ common/migrations/db/m161228_070953_add_discount_and_type.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\db\Migration;
+
+class m161228_070953_add_discount_and_type extends Migration
+{
+    public function up()
+    {
+		$sql = file_get_contents(dirname(__FILE__).'/'.get_class($this).'_'.__FUNCTION__.'.sql');
+
+        return $this->execute($sql);
+    }
+
+    public function down()
+    {
+        echo "m161228_070953_add_discount_and_type cannot be reverted.\n";
+
+        return false;
+    }
+}
