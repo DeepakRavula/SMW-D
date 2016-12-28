ba574ef1e2ca594304a1a75e2888179a07f81058
SenG <senguttuvang@gmail.com>
2016-12-28 12:35:37 +0530

    migration for drop discount table.

---
+++ common/migrations/db/m161228_065912_drop_discount_up.sql
@@ -0,0 +1,11 @@
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Drop `discount` table
+-- ----------------------------
+BEGIN;
+DROP TABLE `discount`; 
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
