d1479183bb73cd72b2d2717417e12aad25310708
SenG <senguttuvang@gmail.com>
2017-01-01 14:22:54 +0530

    removes edit icon in student note page.

--- backend/views/student/note/_view.php
+++ backend/views/student/note/_view.php
@@ -6,10 +6,6 @@ use yii\helpers\Html;
 <div class="panel panel-default">
 <div class="panel-heading">
 <strong><?= $model->createdUser->publicIdentity; ?></strong> <span class="text-muted"><?= 'created on ' . (new \DateTime($model->createdOn))->format('M. d, Y'); ?></span>
-<?= Html::a('<i class="fa fa-pencil" aria-hidden="true"></i>','#', [
-	'id' => 'edit-note',
-	'class' => 'edit-student-note m-l-20'
-]);?>
 </div>
 <div class="panel-body">
 <?= $model->content; ?>
