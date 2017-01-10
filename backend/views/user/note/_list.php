180205b6a0d77e6ce962c43230e380f3bd44c2da
SenG <senguttuvang@gmail.com>
2017-01-10 14:18:59 +0530

    adds missing file to add note.

---
+++ backend/views/user/note/_list.php
@@ -0,0 +1,30 @@
+<?php
+use yii\helpers\Html;
+use dosamigos\editable\Editable;
+use yii\helpers\Url;
+?>
+<div class="col-sm-10">
+<div class="panel panel-default">
+<div class="panel-heading">
+<strong><?= $model->createdUser->publicIdentity; ?></strong> <span class="text-muted">
+<?php if($model->createdOn !== $model->updatedOn): ?>
+<?= 'updated on ' . (new \DateTime($model->updatedOn))->format('M. d, Y'); ?>
+<?php else : ?>
+<?= 'created on ' . (new \DateTime($model->createdOn))->format('M. d, Y'); ?>
+<?php endif; ?></span>
+</div>
+<div class="panel-body">
+<?= Editable::widget( [
+    'name' => 'content',
+    'value' => $model->content,
+    'url' => Url::to(['note/update', 'id' => $model->id]),
+    'type' => 'textarea',
+    'mode' => 'pop',
+    'clientOptions' => [
+        'placement' => 'right',
+	'showbuttons' => 'bottom',
+    ]
+]);?>
+</div><!-- /panel-body -->
+</div><!-- /panel panel-default -->
+</div><!-- /col-sm-5 -->
