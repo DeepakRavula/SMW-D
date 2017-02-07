7b5cd497b2a7081eb0ea2204a4b7585117bfbe0c
SenG <senguttuvang@gmail.com>
2017-02-07 19:56:49 +0530

    shows teacher availability in enrolment edit page.

--- backend/views/enrolment/_form.php
+++ backend/views/enrolment/_form.php
@@ -6,16 +6,47 @@ use kartik\time\TimePicker;
 use kartik\date\DatePicker;
 use common\models\Course;
 use common\models\Enrolment;
+use common\models\TeacherAvailability;
+use yii\data\ActiveDataProvider;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Enrolment */
 /* @var $form yii\bootstrap\ActiveForm */
 ?>
+<style>
+
+	.smw-box {
+    background: #f5ecec;
+    border: 1px solid #f5ecec;
+    padding: 10px;
+    color: white;
+    border-radius: 5px;
+    color:#333;
+}
+.monthly-estimate {
+	margin-top:20px;
+	margin-bottom: 0px;
+}
+</style>
 <?=
 $this->render('_view-enrolment', [
 	'model' => $model->enrolment,
 ]);
 ?>
+<div>
+<div class="smw-box col-md-10 m-l-10 m-b-10 monthly-estimate">
+<?php
+	$query = TeacherAvailability::find()
+                ->joinWith('userLocation')
+                ->where(['user_id' => $model->teacherId]);
+        $teacherDataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]); ?>
+	<?= $this->render('_teacher-availability', [
+    'teacherDataProvider' => $teacherDataProvider,
+]); ?>
+	</div>
+	</div>
 <div class="enrolment-form form-well form-well-smw">
 		<?php $form			 = ActiveForm::begin(); ?>
     <div class="row">
