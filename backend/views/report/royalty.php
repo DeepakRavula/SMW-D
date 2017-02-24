5e8a22ed542c792438d279bc4d583654da18f922
SenG <senguttuvang@gmail.com>
2017-02-24 15:04:09 +0530

    styles royalty report page.

--- backend/views/report/royalty.php
+++ backend/views/report/royalty.php
@@ -6,23 +6,58 @@ use common\models\Location;
 $this->title = 'Royalty';
 ?>
 <?php
-	$location = Location::findOne(['id' => Yii::$app->session->get('location_id')]);
-	$advertisement = !empty($location->advertisement->value) ? $location->advertisement->value : 0;
-	$royalty = !empty($location->royalty->value) ? $location->royalty->value : 0; 
-	$locationDebtAmount = $royalty + $advertisement; 
+$total = $payments - ($invoiceTaxTotal + $royaltyPayment);
+$location = Location::findOne(['id' => Yii::$app->session->get('location_id')]);
+$advertisement = !empty($location->advertisement->value) ? $location->advertisement->value : 0;
+$royalty = !empty($location->royalty->value) ? $location->royalty->value : 0;
+$locationDebtAmount = $royalty + $advertisement;
+$royaltyAmount = ($total * ($royalty / 100));
+$advertisementAmount = ($total * ($advertisement / 100)); 
+$netPrice = round(($total - ($royaltyAmount + $advertisementAmount)), 2);
 ?>
 <div class="col-md-12">
     <div class="col-xs-12 col-md-6 p-10 p-r-0">
 		<?php echo $this->render('_search', ['model' => $searchModel]); ?>
 	</div>
 	<div class="clearfix"></div>
-	<div> <h4>
-	<div> Payments Received : <?= !empty($payments) ? $payments : 0; ?></div>
-	<div>Tax Collected : <?= !empty($invoiceTaxTotal) ? $invoiceTaxTotal : 0;?></div>
-	<div>Royalty Free Items : <?= !empty($royaltyPayment) ? $royaltyPayment : 0; ?></div>
-	<div>Total : <?php $total = $payments - ($invoiceTaxTotal + $royaltyPayment); ?>  <?= !empty($total) ? $total : 0; ?></div>
-	<div>Advertisement : <?= !empty($location->advertisement->value) ? $location->advertisement->value . '%' : ' - '; ?></div>
-	<div>Royalty : <?= !empty($location->royalty->value) ? $location->royalty->value . '%' : ' - '; ?></div>
-	<div>Owe to head office : <?= round(($total - ($total * ($locationDebtAmount / 100))) ,2); ?> </div>
-	</h4>
+	<div>
+		<table cellspacing="0" cellpadding="3" border="0" style="width:40%">
+			<tr>
+				<td class="p-t-10">Payments Received</td>
+				<td></td>
+				<td class="p-t-10"><?= !empty($payments) ? $payments : 0; ?></td>
+			</tr>
+			<tr>
+				<td class="p-t-10">Tax Collected</td>
+				<td></td>
+				<td class="p-t-10"><?= !empty($invoiceTaxTotal) ? $invoiceTaxTotal : 0; ?></td>
+			</tr>
+			<tr>
+				<td class="p-t-10">Royalty Free Items</td>
+				<td></td>
+				<td class="p-t-10"><?= !empty($royaltyPayment) ? $royaltyPayment : 0; ?></td>
+			</tr>
+			<tr>
+				<td class="p-t-10">Total</td>
+				<td></td>
+				<td class="p-t-10"><?= !empty($total) ? $total : 0; ?></td>
+			</tr>
+			<tr>
+			<td class="p-t-10">Advertisement</td>
+			<td class="p-t-10"><?= !empty($location->advertisement->value) ? $location->advertisement->value . '%' : ' - '; ?></td>
+			<td class="p-t-10"><?= round($advertisementAmount, 2); ?></td>
+			</tr>
+			<tr>
+			<td class="p-t-10">Royalty</td>
+			<td class="p-t-10"><?= !empty($location->royalty->value) ? $location->royalty->value . '%' : ' - ';  ?></td>
+			<td class="p-t-10"><?= round($royaltyAmount, 2); ?></td>
+			</tr>
+			<tr>
+			<td class="p-t-10"><strong>Owe to head office</strong></td>
+			<td></td>
+			<td><strong><?= $netPrice; ?></strong></td>
+			</tr>
+		</table>
+	</div>
+	<div class="clearfix"></div>
 </div>
