b5e1214d05a0b58b24cf0e1280cc85b573c1f153
SenG <senguttuvang@gmail.com>
2017-03-16 12:55:05 +0530

    formats payment index page.

--- backend/views/report/payment/_payment.php
+++ backend/views/report/payment/_payment.php
@@ -11,164 +11,164 @@ use common\models\Payment;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 ?>
 <div class="payments-index p-10">
-	<?php if($searchModel->groupByMethod) : ?>
-	<?php $columns = [
-		[
-			'value' => function ($data) {
-				if (!empty($data->date)) {
-					$lessonDate = \DateTime::createFromFormat('Y-m-d H:i:s', $data->date);
-					return $lessonDate->format('l, F jS, Y');
-				}
-
-				return null;
-			},
-			'group' => true,
-			'groupedRow' => true,
-			
-			'groupFooter' => function ($model, $key, $index, $widget) {
-				return [
-					'mergeColumns' => [[1]],
-					'content' => [
-						2 => GridView::F_SUM,
-					],
-					'contentFormats' => [
-						2 => ['format' => 'number', 'decimals' => 2],
-					],
-					'contentOptions' => [
-						2 => ['style' => 'text-align:right'],
-					],
-					'options' => ['style' => 'font-weight:bold;']
-				];
-			}
-		],	
-		[
-			'label' => 'Payment Method',
-			'value' => function ($data) {
-				return $data->paymentMethod->name;
-			},
-		],
-		[
-			'label' => 'Amount',
-			'value' => function ($data) use($searchModel){
-				$locationId = Yii::$app->session->get('location_id');
-				$amount = 0;
-				$payments = Payment::find()
-					->location($locationId) 
-					->andWhere([
-						'payment_method_id' => $data->payment_method_id,
-						'DATE(payment.date)' => (new \DateTime($data->date))->format('Y-m-d')
-					])
-  					->all();
-  				foreach ($payments as $payment) {
-					$amount += $payment->amount;	
-				}
-					
-				return $amount;
-			},
-			'contentOptions' => ['class' => 'text-right'],
-			'hAlign' => 'right',
-			'pageSummary' => true,
-			'pageSummaryFunc' => GridView::F_SUM
-		],	
-	]; ?>
-	<?php else : ?>
-	<?php
-	$columns = [
-			[
-			'value' => function ($data) {
-				if (!empty($data->date)) {
-					$lessonDate = \DateTime::createFromFormat('Y-m-d H:i:s', $data->date);
-					return $lessonDate->format('l, F jS, Y');
-				}
-
-				return null;
-			},
-			'group' => true,
-			'groupedRow' => true,
-			'groupOddCssClass'=>'kv-grouped-row',  
-            'groupEvenCssClass'=>'kv-grouped-row',
-			'groupFooter' => function ($model, $key, $index, $widget) {
-				return [
-					'mergeColumns' => [[1, 3]],
-					'content' => [
-						5 => GridView::F_SUM,
-					],
-					'contentFormats' => [
-						5 => ['format' => 'number', 'decimals' => 2],
-					],
-					'contentOptions' => [
-						5 => ['style' => 'text-align:right'],
-					],
-					'options' => ['style' => 'font-weight:bold;']
-				];
-			}
-		],
-			[
-			'label' => 'Payment Method',
-			'value' => function ($data) {
-				return $data->paymentMethod->name;
-			},
-				'group' => true,
-				 'subGroupOf'=>0 ,
-				'groupFooter'=>function ($model, $key, $index, $widget) { 
-                return [
-                    'mergeColumns'=>[[1, 4]], 
-                    'content'=>[              
-                        5=>GridView::F_SUM,
-                    ],
-                    'contentFormats'=>[     
-                        5 =>['format'=>'number', 'decimals'=>2],
-                    ],
-                    'contentOptions'=>[    
-                        5=>['style'=>'text-align:right'],
-                    ],
-                    'options'=>['class'=>'success','style'=>'font-weight:bold;']
-                ];
-            },
-		],
-			[
-			'label' => 'ID',
-			'value' => function($data) {
-				return $data->invoicePayment->invoice->getInvoiceNumber();
-			}
-		],
-			[
-			'label' => 'Customer',
-			'value' => function ($data) {
-				return !empty($data->user->publicIdentity) ? $data->user->publicIdentity : null;
-			},
-		],
-			[
-			'label' => 'Reference',
-			'value' => function ($data) {
-				if ((int) $data->payment_method_id === (int) PaymentMethod::TYPE_CREDIT_APPLIED || (int) $data->payment_method_id === (int) PaymentMethod::TYPE_CREDIT_USED) {
-					$invoiceNumber = str_pad($data->reference, 5, 0, STR_PAD_LEFT);
-					$invoicePayment = InvoicePayment::findOne(['payment_id' => $data->id]);
-					if ((int) $invoicePayment->invoice->type === Invoice::TYPE_INVOICE) {
-						return 'I - ' . $invoiceNumber;
-					} else {
-						return 'P - ' . $invoiceNumber;
+	<?php if ($searchModel->groupByMethod) : ?>
+		<?php
+		$columns = [
+				[
+				'value' => function ($data) {
+					if (!empty($data->date)) {
+						$lessonDate = \DateTime::createFromFormat('Y-m-d H:i:s', $data->date);
+						return $lessonDate->format('l, F jS, Y');
 					}
-				} else {
-					return $data->reference;
+
+					return null;
+				},
+				'group' => true,
+				'groupedRow' => true,
+				'groupFooter' => function ($model, $key, $index, $widget) {
+					return [
+						'mergeColumns' => [[1]],
+						'content' => [
+							2 => GridView::F_SUM,
+						],
+						'contentFormats' => [
+							2 => ['format' => 'number', 'decimals' => 2],
+						],
+						'contentOptions' => [
+							2 => ['style' => 'text-align:right'],
+						],
+						'options' => ['style' => 'font-weight:bold;']
+					];
 				}
-			},
-		],
-			[
-			'label' => 'Amount',
-			'value' => function ($data) {
-				return $data->amount;
-			},
-			'contentOptions' => ['class' => 'text-right'],
-			'hAlign' => 'right',
-			'pageSummary' => true,
-			'pageSummaryFunc' => GridView::F_SUM
-		],
-	];
-	?>
+			],
+				[
+				'label' => 'Payment Method',
+				'value' => function ($data) {
+					return $data->paymentMethod->name;
+				},
+			],
+				[
+				'label' => 'Amount',
+				'value' => function ($data) use($searchModel) {
+					$locationId = Yii::$app->session->get('location_id');
+					$amount = 0;
+					$payments = Payment::find()
+						->location($locationId)
+						->andWhere([
+							'payment_method_id' => $data->payment_method_id,
+							'DATE(payment.date)' => (new \DateTime($data->date))->format('Y-m-d')
+						])
+						->all();
+					foreach ($payments as $payment) {
+						$amount += $payment->amount;
+					}
+
+					return $amount;
+				},
+				'contentOptions' => ['class' => 'text-right'],
+				'hAlign' => 'right',
+				'pageSummary' => true,
+				'pageSummaryFunc' => GridView::F_SUM
+			],
+		];
+		?>
+	<?php else : ?>
+		<?php
+		$columns = [
+				[
+				'value' => function ($data) {
+					if (!empty($data->date)) {
+						$lessonDate = \DateTime::createFromFormat('Y-m-d H:i:s', $data->date);
+						return $lessonDate->format('l, F jS, Y');
+					}
+
+					return null;
+				},
+				'group' => true,
+				'groupedRow' => true,
+				'groupOddCssClass' => 'kv-grouped-row',
+				'groupEvenCssClass' => 'kv-grouped-row',
+				'groupFooter' => function ($model, $key, $index, $widget) {
+					return [
+						'mergeColumns' => [[1, 3]],
+						'content' => [
+							5 => GridView::F_SUM,
+						],
+						'contentFormats' => [
+							5 => ['format' => 'number', 'decimals' => 2],
+						],
+						'contentOptions' => [
+							5 => ['style' => 'text-align:right'],
+						],
+						'options' => ['style' => 'font-weight:bold;']
+					];
+				}
+			],
+				[
+				'label' => 'Payment Method',
+				'value' => function ($data) {
+					return $data->paymentMethod->name;
+				},
+				'group' => true,
+				'subGroupOf' => 0,
+				'groupFooter' => function ($model, $key, $index, $widget) {
+					return [
+						'mergeColumns' => [[1, 4]],
+						'content' => [
+							5 => GridView::F_SUM,
+						],
+						'contentFormats' => [
+							5 => ['format' => 'number', 'decimals' => 2],
+						],
+						'contentOptions' => [
+							5 => ['style' => 'text-align:right'],
+						],
+						'options' => ['class' => 'success', 'style' => 'font-weight:bold;']
+					];
+				},
+			],
+				[
+				'label' => 'ID',
+				'value' => function($data) {
+					return $data->invoicePayment->invoice->getInvoiceNumber();
+				}
+			],
+				[
+				'label' => 'Customer',
+				'value' => function ($data) {
+					return !empty($data->user->publicIdentity) ? $data->user->publicIdentity : null;
+				},
+			],
+				[
+				'label' => 'Reference',
+				'value' => function ($data) {
+					if ((int) $data->payment_method_id === (int) PaymentMethod::TYPE_CREDIT_APPLIED || (int) $data->payment_method_id === (int) PaymentMethod::TYPE_CREDIT_USED) {
+						$invoiceNumber = str_pad($data->reference, 5, 0, STR_PAD_LEFT);
+						$invoicePayment = InvoicePayment::findOne(['payment_id' => $data->id]);
+						if ((int) $invoicePayment->invoice->type === Invoice::TYPE_INVOICE) {
+							return 'I - ' . $invoiceNumber;
+						} else {
+							return 'P - ' . $invoiceNumber;
+						}
+					} else {
+						return $data->reference;
+					}
+				},
+			],
+				[
+				'label' => 'Amount',
+				'value' => function ($data) {
+					return $data->amount;
+				},
+				'contentOptions' => ['class' => 'text-right'],
+				'hAlign' => 'right',
+				'pageSummary' => true,
+				'pageSummaryFunc' => GridView::F_SUM
+			],
+		];
+		?>
 	<?php endif; ?>
 
 	<?=
