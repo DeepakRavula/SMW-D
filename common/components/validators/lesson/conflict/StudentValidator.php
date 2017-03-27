8322b6a67e6ca7b02aed751265f76b6b93a91039
SenG <senguttuvang@gmail.com>
2017-03-27 15:34:29 +0530

    fixes lesson conflict logic in new enrolment.

--- common/components/validators/lesson/conflict/StudentValidator.php
+++ common/components/validators/lesson/conflict/StudentValidator.php
@@ -35,10 +35,16 @@ class StudentValidator extends Validator
 		}
 		foreach ($otherLessons as $otherLesson) {
             $timebits = explode(':', $otherLesson['duration']);
-            $intervals[] = new DateRangeInclusive(new \DateTime($otherLesson['date']), new \DateTime($otherLesson['date']), new \DateInterval('PT'.$timebits[0].'H'.$timebits[1].'M'), $otherLesson['id']);
+			$endDate = new \DateTime($otherLesson['date']);
+			$endDate->add(new \DateInterval('PT'.$timebits[0].'H'.$timebits[1].'M'));
+            $intervals[] = new DateRangeInclusive(new \DateTime($otherLesson['date']),$endDate,null, $otherLesson['id']);
         }
 		$tree = new IntervalTree($intervals);
-        $conflictedLessonsResults = $tree->search(new \DateTime($model->$attribute));
+		$duration = explode(':', $model->duration);
+		$toDate = new \DateTime($model->date);
+		$toDate->add(new \DateInterval('PT'.$duration[0].'H'.$duration[1].'M'));
+		$searchRange = new DateRangeInclusive(new \DateTime($model->date), $toDate);
+        $conflictedLessonsResults = $tree->search($searchRange);
 		
         if ((!empty($conflictedLessonsResults))) {
             $this->addError($model,$attribute, 'Lesson time conflicts with student\'s another lesson');
