a5ff1cc1b61facf0789fd1aa0b2ae8fb9ea2321a
SenG <senguttuvang@gmail.com>
2017-03-27 22:01:24 +0530

    adds location information in all print pages.

--- backend/views/student/exam-result/_print.php
+++ backend/views/student/exam-result/_print.php
@@ -2,10 +2,30 @@
 
 use yii\grid\GridView;
 ?>
-<div>
-	<h3 class="m-0"> <?= $studentModel->fullName; ?> </h3>
+<div class="row-fluid">
+	<div class="logo invoice-col" style="width: 150px">              
+		<img class="login-logo-img" src="<?= Yii::$app->request->baseUrl ?>/img/logo.png"  />        
+	</div>
+	<div class="invoice-col text-gray" style="font-size:18px; width: 180px;">
+		<small>
+			<?php if (!empty($studentModel->customer->userLocation->location->address)): ?>
+				<?= $studentModel->customer->userLocation->location->address ?><br>
+			<?php endif; ?>
+			<?php if (!empty($studentModel->customer->userLocation->location->phone_number)): ?>
+				<?= $studentModel->customer->userLocation->location->phone_number ?>
+			<?php endif; ?>
+			<?php if (!empty($studentModel->customer->userLocation->location->email)): ?>
+				<?= $studentModel->customer->userLocation->location->email ?>
+			<?php endif; ?> 
+		</small> 
+	</div>
+	<div class="invoice-col" style="width: 220px;">
+		<strong>
+			<?php echo isset($studentModel->fullName) ? $studentModel->fullName : null ?>
+		</strong>
+	</div>
+	<div class="clearfix"></div>
 </div>
-<div class="clearfix"></div>
 <div>
 	<?php
 	echo GridView::widget([
