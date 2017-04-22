ab7f8e2d80e5986016b6312b38a9a0ff27dc498b
SenG <senguttuvang@gmail.com>
2017-04-22 23:40:18 +0530

    refactors lesson review page.

---
+++ backend/views/lesson/review/button.php
@@ -0,0 +1,61 @@
+<?php 
+use common\models\Program;
+use yii\helpers\Html;
+
+?>
+<div class="form-group">
+	<div class="p-10 text-center">
+		<?php if (!empty($vacationId)) : ?>
+			<?=
+			Html::a('Confirm', ['confirm', 'courseId' => $courseId, 'Vacation[id]' => $vacationId, 'Vacation[type]' => $vacationType], [
+				'class' => 'btn btn-danger',
+				'id' => 'confirm-button',
+				'disabled' => $hasConflict,
+				'data' => [
+					'method' => 'post',
+				],
+			])
+			?>
+		<?php elseif (!empty($rescheduleBeginDate)): ?>
+			<?=
+			Html::a('Confirm', ['confirm', 'courseId' => $courseId, 'Course[rescheduleBeginDate]' => $rescheduleBeginDate], [
+				'class' => 'btn btn-danger',
+				'id' => 'confirm-button',
+				'disabled' => $hasConflict,
+				'data' => [
+					'method' => 'post',
+				],
+			])
+			?>
+		<?php elseif (!empty($endDate)): ?>
+			<?=
+			Html::a('Confirm', ['confirm', 'courseId' => $courseId, 'Enrolment[endDate]' => $endDate, 'Enrolment[type]' => $enrolmentEditType], [
+				'class' => 'btn btn-danger',
+				'id' => 'confirm-button',
+				'disabled' => $hasConflict,
+				'data' => [
+					'method' => 'post',
+				],
+			])
+			?>
+		<?php else : ?>
+			<?=
+			Html::a('Confirm', ['confirm', 'courseId' => $courseId], [
+				'class' => 'btn btn-danger',
+				'id' => 'confirm-button',
+				'disabled' => $hasConflict,
+				'data' => [
+					'method' => 'post',
+				],
+			])
+			?>
+		<?php endif; ?>
+		<?php if ((int) $courseModel->program->type === Program::TYPE_PRIVATE_PROGRAM) : ?>
+			<?= Html::a('Cancel', ['student/view', 'id' => $courseModel->enrolment->studentId], ['class' => 'btn']);
+			?>
+		<?php else : ?>
+			<?= Html::a('Cancel', ['course/index'], ['class' => 'btn']);
+			?>
+		<?php endif; ?>
+	</div>
+</div>
