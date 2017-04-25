81566dda9ebeb44a82f3815fa1b140c9be583438
SenG <senguttuvang@gmail.com>
2017-04-25 20:55:00 +0530

    adds pfi,paid column in payment cycle listing.

--- backend/views/enrolment/_payment-cycle.php
+++ backend/views/enrolment/_payment-cycle.php
@@ -5,16 +5,37 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
 
-<h4><strong><?= 'Payment Cycle' ?> </strong></h4> 
     <?php echo GridView::widget([
         'dataProvider' => $paymentCycleDataProvider,
         'tableOptions' => ['class' => 'table table-bordered'],
         'headerRowOptions' => ['class' => 'bg-light-gray'],
         'summary' => '',
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
             'startDate:date',
             'endDate:date',
+			[
+				'label' => 'Pro-Forma Invoice',
+				'value' => function($data) {
+					$invoiceNumber = '-';
+					if($data->hasProformaInvoice()) {
+						$paymentCycleLesson = current($data->paymentCycleLessons);
+						$invoiceNumber = $paymentCycleLesson->proFormaInvoice->getInvoiceNumber();
+					}
+					return $invoiceNumber; 
+				}
+			],
+			[
+				'label' => 'Paid?',
+				'value' => function($data) {
+					$result = 'No';
+					$paymentCycleLesson = current($data->paymentCycleLessons);
+					if(!empty($paymentCycleLesson->proFormaInvoice) && $paymentCycleLesson->proFormaInvoice->isPaid()) {
+						$result = 'Yes';
+					}	
+					return $result;
+				}
+				
+			],
             ['class' => 'yii\grid\ActionColumn',
                 'template' => '{view} {create}',
                 'buttons' => [
