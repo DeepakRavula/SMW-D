8286adce8fd9fd2742cc1318eb6a2cb87bfc2948
SenG <senguttuvang@gmail.com>
2017-04-28 12:38:58 +0530

    fixes vacation create and delete conflict.

--- common/components/validators/lesson/conflict/TeacherValidator.php
+++ common/components/validators/lesson/conflict/TeacherValidator.php
@@ -23,8 +23,19 @@ class TeacherValidator extends Validator
 			->andWhere(['NOT', ['lesson.id' => $model->id]])
 			->overlap($lessonDate, $lessonStartTime, $lessonEndTime)
             ->all();
-        if ((!empty($teacherLessons))) {
+        if ((!empty($teacherLessons)) && empty($model->vacationId)) {
             $this->addError($model,$attribute, 'Teacher occupied with another lesson');
+		}
+		if(!empty($model->vacationId) && !empty($teacherLessons)) {
+			foreach($teacherLessons as $teacherLesson) {
+				if(new \DateTime($model->date) == new \DateTime($teacherLesson->date) && (int) $teacherLesson->status === Lesson::STATUS_SCHEDULED) {
+					continue;
+				}
+				$conflictedLessonIds[] = $model->id; 
+			}	
+			if(!empty($conflictedLessonIds)) {
+				$this->addError($model,$attribute, 'Teacher occupied with another lesson');
+			}
 		}
 		$day = (new \DateTime($model->date))->format('N');
         $teacherAvailabilityDay = TeacherAvailability::find()
