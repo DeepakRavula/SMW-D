f954b8e5a58bf1c545980966d0f939b3a23651e0
SenG <senguttuvang@gmail.com>
2017-05-15 23:08:26 +0530

    fixes date comparision.

--- backend/views/lesson/mail/body.php
+++ backend/views/lesson/mail/body.php
@@ -17,7 +17,7 @@ $lessonTime = Yii::$app->formatter->asTime($model->date);
 $lessonDate = (new \DateTime($model->date))->format('l, F jS, Y');
 $newTeacherName = $model->teacher->publicIdentity;
 ?>
-<?php if (!empty($model->date) && new \DateTime($model->date) !== new \DateTime($lesson->date)) : ?>
+<?php if (!empty($model->date) && new \DateTime($model->date) != new \DateTime($lesson->date)) : ?>
     <?= $studentName . '\'s ' . $programName . ' lesson with ' . $teacherName . ' on ' . $oldLessonDate . ' @ ' . $oldLessonTime . ' for ' . $oldLessonDuration . ' minutes has been rescheduled to ' . $lessonDate . ' @ ' . $lessonTime . ' for ' . $lessonDuration . ' minutes.'; ?>
 <?php elseif ((int)$model->teacherId !== (int)$lesson->teacherId) : ?>
     <?= $studentName . '\'s ' . $programName . ' lesson with ' . $teacherName . ' on ' . $oldLessonDate . ' @ ' . $oldLessonTime . ' for ' . $oldLessonDuration . ' minutes has been rescheduled to ' . $lessonDate . ' @ ' . $lessonTime . ' for ' . $lessonDuration . ' minutes ' . $newTeacherName; ?>
