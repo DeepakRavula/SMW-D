360fccb5ac74491332a11bbf970cfc79365a25df
SenG <senguttuvang@gmail.com>
2017-05-19 12:23:58 +0530

    fixes schedule tooltip position.

--- backend/views/schedule/group-lesson-description.php
+++ backend/views/schedule/group-lesson-description.php
@@ -5,7 +5,12 @@
 	li {
 	  list-style-type: none;
 	}
+	li label {
+		font-family: monospace;
+		font-size: 14px;
+		font-weight: bold;
+	}
 </style>
-<li><label>Teacher : </label><span><?= $lesson->teacher->publicIdentity; ?></span></li>
-<li><label>Program : </label><span><?= $lesson->course->program->name; ?></span></li>
-<li><label>Student Count : </label><span><?= $lesson->course->getEnrolmentsCount(); ?></span></li>
+<li><label>Teacher: </label><span><?= $lesson->teacher->publicIdentity; ?></span></li>
+<li><label>Program: </label><span><?= $lesson->course->program->name; ?></span></li>
+<li><label>Student Count: </label><span><?= $lesson->course->getEnrolmentsCount(); ?></span></li>
