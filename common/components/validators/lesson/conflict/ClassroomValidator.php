ed91fb93c7b53d8068ca8c8c437a2bec7bd1999c
seng <senguttuvang@gmail.com>
2017-05-23 12:29:14 +0530

    fixes schedule classroom edit validation

--- common/components/validators/lesson/conflict/ClassroomValidator.php
+++ common/components/validators/lesson/conflict/ClassroomValidator.php
@@ -11,16 +11,31 @@ class ClassroomValidator extends Validator
     public function validateAttribute($model, $attribute)
     {
         $lessonDate = (new \DateTime($model->date))->format('Y-m-d');
+        $start = new \DateTime($model->date);
+        $lessonDate = (new \DateTime($model->date))->format('Y-m-d');
+        $lessonStartTime = $start->format('H:i:s');
+        $duration = (new \DateTime($model->duration));
+        $start->add(new \DateInterval('PT' . $duration->format('H') . 'H' . $duration->format('i') . 'M'));
+        $start->modify('-1 second');
+        $lessonEndTime = $start->format('H:i:s');
+        $overLapLessons = Lesson::find()
+                ->andWhere(['NOT', ['lesson.id' => $model->id]])
+                ->andWhere(['classroomId' => $model->classroomId])
+                ->overlap($lessonDate, $lessonStartTime, $lessonEndTime)
+                ->all();
+        if (!empty($overLapLessons)) {
+            $this->addError($model, $attribute, 'Classroom already chosen!');
+        }
 
-		$classroomUnavailabilities = ClassroomUnavailability::find()
-			->andWhere(['classroomId' => $model->classroomId])
-			->andWhere(['AND',
-				['<=', 'DATE(fromDate)', $lessonDate],
-				['>=', 'DATE(toDate)', $lessonDate]
-			])
-			->all();
-		if(!empty($classroomUnavailabilities)) {
+        $classroomUnavailabilities = ClassroomUnavailability::find()
+                ->andWhere(['classroomId' => $model->classroomId])
+                ->andWhere(['AND',
+                        ['<=', 'DATE(fromDate)', $lessonDate],
+                        ['>=', 'DATE(toDate)', $lessonDate]
+                ])
+                ->all();
+        if(!empty($classroomUnavailabilities)) {
             $this->addError($model, $attribute, 'Classroom is unavailable');
-		}
+        }
     }
 }
