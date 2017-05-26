bec121309435b43c224486990af121ec3b6d68f5
SenG <senguttuvang@gmail.com>
2017-05-26 10:27:17 +0530

    small change in message

--- common/models/TimelineEventPayment.php
+++ common/models/TimelineEventPayment.php
@@ -130,7 +130,7 @@ class TimelineEventPayment extends \yii\db\ActiveRecord
         $payment = Payment::find(['id' => $paymentModel->id])->asArray()->one();
         $timelineEvent = Yii::$app->commandBus->handle(new AddToTimelineCommand([
             'data' => $payment,
-           'message' => $paymentModel->userName . ' changed  '.$paymentModel->paymentMethod->name.'  payment  amount  from  '.Yii::$app->formatter->asCurrency($data['amount']) . ' to ' . Yii::$app->formatter->asCurrency($paymentModel->amount).'  on    {{invoice #' . $paymentModel->invoice->getInvoiceNumber() . '}}',
+           'message' => $paymentModel->userName . ' changed  '.$paymentModel->paymentMethod->name.'  payment  amount  from  '.Yii::$app->formatter->asCurrency($data['amount']) . ' to ' . Yii::$app->formatter->asCurrency($paymentModel->amount).'  for an  {{invoice #' . $paymentModel->invoice->getInvoiceNumber() . '}}',
         ]));
         if ($timelineEvent) {
             $timelineEventLink = new TimelineEventLink();
