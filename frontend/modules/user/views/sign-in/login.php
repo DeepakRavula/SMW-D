b41193d4880aff0f3943a0e86d95ca1f8088e731
SenG <senguttuvang@gmail.com>
2017-06-15 15:47:16 +0530

    fixes reset password ui.

--- frontend/modules/user/views/sign-in/login.php
+++ frontend/modules/user/views/sign-in/login.php
@@ -9,24 +9,35 @@ use yii\widgets\ActiveForm;
 $this->title = Yii::t('frontend', 'Login');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
+<div class="login-box">
+	 <div class="login-logo">
+        <a href="<?php echo Yii::getAlias('@frontendUrl') ?>" class="logo">
+                <!-- Add the class icon to your logo image or logo icon to add the margining -->                
+                <img class="login-logo-img" src="<?= Yii::$app->request->baseUrl ?>/img/logo.png"  />        
+            </a>
+        <?php //echo Html::encode($this->title)?>
+    </div><!-- /.login-logo -->
+    <div class="header"></div>
+	<div class="login-box-body">
+        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+        <div class="body">
+            <?php echo $form->field($model, 'identity') ?>
+            <?php echo $form->field($model, 'password')->passwordInput() ?>
+            <?php echo $form->field($model, 'rememberMe')->checkbox(['class' => 'simple']) ?>
+        </div>
+        <div class="footer">
+            <?php echo Html::submitButton(Yii::t('backend', 'Sign me in'), [
+                'class' => 'btn btn-primary btn-flat btn-block',
+                'name' => 'login-button',
+            ]) ?>
+        </div>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?php echo $form->field($model, 'identity') ?>
-                <?php echo $form->field($model, 'password')->passwordInput() ?>
-                <?php echo $form->field($model, 'rememberMe')->checkbox() ?>
-			<div class="m-t-10">
+        <div class="m-t-10 text-right">
             <?php echo Yii::t('frontend', '<a href="{link}">Forgot your password?</a>', [
                 'link' => yii\helpers\Url::to(['sign-in/request-password-reset']),
             ]) ?>
         </div>
-                <div class="form-group">
-                    <?php echo Html::submitButton(Yii::t('frontend', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-                
-            <?php ActiveForm::end(); ?>
-        </div>
+        <?php ActiveForm::end(); ?>
     </div>
+
 </div>
