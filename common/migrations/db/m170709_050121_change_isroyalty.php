72c770cc3f2a838c5a477f2c5b42334416af218d
SenG <senguttuvang@gmail.com>
2017-07-09 11:44:00 +0530

    migration for change isroyalty field.

---
+++ common/migrations/db/m170709_050121_change_isroyalty.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Migration;
+
+class m170709_050121_change_isroyalty extends Migration
+{
+    public function up()
+    {
+		$this->renameColumn('invoice_line_item', 'isRoyalty', 'royaltyFree');
+    }
+
+    public function down()
+    {
+        echo "m170709_050121_change_isroyalty cannot be reverted.\n";
+
+        return false;
+    }
+}
