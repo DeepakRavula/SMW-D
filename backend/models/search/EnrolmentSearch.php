953c1ad9555a172106d031ea57d29e50a51bfd87
SenG <senguttuvang@gmail.com>
2017-07-17 12:06:13 +0530

    changing WHERE into Where

--- backend/models/search/EnrolmentSearch.php
+++ backend/models/search/EnrolmentSearch.php
@@ -86,11 +86,11 @@ class EnrolmentSearch extends Enrolment
             ]
         ]);
          
-$query->andFilterWHERE(['like','p.name',$this->program]);
- $query->andFilterWHERE(['like','student.first_name',$this->student]);
- $query->orFilterWHERE(['like','student.last_name',$this->student]);
- $query->andFilterWHERE(['like','up.firstname',$this->teacher]);
- $query->orFilterWHERE(['like','up.lastname',$this->teacher]);
+$query->andFilterWhere(['like','p.name',$this->program]);
+ $query->andFilterWhere(['like','student.first_name',$this->student]);
+ $query->orFilterWhere(['like','student.last_name',$this->student]);
+ $query->andFilterWhere(['like','up.firstname',$this->teacher]);
+ $query->orFilterWhere(['like','up.lastname',$this->teacher]);
  if($this->expirydate)
  {
  $query->andFilterWhere(['DATE(course.endDate)' =>(new \DateTime($this->expirydate))->format('Y-m-d')]);
