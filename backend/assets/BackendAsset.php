d05b2cd2eeb31856f707bd119c307450f04ab98c
SenG <senguttuvang@gmail.com>
2017-07-18 12:11:56 +0530

    removes commented code.

--- backend/assets/BackendAsset.php
+++ backend/assets/BackendAsset.php
@@ -20,18 +20,12 @@ class BackendAsset extends AssetBundle
         'css/style.css',
         'plugins/jquery-timepicker/jquery.timepicker.css',
         'plugins/jquery-multiselect/style.css',
-//		'plugins/fullcalendar-scheduler/lib/fullcalendar.print.min.css',
-//		'plugins/fullcalendar-scheduler/scheduler.css',
-//		'plugins/bootstrap-datepicker/bootstrap-datepicker.css',
     ];
     public $js = [
         'plugins/momentjs/moment-with-locales.js',
         'plugins/jquery-timepicker/jquery.timepicker.js',
         'plugins/jquery-multiselect/multiselect.js',
         'plugins/jquery-multiselect/multiselect.min.js',
-//		'plugins/fullcalendar-scheduler/lib/fullcalendar.min.js',
-//		'plugins/fullcalendar-scheduler/scheduler.js',
-//		'plugins/bootstrap-datepicker/bootstrap-datepicker.js'	
     ];
 
     public $depends = [
