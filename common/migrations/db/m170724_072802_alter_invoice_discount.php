4f34e7279b090ff00f1784aabe8b140b098abcb7
seng <senguttuvang@gmail.com>
2017-07-24 15:13:32 +0530

    rename column name

--- common/migrations/db/m170724_072802_alter_invoice_discount.php
+++ common/migrations/db/m170724_072802_alter_invoice_discount.php
@@ -7,6 +7,7 @@ class m170724_072802_alter_invoice_discount extends Migration
     public function up()
     {
         $this->renameTable('invoice_discount', 'invoice_line_item_discount');
+        $this->renameColumn('invoice_line_item_discount', 'invoiceId', 'invoiceLineItemId');
     }
 
     public function down()
