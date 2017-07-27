c90e40029486525902ce85b961449b81e25499bf
SenG <senguttuvang@gmail.com>
2017-07-27 11:21:30 +0530

    adds error log in course.

--- backend/controllers/EnrolmentController.php
+++ backend/controllers/EnrolmentController.php
@@ -268,6 +268,8 @@ class EnrolmentController extends Controller
 						Yii::error('New enrolment payment frequency discount: ' . \yii\helpers\VarDumper::dumpAsString($paymentFrequencyDiscount->getErrors()));	
 					}
 				}
+			} else {
+				Yii::error('New enrolment Course: ' . \yii\helpers\VarDumper::dumpAsString($course->getErrors()));
 			}
 			return $this->redirect(['lesson/review', 'courseId' => $course->id, 'LessonSearch[showAllReviewLessons]' => false, 'Enrolment[type]' => Enrolment::TYPE_REVERSE]);
 		} else {
