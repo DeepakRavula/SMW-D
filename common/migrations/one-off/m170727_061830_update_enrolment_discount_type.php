a0637c4f0712459a68becd61980c863f0db0cf2e
seng <senguttuvang@gmail.com>
2017-07-27 11:55:22 +0530

    migration for enrolment discount's discountType field fix

---
+++ common/migrations/one-off/m170727_061830_update_enrolment_discount_type.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+use common\models\EnrolmentDiscount;
+
+class m170727_061830_update_enrolment_discount_type extends Migration
+{
+    public function up()
+    {
+        $paymentFrequencyDiscounts = EnrolmentDiscount::find()
+            ->where(['type' => EnrolmentDiscount::TYPE_PAYMENT_FREQUENCY, 'discountType' => true])
+            ->all();
+        foreach ($paymentFrequencyDiscounts as $paymentFrequencyDiscount) {
+            $paymentFrequencyDiscount->updateAttributes([
+                'discountType' => 0
+            ]);
+        }
+        $multiEnrolmentDiscounts = EnrolmentDiscount::find()
+            ->where(['type' => EnrolmentDiscount::TYPE_MULTIPLE_ENROLMENT, 'discountType' => 0])
+            ->all();
+        foreach ($multiEnrolmentDiscounts as $multiEnrolmentDiscount) {
+            $multiEnrolmentDiscount->updateAttributes([
+                'discountType' => true
+            ]);
+        }
+    }
+
+    public function down()
+    {
+        echo "m170727_061830_update_enrolment_discount_type cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
