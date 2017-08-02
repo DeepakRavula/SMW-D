66a06f0db98b502b04a1a3cb9224bd1e30c49e63
SenG <senguttuvang@gmail.com>
2017-08-02 13:08:07 +0530

    activity-log-for-user-creation

--- backend/models/UserForm.php
+++ backend/models/UserForm.php
@@ -13,6 +13,7 @@ use yii\base\Model;
 use Yii;
 use yii\helpers\ArrayHelper;
 use common\models\Qualification;
+use common\models\timelineEvent\UserLog;
 /**
  * Create user form.
  */
@@ -228,7 +229,14 @@ class UserForm extends Model
             $userProfileModel->lastname = $lastname;
             $userProfileModel->firstname = $firstname;
             $userProfileModel->save();
-			
+			$loggedUser = User::findOne(['id' => Yii::$app->user->id]);
+            $userProfileModel->loggedUser = $loggedUser->publicIdentity;
+            $roles = Yii::$app->authManager->getRolesByUser($userProfileModel->user_id);
+            foreach ($roles as $name => $description) {
+                $role = $name;
+            }
+            $userProfileModel->on(UserProfile::EVENT_USER_CREATE, [new UserLog(), 'userCreate'], ['role' => $role]);
+            $userProfileModel->trigger(UserProfile::EVENT_USER_CREATE);
             return !$model->hasErrors();
         }
 
