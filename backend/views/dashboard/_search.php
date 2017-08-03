aa9b03047db249a66535ff0c25e58c0b5daa84ba
SenG <senguttuvang@gmail.com>
2017-08-03 12:03:09 +0530

    changing dateformat in dashboard

--- backend/views/dashboard/_search.php
+++ backend/views/dashboard/_search.php
@@ -32,16 +32,10 @@ use kartik\daterange\DateRangePicker;
             'initRangeExpr' => true,
             'pluginOptions' => [
                 'autoApply' => true,
-                'ranges' => [
-                    Yii::t('kvdrp', 'Last {n} Days', ['n' => 7]) => ["moment().startOf('day').subtract(6, 'days')", 'moment()'],
-                    Yii::t('kvdrp', 'Last {n} Days', ['n' => 30]) => ["moment().startOf('day').subtract(29, 'days')", 'moment()'],
-                    Yii::t('kvdrp', 'This Month') => ["moment().startOf('month')", "moment().endOf('month')"],
-                    Yii::t('kvdrp', 'Last Month') => ["moment().subtract(1, 'month').startOf('month')", "moment().subtract(1, 'month').endOf('month')"],
-                ],
                 'locale' => [
-                    'format' => 'd-m-Y',
+                    'format' => 'M d,Y',
                 ],
-                'opens' => 'left',
+                'opens' => 'bottom',
                 ],
 
             ]);
