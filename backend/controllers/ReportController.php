dfc26595a7d33907c541f0e28961a32cccebeadd
SenG <senguttuvang@gmail.com>
2017-08-08 13:14:15 +0530

    adding-daterange-in-itemcategory

--- backend/controllers/ReportController.php
+++ backend/controllers/ReportController.php
@@ -220,8 +220,9 @@ class ReportController extends Controller {
     {
         $searchModel                      = new InvoiceLineItemSearch();
         $searchModel->groupByItemCategory = true;
-        $searchModel->fromDate            = (new \DateTime())->format('d-m-Y');
-        $searchModel->toDate              = (new \DateTime())->format('d-m-Y');
+        $searchModel->fromDate            = (new \DateTime())->format('M d,Y');
+        $searchModel->toDate              = (new \DateTime())->format('M d,Y');
+        $searchModel->dateRange           = $searchModel->fromDate.' - '.$searchModel->toDate;
         $dataProvider                     = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('item-category/index',
