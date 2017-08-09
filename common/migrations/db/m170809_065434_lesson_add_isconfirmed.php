dcbc869e7f137fefacdc80f2bd53c9e8a61529bc
SenG <senguttuvang@gmail.com>
2017-08-09 12:52:53 +0530

    migration for add isConfirmed field in lesson table.

---
+++ common/migrations/db/m170809_065434_lesson_add_isconfirmed.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Migration;
+
+class m170809_065434_lesson_add_isconfirmed extends Migration
+{
+    public function up()
+    {
+		$this->addColumn('lesson', 'isConfirmed', 
+			$this->boolean()->after('isDeleted'));
+    }
+
+    public function down()
+    {
+        echo "m170809_065434_lesson_add_isconfirmed cannot be reverted.\n";
+
+        return false;
+    }
+}
