3ed96e041940feb962c46348c64ffff970713148
SenG <senguttuvang@gmail.com>
2017-08-20 20:46:00 +0530

    one off migration for root lesson mapping

---
+++ common/migrations/one-off/m170820_110736_root_lesson_mapping.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+use common\models\InvoiceItemPaymentCycleLesson;
+
+class m170820_110736_root_lesson_mapping extends Migration
+{
+    public function up()
+    {
+        $invoiceItems = InvoiceItemPaymentCycleLesson::find()
+                ->all();
+        foreach ($invoiceItems as $invoiceItem) {
+            $invoiceItem->paymentCycleLessonId = $invoiceItem->lesson->rootLesson
+                    ->paymentCycleLesson->id;
+            $invoiceItem->save();
+        }
+    }
+
+    public function down()
+    {
+        echo "m170820_110736_root_lesson_mapping cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
