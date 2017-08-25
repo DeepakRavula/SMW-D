5ad5b59e2723a880cba1f58c76a5680f3ed42792
SenG <senguttuvang@gmail.com>
2017-08-25 10:31:18 +0530

    adds lesson buttons.

--- backend/views/lesson/_attendance.php
+++ backend/views/lesson/_attendance.php
@@ -2,11 +2,33 @@
 
 use insolita\wgadminlte\LteBox;
 use insolita\wgadminlte\LteConst;
+use yii\widgets\ActiveForm;
+use kartik\switchinput\SwitchInput;
+
 ?>
+<?php if (!$model->isGroup()) : ?>
+	<?php $form = ActiveForm::begin(['id' => 'lesson-present-form']); ?>
+	<?php $model->present = $model->isMissed() ? false : true; 
+	$disabled = $model->isMissed() ? true : false;
+	?> 
+	<?php $boxTools = 
+	$form->field($model, 'present')->widget(SwitchInput::classname(), [
+		'name' => 'present',
+		'disabled' => $disabled,
+		'pluginOptions' => [
+			'handleWidth' => 61,
+			'onText' => 'Present',
+			'offText' => 'Absent',
+		],
+	])->label(false);
+	?>
+<?php ActiveForm::end(); ?>
+	<?php endif; ?>
 <?php
 LteBox::begin([
 	'type' => LteConst::TYPE_DEFAULT,
 	'title' => 'Attendance',
+	'boxTools' => $boxTools,
 	'withBorder' => true,
 ])
 ?>
