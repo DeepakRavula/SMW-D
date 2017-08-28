43afa672551c76bade6df211702e94ded274d0b3
SenG <senguttuvang@gmail.com>
2017-08-28 12:00:35 +0530

    invoice print change view

--- backend/views/invoice/print/view.php
+++ backend/views/invoice/print/view.php
@@ -12,128 +12,85 @@
    $this->params['breadcrumbs'][] = $this->title;
    ?>
     <div class="invoice-view">
-        <div class="row-fluid" >
-            <div class="logo invoice-col" style="width: 100%">
-                <img class="login-logo-img" src="<?= Yii::$app->request->baseUrl ?>/img/logo.png" />
-                <div class="invoice-status">
-                    <div class="invoice-col" style="width: 125px; text-align:right;">
-                        <p class="invoice-number" style="font-weight:700; font-size:16px;">
-                            <h3><strong><?= $model->getInvoiceNumber();?></strong></h3>
-                        </p>
-                        <p>
-                            <?= Yii::$app->formatter->asDate($model->date); ?>
-                        </p>
-                        <p>
-                            <?= $model->getStatus(); ?>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="invoice-col " style="clear: both;">
-                <div class="invoice-print-address">
-                    <ul>
-						<li><strong>Arcadia Music Academy ( <?= $model->location->name; ?> )</strong></li>
-                        <li>
-                            <?php if (!empty($model->location->address)): ?>
-                                <?= $model->location->address;?>
-                            <?php endif; ?>
-                        </li>
-						<li>
-                            <?php if (!empty($model->location->city_id)): ?>
-                                <?= $model->location->city->name;?>
-                            <?php endif; ?>
-							<?php if (!empty($model->location->province_id)): ?>
-                                <?= ', ' . $model->location->province->name;?>
-                            <?php endif; ?>
-						</li>
-						<li>
-							<?php if (!empty($model->location->postal_code)): ?>
-                                <?= $model->location->postal_code;?>
-                            <?php endif; ?>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            </br>
-                        </li>
-                        <li>
-                            <?php if (!empty($model->location->phone_number)): ?>
-                                <?= $model->location->phone_number?>
-                            <?php endif; ?>
-                        </li>
-                        <li>
-                            <?php if (!empty($model->location->email)): ?>
-                                <?= $model->location->email?>
-                            <?php endif; ?>
-                        </li>
-						<li>
-                           www.arcadiamusicacademy.com
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="invoice-col" style="clear:both; ">
-				<?php if(!empty($model->user)) : ?>
-                <div class="invoice-print-address">
-                    <ul>
-						<li>
-						<strong>Customer</strong>
-						</li>
-                        <li>
-                            <h1 class="m-0" style="font-size:14px;">
-                     <?php echo isset($model->user->publicIdentity) ? $model->user->publicIdentity : null?>
-                  </h1>
-                        </li>
-						<?php 
-						 $addresses = $model->user->addresses;
-                     foreach ($addresses as $address) {
-                     	if ($address->label === 'Billing') {
-                     		$billingAddress = $address;
-                     		break;
-                     	}
-                     }
-                     $phoneNumber = $model->user->phoneNumber;
-					 ?>
-                        <li>
-                            <!-- Billing address -->
-                            <?php if (!empty($billingAddress->address)) : ?>
-								<?= $billingAddress->address; ?>
-							<?php endif; ?>
-						</li>
-						<li>                          
-                            <?php if (!empty($billingAddress->city->name)) : ?>
-								<?= $billingAddress->city->name; ?>
-							<?php endif; ?>                          
-                            <?php if (!empty($billingAddress->province->name)) : ?>
-								<?= ', ' . $billingAddress->province->name; ?>
-							<?php endif; ?>
-						</li>
-						<li>                           
-                            <?php if (!empty($billingAddress->postal_code)) : ?>
-								<?= $billingAddress->postal_code; ?>
-							<?php endif; ?>
-						</li>
-                    </ul>
-                    <ul>
-                        <li>
-                            </br>
-                        </li>
-						<li>
-                            <!-- Phone number -->
-                            <?php if (!empty($phoneNumber)) : ?>
-                                <?php echo $phoneNumber->number; ?>
-							<?php endif; ?>
-                        </li>
-                        <li>
-                            <?php if (!empty($model->user->email)): ?>
-                                <?php echo $model->user->email?>
-                            <?php endif; ?>
-                        </li>   
-                    </ul>
-                </div>
-			<?php endif; ?>
-            </div>
-        </div>
+       <div class="row">
+      <div class="col-xs-12">
+        <h2 class="page-header">
+          <span class="logo-lg"><b>Arcadia</b>SMW</span>
+          <small class="pull-right"><?= Yii::$app->formatter->asDate($model->date); ?></small>
+        </h2>
+      </div>
+           <div class="row invoice-info">
+      <div class="col-sm-4 invoice-col">
+        From
+        <address>
+          <strong>Arcadia Music Academy ( <?= $model->location->name; ?> )</strong><br>
+          <?php if (!empty($model->location->address)): ?>
+              <?= $model->location->address; ?>
+          <?php endif; ?>
+          <br/>
+          <?php if (!empty($model->location->city_id)): ?>
+              <?= $model->location->city->name; ?>
+          <?php endif; ?>
+          <br/>
+          <?php if (!empty($model->location->postal_code)): ?>
+              <?= $model->location->postal_code; ?>
+          <?php endif; ?>
+          <br/>
+          <?php if (!empty($model->location->phone_number)): ?>
+              <?= $model->location->phone_number ?>
+          <?php endif; ?>
+          <br/>
+          <?php if (!empty($model->location->email)): ?>
+              <?= $model->location->email ?>
+          <?php endif; ?>
+          <br/>
+          www.arcadiamusicacademy.com
+        </address>
+      </div>
+      <!-- /.col -->
+      <div class="col-sm-4 invoice-col">
+        To
+        <?php if(!empty($model->user)) : ?>
+        <address>
+          <strong><?php echo isset($model->user->publicIdentity) ? $model->user->publicIdentity : null?></strong><br>
+          <?php
+          $addresses = $model->user->addresses;
+          foreach ($addresses as $address) {
+              if ($address->label === 'Billing') {
+                  $billingAddress = $address;
+                  break;
+              }
+          }
+          $phoneNumber = $model->user->phoneNumber;
+          ?>
+          <?php if (!empty($billingAddress->address)) : ?>
+              <?= $billingAddress->address; echo '<br/>'; ?>
+          <?php endif; ?>
+          <?php if (!empty($billingAddress->city->name)) : ?>
+              <?= $billingAddress->city->name; echo '<br/>'; ?>
+          <?php endif; ?>    
+          <?php if (!empty($billingAddress->postal_code)) : ?>
+              <?= $billingAddress->postal_code; echo '<br/>'; ?>
+          <?php endif; ?>
+          <?php if (!empty($phoneNumber)) : ?>
+              <?php echo $phoneNumber->number; echo '<br/>'; ?>
+          <?php endif; ?>
+          <?php if (!empty($model->user->email)): ?>
+              <?php echo $model->user->email; echo '<br/>'; ?>
+          <?php endif; ?>
+          <?php endif; ?>
+        </address>
+      </div>
+      <div class="col-sm-4 invoice-col">
+        <b><?= $model->getInvoiceNumber();?></b><br>
+        <br>
+        <b>Date:</b><?= Yii::$app->formatter->asDate($model->date); ?> <br>
+        <b>Status:</b>  <?= $model->getStatus(); ?><br>
+      </div>
+      <!-- /.col -->
+    </div>
+      <!-- /.col -->
+    </div>
         <div class="row-fluid invoice-info m-t-10">
             <?php yii\widgets\Pjax::begin(['id' => 'lesson-index']); ?>
                 <?php echo GridView::widget([
@@ -169,59 +126,52 @@
          ]); ?>
                     <?php yii\widgets\Pjax::end(); ?>
         </div>
-        <table class="table-responsive below-description " style="width:100%">
-            <tr>
-                <?php if (!empty($model->notes)):?>
-                    <td class="notes-table">
-                        <div class="row-fluid m-t-15 m-b-15">
-                            <em><strong> Notes: </strong><Br>
-               <?php echo $model->notes; ?></em>
-                        </div>
-                    </td>
-                    <?php endif; ?>
-                        <?php if (!empty($model->payments)) : ?>
-                            <td class="payment-method-table p-t-10">
-                               <?= $this->render('_payment', [
-								   'paymentsDataProvider' => $paymentsDataProvider
-							   ]);?> 
-                            </td>
-                            <?php endif; ?>
-                                <td rowspan="2" class="subtotal-table p-t-10">
-                                    <table class="table-invoice-childtable table-more-condensed" style="float:right; width:auto;">
-										<tr>
-											<td id="invoice-discount">Discounts</td>
-											<td><?= Yii::$app->formatter->format($model->totalDiscount, ['currency']); ?></td>
-										</tr>
-                                        <tr>
-                                            <td>SubTotal</td>
-                                            <td>
-                                                <?= Yii::$app->formatter->format($model->subTotal, ['currency']); ?>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Tax</td>
-                                            <td>
-                                                <?= Yii::$app->formatter->format($model->tax, ['currency']); ?>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td><strong>Total</strong></td>
-                                            <td><strong><?= Yii::$app->formatter->format($model->total, ['currency']); ?></strong></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Paid</td>
-                                            <td>
-                                                <?= Yii::$app->formatter->format($model->invoicePaymentTotal, ['currency']); ?>
-                                            </td>
-                                        </tr>
-                                        <tr class="last-balance">
-                                            <td class="p-t-0"><strong>Balance</strong></td>
-                                            <td class="p-t-0"><strong><?= Yii::$app->formatter->format($model->balance, ['currency']); ?></strong></td>
-                                        </tr>
-                                    </table>
-                                </td>
+        <div class="row">
+      <!-- accepted payments column -->
+      <div class="col-xs-6">
+        <p class="lead">Notes:</p>
+        <?php if (!empty($model->notes)):?>
+        <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+         <?php echo $model->notes; ?>
+        </p>
+        <?php endif; ?>
+      </div>
+      <!-- /.col -->
+      <div class="col-xs-6">
+          <?php if (!empty($model->dueDate)) : 
+              echo '<p class="lead">Due Date  '. Yii::$app->formatter->asDate($model->dueDate).'</p>';
+           endif; ?>
+          <div class="table-responsive">
+          <table class="table">
+            <tbody><tr>
+              <th style="width:50%">Discounts:</th>
+              <td><?= Yii::$app->formatter->format($model->totalDiscount, ['currency']); ?></td>
             </tr>
-        </table>
+            <tr>
+              <th>SubTotal</th>
+              <td><?= Yii::$app->formatter->format($model->subTotal, ['currency']); ?></td>
+            </tr>
+            <tr>
+              <th>Tax:</th>
+              <td><?= Yii::$app->formatter->format($model->tax, ['currency']); ?></td>
+            </tr>
+            <tr>
+              <th>Total:</th>
+              <td><?= Yii::$app->formatter->format($model->total, ['currency']); ?></td>
+            </tr>
+            <tr>
+              <th>Paid:</th>
+              <td><?= Yii::$app->formatter->format($model->invoicePaymentTotal, ['currency']); ?></td>
+            </tr>
+            <tr>
+              <th>Balance:</th>
+              <td><?= Yii::$app->formatter->format($model->balance, ['currency']); ?></td>
+            </tr>
+          </tbody></table>
+        </div>
+      </div>
+      <!-- /.col -->
+    </div>
         <!-- /.col -->
     </div>
     <div class="reminder_notes text-muted well well-sm no-shadow" style="clear:both; margin-top: 20px; position: relative;">
