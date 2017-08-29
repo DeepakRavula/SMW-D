fb8cdad1661b02556f98860858f63aac3285f8f0
SenG <senguttuvang@gmail.com>
2017-08-29 13:23:37 +0530

    timevoucher-teacher-print

--- backend/views/user/teacher/_print-time-voucher.php
+++ backend/views/user/teacher/_print-time-voucher.php
@@ -2,26 +2,83 @@
 
 use kartik\grid\GridView;
 ?>
-<?php $this->registerCssFile("@web/css/teacher/style.css");?>
-<div class="row-fluid print-container">
-	<div class="logo invoice-col">              
-		<img class="login-logo-img" src="<?= Yii::$app->request->baseUrl ?>/img/logo.png"  />        
-	</div>
-	<div class="location-address">
-			<p>Arcadia Music Academy ( <?= $model->userLocation->location->name;?> )</p>
-			<p><?php if (!empty($model->userLocation->location->address)): ?>
+ <div class="row">
+      <div class="col-md-12">
+        <h2 class="page-header">
+          <span class="logo-lg"><b>Arcadia</b>SMW</span>
+          <small class="pull-right"><?=  Yii::$app->formatter->asDate('now');?></small>
+        </h2>
+      </div>
+       </div>
+    <!-- info row -->
+    <div class="row invoice-info">
+      <div class="col-md-6 invoice-col">
+          <div class="invoice-print-address">
+        From
+        <address>
+         Arcadia Music Academy ( <?= $model->userLocation->location->name;?> )<br/>
+			<?php if (!empty($model->userLocation->location->address)): ?>
 				<?= $model->userLocation->location->address ?><br>
-			<?php endif; ?></p>
-			<p><?php if (!empty($model->userLocation->location->city_id)): ?>
+			<?php endif; ?>
+                <?php if (!empty($model->userLocation->location->city_id)): ?>
 				<?= $model->userLocation->location->city->name ?>
 			<?php endif; ?>
 			<?php if (!empty($model->userLocation->location->province_id)): ?>
-				<?= ', ' . $model->userLocation->location->province->name ?>
-			<?php endif; ?> </p>
-	</div>
-	<div class="clearfix"></div>
-</div>
-<h2 class="col-md-12"><b><?= $model->publicIdentity . '\'s Time Voucher for ' . $fromDate->format('F jS, Y') . ' to ' . $toDate->format('F jS, Y');?></b></h2>
+				<?= ', ' . $model->userLocation->location->province->name ?><br/>
+			<?php endif; ?>     
+                 <?php if (!empty($model->userLocation->location->postal_code)): ?>
+              <?= $model->userLocation->location->postal_code; ?>
+          <?php endif; ?>
+          <br/>
+          <?php if (!empty($model->userLocation->location->phone_number)): ?>
+              <?= $model->userLocation->location->phone_number ?>
+          <?php endif; ?>
+          <br/>
+          <?php if (!empty($model->email)): ?>
+              <?= $model->email ?>
+          <?php endif; ?>
+          <br/>
+          www.arcadiamusicacademy.com
+        </address>
+          </div>
+      </div>
+      <!-- /.col -->
+      <div class="col-md-6 invoice-col">
+          <div class="invoice-print-address">
+        Teacher
+        <address>
+           <?php $primaryAddress=$model->primaryAddress;
+                 $phoneNumber=$model->phoneNumber;   
+           ?>
+          <strong><?php echo $model->publicIdentity; ?></strong><br>
+          <?php if (!empty($primaryAddress->address)): ?>
+              <?php echo $primaryAddress->address; ?><br>
+          <?php endif; ?>
+           <?php if (!empty($primaryAddress->city->name)): ?>
+              <?php echo $primaryAddress->city->name; ?>,
+          <?php endif; ?>
+          <?php if (!empty($primaryAddress->province->name)): ?>
+              <?php echo $primaryAddress->province->name; ?><br>
+          <?php endif; ?>
+          <?php if (!empty($phoneNumber->number)): ?>
+               Phone:<?php echo $phoneNumber->number; ?><br>
+          <?php endif; ?> <br>
+            <?php if (!empty($model->email)): ?>
+            Email:  <?php echo $model->email; ?><br>
+          <?php endif; ?>
+        </address>
+          </div>
+      </div>
+      <!-- /.col -->
+     <div class="col-sm-4 invoice-col">
+        <b>Time Voucher</b><br>
+        <br>
+        <b><?php echo  $fromDate->format('F jS, Y') . ' to ' . $toDate->format('F jS, Y');?></b>
+      </div>
+      <!-- /.col -->
+    </div>
+    <div class="row">
+      <div class="col-xs-12 table-responsive">
 <div class="report-grid">
 <?php
 if(!$searchModel->summariseReport) {
@@ -152,6 +209,7 @@ $columns = [
 <?=
 GridView::widget([
 	'dataProvider' => $timeVoucherDataProvider,
+    'summary' => '',
 	'options' => ['class' => 'col-md-12'],
 	'tableOptions' => ['class' => 'table table-bordered'],
 	'headerRowOptions' => ['class' => 'bg-light-gray'],
@@ -167,6 +225,8 @@ GridView::widget([
 ]);
 ?>
 </div>
+      </div>
+    </div>
 <div class="boxed col-md-12 pull-right">
 <div class="sign">
  Teacher Signature <span></span>
