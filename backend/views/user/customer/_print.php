76eb3bb71779a6113ec06ecb1e85a01db233968b
SenG <senguttuvang@gmail.com>
2017-08-29 15:51:25 +0530

    printing invoice in customer tab

--- backend/views/user/customer/_print.php
+++ backend/views/user/customer/_print.php
@@ -13,44 +13,51 @@ $this->params['breadcrumbs'][] = ['label' => 'Invoices', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="invoice-view p-10">
-    <div class="row">
-        <div class="col-xs-12 p-0">
-          <h2 class="m-0 border-bottom-gray">
-            <a class="logo pull-left">
-                <!-- Add the class icon to your logo image or logo icon to add the margining -->                
-                <img class="login-logo-img" src="<?= Yii::$app->request->baseUrl ?>/img/logo.png"  />        
-            </a>
-          <div class="pull-left invoice-address  text-gray">
-	      <div class="row-fluid">
-              <h4 class="m-0 text-inverse"><strong>STATEMENT</strong></h4>
-          </div>
-          <small><?php
-		  $locationId = Yii::$app->session->get('location_id');
-		  $location = Location::findOne(['id' => $locationId]);
-		  if (!empty($location->address)): ?>
+<div class="row">
+      <div class="col-xs-12">
+        <h2 class="page-header">
+          <span class="logo-lg"><b>Arcadia</b>SMW</span>
+          <small class="pull-right"><?= Yii::$app->formatter->asDate('now'); ?></small>
+        </h2>
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- info row -->
+    <div class="row invoice-info">
+      <div class="col-sm-4 invoice-col">
+          <?php $locationId = Yii::$app->session->get('location_id');
+		  $location = Location::findOne(['id' => $locationId]);?>
+		  
+        From
+        <address>
+          <strong> <?php if (!empty($location->name)): ?>
+                <?= $location->name?>
+      <?php endif; ?></strong><br>
+         <?php if (!empty($location->address)): ?>
                 <?= $location->address?>
+      <?php endif; ?><br>
+         <?php if (!empty($location->city->name)): ?>
+                <?= $location->city->name?>,
+      <?php endif; ?><?php if (!empty($location->province->name)): ?>
+                <?= $location->province->name?>
+      <?php endif; ?><br>
+      <?php if (!empty($location->postal_code)): ?>
+                <?= $location->postal_code?>
+      <?php endif; ?><br>
+       <?php if (!empty($location->phone_number)): ?>
+              Phone:<?= $location->phone_number?>
+      <?php endif; ?><br>
+          <?php if (!empty($location->email)): ?>
+              E-mail:<?= $location->email?>
       <?php endif; ?>
-      <?php if (!empty($location->phone_number)): ?><br>
-            <?= $location->phone_number; ?>
-      <?php endif; ?> 
-      <?php if (!empty($location->email)): ?><br>
-            <?= $location->email; ?>
-      <?php endif; ?> 
-      </small> 
+        </address>
       </div>
-      <div class="clearfix"></div>
-          </h2>
-        </div>
-        <!-- /.col -->
-      </div>
-    <div class="row invoice-info m-t-10">
-        <!-- /.col -->
-        <div class="col-sm-9 invoice-col m-b-10 pull-left p-0">
-          <div class="row m-T-10">
-            <div class="col-xs-12">
-                <h4 class="m-0 f-w-400"><strong><?php echo isset($model->publicIdentity) ? $model->publicIdentity : null?></strong></h4>
-            <div class="text-gray">
-            <?php
+      <!-- /.col -->
+      <div class="col-sm-4 invoice-col">
+        To
+        <address>
+          <strong><?php echo isset($model->publicIdentity) ? $model->publicIdentity : null?></strong><br>
+          <?php
                 $addresses = $model->addresses;
                 foreach ($addresses as $address) {
                     if ($address->label === 'Billing') {
@@ -61,48 +68,40 @@ $this->params['breadcrumbs'][] = $this->title;
                 $phoneNumber = $model->phoneNumber;
 
                 ?>
-                <!-- Billing address -->
+          <!-- Billing address -->
                 <?php if (!empty($billingAddress)) {
                     ?>
                   <?php 
                         echo $billingAddress->address.'<br> '.$billingAddress->city->name.', ';
-                    echo $billingAddress->province->name.'<br>'.$billingAddress->country->name.' ';
-                    echo $billingAddress->postal_code;
+                    echo $billingAddress->province->name.'<br>';
+                    echo $billingAddress->postal_code.'<br/>';
                 } ?>
-                <div class="row-fluid m-t-5">
-                  <?php if (!empty($model->email)): ?>
-                  <?php echo 'E: '; ?><?php echo $model->email?>
-                  <?php endif; ?>
-                </div>
-              </div>
-              </div>
-            <!-- Phone number -->
-            <div class="row-fluid text-gray">
-              <?php if (!empty($phoneNumber)) {
-                    ?><?php echo 'P: '; ?>
-              <?php echo $phoneNumber->number;
-                } ?>
-            </div>
-            </div>
-          </div>
-		 <div class="col-sm-4 invoice-col m-t-10 text-right p-0">
-          <div class="row-fluid text-gray">
-              <div class="col-md-4 pull-right text-left p-r-0 p-l-30"><?= $dateRange; ?></div>
-              <div class="col-md-2 pull-right">Date:</div>
-              <div class="clearfix"></div>
-          </div>
-          </div> 
-        </div>
-        <!-- /.col -->
-     
-          <div class="clearfix"></div>
-		  <?php echo  GridView::widget([
-    'dataProvider' => $invoiceDataProvider,
-    'options' => ['class' => 'col-md-12'],
-    'tableOptions' => ['class' => 'table table-bordered m-0 table-more-condensed'],
-    'headerRowOptions' => ['class' => 'bg-light-gray'],
-    
-    'columns' => [
+         <?php if (!empty($phoneNumber)): ?>
+              Phone:<?= $phoneNumber->number?><br/>
+      <?php endif; ?>
+         <?php if (!empty($model->email)): ?>
+              E-mail:<?= $model->email?>
+      <?php endif; ?>
+        </address>
+      </div>
+      <!-- /.col -->
+      <div class="col-sm-4 invoice-col">
+        <b>Invoices</b><br>
+        <br>
+       
+        <b><?= $dateRange; ?></b> 
+      </div>
+      <!-- /.col -->
+    </div>
+     <div class="clearfix"></div>
+    <?php
+     echo GridView::widget([
+         'dataProvider' => $invoiceDataProvider,
+         'options' => ['class' => 'col-md-12'],
+         'summary' =>'',
+         'tableOptions' => ['class' => 'table table-striped table-more-condensed'],
+         'headerRowOptions' => ['class' => 'bg-light-gray'],
+         'columns' => [
         [
             'label' => 'Invoice Number',
             'value' => function ($data) {
@@ -135,9 +134,11 @@ $this->params['breadcrumbs'][] = $this->title;
             'enableSorting' => false,
         ],
     ],
-]); ?>
-    </div>
-</div>
+]); ?> 
+              
+              
+              
+      </div>
 <script>
 	$(document).ready(function(){
 		window.print();
