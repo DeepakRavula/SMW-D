9e8a6e2d0d76d3fc22d9945811b944867cc053a9
SenG <senguttuvang@gmail.com>
2017-08-29 10:08:36 +0530

    align formatting

--- backend/views/invoice/print/view.php
+++ backend/views/invoice/print/view.php
@@ -156,44 +156,44 @@
       <!-- /.col -->
       <div class="col-xs-6">
           <div class="table-responsive">
-           <table class="table-invoice-childtable" style="float:right; width:auto;">
-										<tr>
-											<td id="invoice-discount">Discounts</td>
-											<td><?= Yii::$app->formatter->format($model->totalDiscount, ['currency']); ?></td>
-										</tr>
-                                        <tr>
-                                            <td>SubTotal</td>
-                                            <td>
-                                                <?= Yii::$app->formatter->format($model->subTotal, ['currency']); ?>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Tax</td>
-                                            <td>
-                                                <?= Yii::$app->formatter->format($model->tax, ['currency']); ?>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td><strong>Total</strong></td>
-                                            <td><strong><?= Yii::$app->formatter->format($model->total, ['currency']); ?></strong></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Paid</td>
-                                            <td>
-                                                <?= Yii::$app->formatter->format($model->invoicePaymentTotal, ['currency']); ?>
-                                            </td>
-                                        </tr>
-                                        <tr class="last-balance">
-                                            <td class="p-t-0"><strong>Balance</strong></td>
-                                            <td class="p-t-0"><strong><?= Yii::$app->formatter->format($model->balance, ['currency']); ?></strong></td>
-                                        </tr>
-                                    </table>
-        </div>
+              <table class="table-invoice-childtable" style="float:right; width:auto;">
+                  <tr>
+                      <td id="invoice-discount">Discounts</td>
+                      <td><?= Yii::$app->formatter->format($model->totalDiscount, ['currency']); ?></td>
+                  </tr>
+                  <tr>
+                      <td>SubTotal</td>
+                      <td>
+                          <?= Yii::$app->formatter->format($model->subTotal, ['currency']); ?>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td>Tax</td>
+                      <td>
+                          <?= Yii::$app->formatter->format($model->tax, ['currency']); ?>
+                      </td>
+                  </tr>
+                  <tr>
+                      <td><strong>Total</strong></td>
+                      <td><strong><?= Yii::$app->formatter->format($model->total, ['currency']); ?></strong></td>
+                  </tr>
+                  <tr>
+                      <td>Paid</td>
+                      <td>
+                          <?= Yii::$app->formatter->format($model->invoicePaymentTotal, ['currency']); ?>
+                      </td>
+                  </tr>
+                  <tr class="last-balance">
+                      <td class="p-t-0"><strong>Balance</strong></td>
+                      <td class="p-t-0"><strong><?= Yii::$app->formatter->format($model->balance, ['currency']); ?></strong></td>
+                  </tr>
+              </table>
+          </div>
       </div>
       <!-- /.col -->
-    </div>
+        </div>
         <!-- /.col -->
-    </div>
+        </div>
     <div class="reminder_notes text-muted well well-sm no-shadow" style="clear:both; margin-top: 20px; position: relative;">
         <?php echo $model->reminderNotes; ?>
     </div>
