2e46147e00d4aa9492a2541abdb2add36e3240c4
SenG <senguttuvang@gmail.com>
2017-09-01 11:03:24 +0530

    invoice-view-formatting

--- backend/views/invoice/_view-bottom-summary.php
+++ backend/views/invoice/_view-bottom-summary.php
@@ -1,25 +1,32 @@
-<tr>
-    <td id="invoice-discount">Discounts</td>
-	<td><?= Yii::$app->formatter->format($model->totalDiscount, ['currency']); ?></td>
-</tr>
-<tr>
-	<td>SubTotal</td>
-	<td><?= Yii::$app->formatter->asCurrency($model->subTotal); ?></td>
-</tr>
-
-<tr>
-	<td>Tax</td>
-	<td><?= Yii::$app->formatter->format($model->tax, ['currency']); ?></td>
-</tr>
-<tr>
-	<td><strong>Total</strong></td>
-	<td><strong><?= Yii::$app->formatter->asCurrency($model->total); ?></strong></td>
-</tr>
-<tr>
-	<td>Paid</td>
-	<td><?= !empty($model->invoicePaymentTotal) ? Yii::$app->formatter->asCurrency($model->invoicePaymentTotal) : '$0.00'; ?></td>
-</tr>
-<tr>
-<td class="p-t-20"><strong>Balance</strong></td>
-<td class="p-t-20"><strong><?= Yii::$app->formatter->asCurrency($model->balance); ?></strong></td>
-</tr>
+<table class="table-invoice-childtable" style="float:right; width:auto;">
+    <tr>
+        <td id="invoice-discount">Discounts</td>
+        <td><?= Yii::$app->formatter->format($model->totalDiscount, ['currency']); ?></td>
+    </tr>
+    <tr>
+        <td>SubTotal</td>
+        <td>
+            <?= Yii::$app->formatter->format($model->subTotal, ['currency']); ?>
+        </td>
+    </tr>
+    <tr>
+        <td>Tax</td>
+        <td>
+            <?= Yii::$app->formatter->format($model->tax, ['currency']); ?>
+        </td>
+    </tr>
+    <tr>
+        <td><strong>Total</strong></td>
+        <td><strong><?= Yii::$app->formatter->format($model->total, ['currency']); ?></strong></td>
+    </tr>
+    <tr>
+        <td>Paid</td>
+        <td>
+            <?= Yii::$app->formatter->format($model->invoicePaymentTotal, ['currency']); ?>
+        </td>
+    </tr>
+    <tr class="last-balance">
+        <td class="p-t-0"><strong>Balance</strong></td>
+        <td class="p-t-0"><strong><?= Yii::$app->formatter->format($model->balance, ['currency']); ?></strong></td>
+    </tr>
+</table>
