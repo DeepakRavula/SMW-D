dd93299c5fadc2e8f4aef772190b73e2b7529133
SenG <senguttuvang@gmail.com>
2017-09-12 12:50:32 +0530

    student-print-change

--- backend/views/student/_print.php
+++ backend/views/student/_print.php
@@ -1,6 +1,7 @@
 <?php
 
 use common\models\Location;
+use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $model common\models\Invoice */
 
@@ -8,7 +9,47 @@ use common\models\Location;
 <div>
 <?php $location = Location::findOne(['id' => Yii::$app->session->get('location_id')]); ?>
 <h3><strong>Student's List for <?= $location->name;?> Location</strong></h3></div>
-<?php echo $this->render('_index', ['searchModel' => $searchModel, 'dataProvider' => $dataProvider]); ?>
+<?php yii\widgets\Pjax::begin(['id' => 'student-listing']); ?>
+<?php
+echo GridView::widget([
+	'id' => 'student-grid',
+	'dataProvider' => $dataProvider,
+    'summary'=>'',
+	'tableOptions' => ['class' => 'table table-bordered'],
+	'headerRowOptions' => ['class' => 'bg-light-gray'],
+	'columns' => [
+		[
+			'label' => 'First Name',
+			'value' => function ($data) {
+				return !(empty($data->first_name)) ? $data->first_name : null;
+			},
+		],
+		[
+			'label' => 'Last Name',
+			'value' => function ($data) {
+				return !(empty($data->last_name)) ? $data->last_name : null;
+			},
+		],
+			[
+			'label' => 'Customer',
+			'value' => function ($data) {
+				$fullName = !(empty($data->customer->userProfile->fullName)) ? $data->customer->userProfile->fullName : null;
+
+				return $fullName;
+			},
+		],
+			[
+			'label' => 'Phone',
+			'headerOptions' => ['class' => 'text-left'],
+			'contentOptions' => ['class' => 'text-left'],
+			'value' => function ($data) {
+				return !empty($data->customer->phoneNumber->number) ? $data->customer->phoneNumber->number : null;
+			},
+		],
+	],
+]);
+?>
+<?php yii\widgets\Pjax::end(); ?>
 <script>
 	$(document).ready(function(){
 		window.print();
