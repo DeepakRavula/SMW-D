f9ba31fa799206c2510c7f48831e398141c8e671
SenG <senguttuvang@gmail.com>
2017-09-13 14:38:50 +0530

    adding mote than one phone numbers

--- backend/views/student/_print.php
+++ backend/views/student/_print.php
@@ -7,8 +7,44 @@ use yii\grid\GridView;
 
 ?>
 <div>
-<?php $location = Location::findOne(['id' => Yii::$app->session->get('location_id')]); ?>
-<h3><strong>Student's List for <?= $location->name;?> Location</strong></h3></div>
+<<?php $model = Location::findOne(['id' => Yii::$app->session->get('location_id')]); ?>
+<div class="row">
+    <div class="col-md-12">
+        <h2 class="page-header">
+            <span class="logo-lg"><b>Arcadia</b>SMW</span>
+            <small class="pull-right"><?= Yii::$app->formatter->asDate('now'); ?></small>
+        </h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6 invoice-col">
+        <div class="invoice-print-address">
+            <address>
+                <strong> Arcadia Music Academy ( <?= $model->name; ?> )</strong><br/>
+                <?php if (!empty($model->address)): ?>
+                    <?= $model->address ?><br/>
+                <?php endif; ?>
+                <?php if (!empty($model->city_id)): ?>
+                    <?= $model->city->name ?>
+                <?php endif; ?>
+                <?php if (!empty($model->province_id)): ?>
+                    <?= ', ' . $model->province->name ?><br/>
+                <?php endif; ?>
+                <?php if (!empty($model->postal_code)): ?>
+                    <?= $model->postal_code ?><br/>
+                <?php endif; ?>    
+                <?php if (!empty($model->phone_number)): ?>
+                    <?= $model->phone_number ?>
+                <?php endif; ?>
+                <br/>
+                www.arcadiamusicacademy.com
+            </address>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
+<div>
+<h3><strong>Student's List for <?= $model->name;?> Location</strong></h3></div>
 <?php yii\widgets\Pjax::begin(['id' => 'student-listing']); ?>
 <?php
 echo GridView::widget([
