69b9bbaf3e20e77d369c1bbede3948fa223d9741
SenG <senguttuvang@gmail.com>
2017-10-16 10:10:17 +0530

    program delete

--- backend/views/program/_form.php
+++ backend/views/program/_form.php
@@ -54,24 +54,35 @@ if (!$model->isNewRecord) {
 				</div>
    		</div>
 	</div>
-	<div class="row-fluid">
-		<div class="form-group">
-       <?php echo Html::submitButton(Yii::t('backend', 'Save'), ['class' => 'btn btn-info', 'name' => 'signup-button']) ?>
-        <?= Html::a('Cancel', '', ['class' => 'btn btn-default program-cancel']);?>
-           <?php if (!$model->isNewRecord) {
-            echo Html::a('Delete', ['delete', 'id' => $model->id], [
-                'id' => 'holiday-delete-button',
-                'class' => 'btn btn-danger',
-                'data' => [
-                    'confirm' => 'Are you sure you want to delete this item?',
-                    'method' => 'post',
-                ]
-            ]);
-        } ?>
+    <div class="row-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group pull-left">
+                    <?php
+                    if (!$model->isNewRecord) {
+                        echo Html::a('Delete', ['delete', 'id' => $model->id], [
+                            'id' => 'holiday-delete-button',
+                            'class' => 'btn btn-danger',
+                            'data' => [
+                                'confirm' => 'Are you sure you want to delete this item?',
+                                'method' => 'post',
+                            ]
+                        ]);
+                    }
+
+                    ?>
+                </div>
+
+                <div class="form-group pull-right">
+<?php echo  Html::a('Cancel', '', ['class' => 'btn btn-default program-cancel']); ?>
+<?php echo Html::submitButton(Yii::t('backend', 'Save'), ['class' => 'btn btn-info', 'name' => 'signup-button']) ?>
+
+                </div>
+            </div></div>
     </div>
-    </div>
-    <?php ActiveForm::end(); ?>
 </div>
+    <?php ActiveForm::end(); ?>
+
 
 </div>
 <script type="text/javascript">
