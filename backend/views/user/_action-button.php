0b467981108f4d7caed83b8487cd283a89d2a63b
SenG <senguttuvang@gmail.com>
2017-11-08 15:01:41 +0530

    customizing delete user access

--- backend/views/user/_action-button.php
+++ backend/views/user/_action-button.php
@@ -1,12 +1,36 @@
 <?php
 use yii\helpers\Html;
+use common\models\User;
 ?>
 <?php
 $loggedInUserRole = $model->getRoleById(Yii::$app->user->id);
 $viewedUserRole = $model->getRoleById($model->id);
-echo $loggedInUserRole;
-echo $viewedUserRole;die;
-Html::a('<i title="Delete" class="fa fa-trash"></i>', ['delete', 'id' => $model->id],
+$button=Html::a('<i title="Delete" class="fa fa-trash"></i>', ['delete', 'id' => $model->id],
 		[ 
 			'class' => 'm-r-10 btn btn-box-tool user-delete-button', 
-]); ?>
+]); 
+
+if($loggedInUserRole!==$viewedUserRole)
+{
+    if($loggedInUserRole===User::ROLE_ADMINISTRATOR)
+    {
+      echo $button;
+    }
+    else if($loggedInUserRole===User::ROLE_OWNER)
+    {
+        if($viewedUserRole===User::ROLE_CUSTOMER || $viewedUserRole===User::ROLE_TEACHER || $viewedUserRole===User::ROLE_STAFFMEMBER)
+        {
+          echo $button;  
+        }
+    }
+    else if($loggedInUserRole===User::ROLE_STAFFMEMBER)
+    {
+        if($viewedUserRole===User::ROLE_CUSTOMER || $viewedUserRole===User::ROLE_TEACHER)
+        {
+          echo $button;  
+        }
+    }
+    
+}
+
+?>
