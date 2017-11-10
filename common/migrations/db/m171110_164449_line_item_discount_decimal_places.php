b4dfd028ef5dfab1feb90594f2f0e4c7e8c24865
SenG <senguttuvang@gmail.com>
2017-11-10 22:33:03 +0530

    fixes line item discount decimal places

---
+++ common/migrations/db/m171110_164449_line_item_discount_decimal_places.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m171110_164449_line_item_discount_decimal_places extends Migration
+{
+    public function up()
+    {
+        $this->alterColumn ('invoice_line_item_discount', 'value', $this->decimal(10,4));
+    }
+
+    public function down()
+    {
+        echo "m171110_164449_line_item_discount_decimal_places cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
