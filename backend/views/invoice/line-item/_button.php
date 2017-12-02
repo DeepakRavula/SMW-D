d4e6b73eadae0793f4837a82fb2c658c42651052
SenG <senguttuvang@gmail.com>
2017-12-02 15:57:43 +0530

    disabling menu items when not needed

--- backend/views/invoice/line-item/_button.php
+++ backend/views/invoice/line-item/_button.php
@@ -2,13 +2,17 @@
 <ul class="dropdown-menu dropdown-menu-right">
     <?php if(!$model->lineItem) :?>
         <li><a class="add-new-misc" href="#">Add Item...</a></li>
+        <li><a class="edit-tax multiselect-disable" href="#">Edit Tax...</a></li>
+        <li><a class="edit-item multiselect-disable" href="#">Edit Item...</a></li>
+        <li><a class = "apply-discount multiselect-disable" href="#">Edit Discount...</a></li>        
     <?php endif; ?>
     <?php if ($model->lineItem) : ?>
         <?php if ($model->lineItem->isOtherLineItems() && !$model->lineItem->isLessonItem()) : ?>
             <li><a class="add-new-misc" href="#">Add Item...</a></li>
-            <?php if($model->lineItem->isOtherLineItems() && !$model->lineItem->isLessonItem()) :?>
-                <li><a class="edit-tax" href="#">Edit Tax...</a></li>
-            <?php endif; ?>
+            <li><a class="edit-tax" href="#">Edit Tax...</a></li>
+        <?php else :?>
+            <li><a class="add-new-misc multiselect-disable" href="#">Add Item...</a></li>
+            <li><a class="edit-tax multiselect-disable" href="#">Edit Tax...</a></li> 
         <?php endif; ?>
         <li><a class="edit-item" href="#">Edit Item...</a></li>
         <li><a class = "apply-discount" href="#">Edit Discount...</a></li>
