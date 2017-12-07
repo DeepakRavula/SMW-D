aece195d9a07b925689c90443ba5e2921e29d690
SenG <senguttuvang@gmail.com>
2017-12-07 11:58:17 +0530

    removes backend login screen logo.

--- backend/views/sign-in/login.php
+++ backend/views/sign-in/login.php
@@ -19,7 +19,6 @@ $this->params['body-class'] = 'login-page';
             </a>
         <?php //echo Html::encode($this->title)?>
     </div><!-- /.login-logo -->
-<img src="<?php echo Yii::$app->request->baseUrl ?>/img/admin.png" class="img-circle admin-img" />	
     <div class="login-box-body">
         <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
         <div class="body">
