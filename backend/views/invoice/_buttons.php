0a14ecdcf696ae427f3de1d8e15c09a16a510e8b
SenG <senguttuvang@gmail.com>
2017-12-07 12:23:28 +0530

    fixes invoice header total.

--- backend/views/invoice/_buttons.php
+++ backend/views/invoice/_buttons.php
@@ -38,5 +38,8 @@ use backend\models\search\InvoiceSearch;
 	'id' => 'invoice-mail-button',
 	'class' => 'm-r-10 btn btn-box-tool']) ?>
 <?= Html::a('<i title="Print" class="fa fa-print m-r-10"></i>', ['print/invoice', 'id' => $model->id], ['class' => 'm-r-10 btn btn-box-tool', 'target' => '_blank']) ?>
-<?= strtoupper($model->getStatus()) . ' $'?>
-<?= !empty($model->invoicePaymentTotal)? Yii::$app->formatter->asDecimal($model->invoicePaymentTotal, 4) : Yii::$app->formatter->asDecimal(0, 4) ?>
+<?= strtoupper($model->getStatus()) . ' '?>
+<?= Yii::$app->formatter->format($model->total, ['currency', 'USD', [
+    \NumberFormatter::MIN_FRACTION_DIGITS => 2,
+    \NumberFormatter::MAX_FRACTION_DIGITS => 2,
+]]); ?>
