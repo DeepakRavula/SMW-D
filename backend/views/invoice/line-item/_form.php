895d21a483a68950c46a2a751a0a462d28caf1bb
SenG <senguttuvang@gmail.com>
2017-12-09 12:06:18 +0530

    fixes ui feedback

--- backend/views/invoice/line-item/_form.php
+++ backend/views/invoice/line-item/_form.php
@@ -8,22 +8,7 @@ use yii\helpers\Url;
 /* @var $model common\models\Student */
 /* @var $form yii\bootstrap\ActiveForm */
 ?>
-<style>
-@media (min-width: 768px) {
-  .item-view dt {
-    float: left;
-    width: 110px;
-    overflow: hidden;
-    clear: left;
-    text-align: right;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-  .item-view dd {
-    margin-left: 130px;
-  }
-}
-</style>
+
 <div class="lesson-qualify p-10">
 
 <?php $form = ActiveForm::begin([
@@ -35,8 +20,8 @@ use yii\helpers\Url;
         <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
         <span class="sr-only">Loading...</span>
     </div>
-    <dl class="dl-horizontal">
-        <div class="row" style="height: 35px">
+    <dl class="dl-horizontal item-main-view">
+        <div class="row item-code">
             <div class="col-md-12">
                 <dt>Code</dt>
                 <dd><?= $model->code ?></dd>
@@ -44,26 +29,26 @@ use yii\helpers\Url;
         </div>
         <div class="row">
             <div class="col-md-12">
-                <dt style="padding-top:6px;">Description</dt>
+                <dt>Description</dt>
                 <dd><?= $form->field($model, 'description')->textarea()->label(false);?></dd>
             </div>
         </div>
         <div class="row">
             <div class="col-md-7">
-            <dt style="padding-top:6px;">Price</dt>
-            <dd><?= $form->field($model, 'amount')->textInput(['id' => 'amount-line'])->label(false);?></dd>
+            <dt>Price</dt>
+            <dd><?= $form->field($model, 'amount')->textInput(['class' => 'text-right form-control', 'id' => 'amount-line'])->label(false);?></dd>
             </div>
             <div class="col-md-5">
                 <dl class="item-view">
-                    <dt style="padding-top:7px;">Cost</label></dt>
-                    <dd><?= $form->field($model, 'cost')->textInput()->label(false);?></dd>
+                    <dt>Cost</label></dt>
+                    <dd><?= $form->field($model, 'cost')->textInput(['class' => 'text-right form-control'])->label(false);?></dd>
                 </dl>
             </div>
         </div>
         <div class="row">
             <div class="col-md-6">
-                <dt style="padding-top:6px;">Quantity</dt>
-                <dd><?= $form->field($model, 'unit')->textInput(['id' => 'unit-line'])->label(false);?></dd>
+                <dt>Quantity</dt>
+                <dd><?= $form->field($model, 'unit')->textInput(['class' => 'text-right form-control', 'id' => 'unit-line'])->label(false);?></dd>
             </div>
         </div>
         <div class="row">
