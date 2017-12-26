665fd9bee723f3b5ec31fa4391837c6b98788155
SenG <senguttuvang@gmail.com>
2017-12-26 23:22:04 +0530

    changes error message

--- common/components/validators/lesson/conflict/PastDateValidator.php
+++ common/components/validators/lesson/conflict/PastDateValidator.php
@@ -8,7 +8,7 @@ class PastDateValidator extends Validator
     public function validateAttribute($model, $attribute)
     {   
 	if(new \DateTime($model->date) < new \DateTime()) {
-            $this->addError($model,$attribute, 'Lesson cannot be schedule on past dates!');
+            $this->addError($model,$attribute, 'The lesson cannot be scheduled on past dates!');
         }
     }
 }
