96fe43dfa54fda0d47eb3182b8cb42b849e33604
SenG <senguttuvang@gmail.com>
2018-01-02 15:28:56 +0530

    adding log for Proforma Invoice while creating enrolment

--- backend/views/invoice/log.php
+++ backend/views/invoice/log.php
@@ -1,12 +1,20 @@
 <?php
 
 use yii\grid\GridView;
-
+use insolita\wgadminlte\LteBox;
+use insolita\wgadminlte\LteConst;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-?> 
+?>
+<?php
+LteBox::begin([
+	'type' => LteConst::TYPE_DEFAULT,
+	'title' => 'History',
+	'withBorder' => true,
+])
+?>
 <div class="student-index"> 
     <?php yii\widgets\Pjax::begin([
         'id' => 'invoice-log',
@@ -37,4 +45,5 @@ use yii\grid\GridView;
 ]); ?>
     <?php \yii\widgets\Pjax::end(); ?>
 </div>
+<?php LteBox::end() ?>
 	
