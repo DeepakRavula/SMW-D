8061f30508cda4bb603075853d6ce8f4a149d5d0
SenG <senguttuvang@gmail.com>
2018-01-04 19:20:20 +0530

    migration for adding location slug

--- common/migrations/one-off/m180103_165133_adding_location_slug_in_log_table.php
+++ common/migrations/one-off/m180103_165133_adding_location_slug_in_log_table.php
@@ -16,10 +16,10 @@ class m180103_165133_adding_location_slug_in_log_table extends Migration
         foreach ($logLinks as $logLink) {
             $path       = $logLink->path;
             $pathArray  = explode("/", $path);
-            $tobeEdited = false;
+            $tobeEdited = true;
             foreach ($locations as $location) {
                 if (array_search($location, $pathArray)) {
-                    $tobeEdited = true;
+                    $tobeEdited = false;
                 }
             }
             if (!$tobeEdited) {
