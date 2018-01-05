b41a22aa54c6d1658366de04a62c53c7baac0829
SenG <senguttuvang@gmail.com>
2018-01-05 11:15:31 +0530

    edit-discount-in-enrolment-log

--- common/models/log/DiscountLog.php
+++ common/models/log/DiscountLog.php
@@ -1,48 +1,53 @@
 <?php
+
 namespace common\models\log;
 
 use Yii;
 use common\models\Enrolment;
-use common\models\log\Discount;
 use yii\helpers\Json;
 use yii\helpers\Url;
 
 class DiscountLog extends Log
 {
+
     public function enrolmentMultipleDiscountEdit($event)
     {
-        $enrolmentModel     = $event->sender;
-        $loggedUser         = $event->data['loggedUser'];
-        $oldDiscount        = $event->data['oldDiscount'];
-        $newDiscount        = $event->data['newDiscount'];
-        $data               = Enrolment::find(['id' => $enrolmentModel->id])->asArray()->one();
-        $index              = $enrolmentModel->customer->publicIdentity;
-        $path               =  Url::to(['/user/view', 'UserSearch[role_name]' => 'customer',
-                               'id' => $enrolmentModel->customer->id]);
-        $message            = $loggedUser->publicIdentity.' changed the Multiple Enrolment Discount from  '.$oldDiscount.'to    $'.$newDiscount.'  for {{'.$index.'}}';
-        $object             = LogObject::findOne(['name' => LogObject::TYPE_ENROLMENT]);
-        $activity           = LogActivity::findOne(['name' => LogActivity::TYPE_UPDATE]);
-        $locationId         = $enrolmentModel->customer->userLocation->location->id;
-        $this->addLog($object, $activity, $message, $data, $loggedUser, $enrolmentModel, $locationId, $index, $path);
-    }
-    public function enrolmentPaymentFrequencyDiscountEdit($event)
-    {
-        $enrolmentModel     = $event->sender;
-        $loggedUser         = $event->data['loggedUser'];
-        $oldDiscount        = $event->data['oldDiscount'];
-        $newDiscount        = $event->data['newDiscount'];
-        $data               = Enrolment::find(['id' => $enrolmentModel->id])->asArray()->one();
-        $index              = $enrolmentModel->customer->publicIdentity;
-        $path               =  Url::to(['/user/view', 'UserSearch[role_name]' => 'customer',
-                               'id' => $enrolmentModel->customer->id]);
-        $message            = $loggedUser->publicIdentity.' changed the Payment Frequency Discount from  '.$oldDiscount.' to   '.$newDiscount.'%    for {{'.$index.'}}';
-        $object             = LogObject::findOne(['name' => LogObject::TYPE_ENROLMENT]);
-        $activity           = LogActivity::findOne(['name' => LogActivity::TYPE_UPDATE]);
-        $locationId         = $enrolmentModel->customer->userLocation->location->id;
-        $this->addLog($object, $activity, $message, $data, $loggedUser, $enrolmentModel, $locationId, $index, $path);
+        $enrolmentModel = $event->sender;
+        $loggedUser     = $event->data['loggedUser'];
+        $oldDiscount    = $event->data['oldDiscount'];
+        $newDiscount    = $event->data['newDiscount'];
+        $data           = Enrolment::find(['id' => $enrolmentModel->id])->asArray()->one();
+        $index          = $enrolmentModel->customer->publicIdentity;
+        $path           = Url::to(['/user/view', 'UserSearch[role_name]' => 'customer',
+                          'id' => $enrolmentModel->customer->id]);
+        $message        = $loggedUser->publicIdentity.' changed the Multiple Enrolment Discount from  '.$oldDiscount.'  to    $'.$newDiscount.'  for {{'.$index.'}}';
+        $object         = LogObject::findOne(['name' => LogObject::TYPE_ENROLMENT]);
+        $activity       = LogActivity::findOne(['name' => LogActivity::TYPE_UPDATE]);
+        $locationId     = $enrolmentModel->customer->userLocation->location->id;
+        $this->addLog($object, $activity, $message, $data, $loggedUser,
+            $enrolmentModel, $locationId, $index, $path);
     }
 
-   	public function addLog($object, $activity, $message, $data, $loggedUser, $model, $locationId, $index, $path)
+    public function enrolmentPaymentFrequencyDiscountEdit($event)
+    {
+        $enrolmentModel = $event->sender;
+        $loggedUser     = $event->data['loggedUser'];
+        $oldDiscount    = $event->data['oldDiscount'];
+        $newDiscount    = $event->data['newDiscount'];
+        $data           = Enrolment::find(['id' => $enrolmentModel->id])->asArray()->one();
+        $index          = $enrolmentModel->customer->publicIdentity;
+        $path           = Url::to(['/user/view', 'UserSearch[role_name]' => 'customer',
+                          'id' => $enrolmentModel->customer->id]);
+        $message        = $loggedUser->publicIdentity.' changed the Payment Frequency Discount from  '.$oldDiscount.'  to   '.$newDiscount.'%    for {{'.$index.'}}';
+        $object         = LogObject::findOne(['name' => LogObject::TYPE_ENROLMENT]);
+        $activity       = LogActivity::findOne(['name' => LogActivity::TYPE_UPDATE]);
+        $locationId     = $enrolmentModel->customer->userLocation->location->id;
+        $this->addLog($object, $activity, $message, $data, $loggedUser,
+            $enrolmentModel, $locationId, $index, $path);
+    }
+
+    public function addLog($object, $activity, $message, $data, $loggedUser,
+                           $model, $locationId, $index, $path)
     {
         $log                = new Log();
         $log->logObjectId   = $object->id;
@@ -56,6 +61,7 @@ class DiscountLog extends Log
             $this->addLink($log, $index, $path);
         }
     }
+
     public function addLink($log, $index, $path)
     {
         $logLink          = new LogLink();
