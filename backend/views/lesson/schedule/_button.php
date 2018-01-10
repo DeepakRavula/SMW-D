e928a58bb60caef5a2ec6669d8da4cb1321e2fc5
SenG <senguttuvang@gmail.com>
2018-01-10 12:18:40 +0530

    enabling unschedule lesson option in group lesson

--- backend/views/lesson/schedule/_button.php
+++ backend/views/lesson/schedule/_button.php
@@ -3,10 +3,12 @@ use yii\helpers\Url;
 
 ?>
 <i title="Edit" class="m-r-10 fa fa-pencil edit-lesson-schedule"></i>
-<?php if ($model->isScheduled() && $model->isPrivate()) : ?>
+<?php if ($model->isScheduled()) : ?>
 	<i class="fa fa-angle-down fa-lg dropdown-toggle" data-toggle="dropdown"></i>
         <ul class="dropdown-menu dropdown-menu-right">
             <li><a id="lesson-unschedule" href="#">Unschedule Lesson</a></li>
+            
+            <?php if ($model->isPrivate()) : ?>
             <?php if (!$model->invoice) : ?>
 		<li>
                     <a href= "<?= Url::to(['lesson/invoice', 'id' => $model->id]); ?>">
@@ -33,5 +35,6 @@ use yii\helpers\Url;
                     </a>
                 </li>
             <?php endif; ?>
+                <?php endif; ?>
         </ul>
 <?php endif; ?>
