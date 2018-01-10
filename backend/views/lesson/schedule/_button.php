9bc31199b4956a560d4e176f5ff189d27b004301
SenG <senguttuvang@gmail.com>
2018-01-10 12:42:44 +0530

    unscheduling group lesson

--- backend/views/lesson/schedule/_button.php
+++ backend/views/lesson/schedule/_button.php
@@ -3,14 +3,14 @@ use yii\helpers\Url;
 
 ?>
 <i title="Edit" class="m-r-10 fa fa-pencil edit-lesson-schedule"></i>
-<?php if ($model->isScheduled()) : ?>
-	<i class="fa fa-angle-down fa-lg dropdown-toggle" data-toggle="dropdown"></i>
+    <?php if ($model->isScheduled()) : ?>
+        <i class="fa fa-angle-down fa-lg dropdown-toggle" data-toggle="dropdown"></i>
         <ul class="dropdown-menu dropdown-menu-right">
             <li><a id="lesson-unschedule" href="#">Unschedule Lesson</a></li>
-            
-            <?php if ($model->isPrivate()) : ?>
-            <?php if (!$model->invoice) : ?>
-		<li>
+
+             <?php if ($model->isPrivate()) : ?>
+                 <?php if (!$model->invoice) : ?>
+                <li>
                     <a href= "<?= Url::to(['lesson/invoice', 'id' => $model->id]); ?>">
                         Generate Invoice
                     </a>
@@ -23,7 +23,7 @@ use yii\helpers\Url;
                 </li>
             <?php endif; ?>
             <?php if (!$model->proFormaInvoice): ?>
-		<li>
+                <li>
                     <a href= "<?= Url::to(['lesson/take-payment', 'id' => $model->id]) ?>">
                         Generate PFI
                     </a>
@@ -35,6 +35,6 @@ use yii\helpers\Url;
                     </a>
                 </li>
             <?php endif; ?>
-                <?php endif; ?>
-        </ul>
+        <?php endif; ?>
+    </ul>
 <?php endif; ?>
