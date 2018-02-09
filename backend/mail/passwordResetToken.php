896ef3de7e7977ea796ecb3cfec23ef2934a2460
SenG <senguttuvang@gmail.com>
2018-02-09 15:04:13 +0530

    reset-password-changes

--- backend/mail/passwordResetToken.php
+++ backend/mail/passwordResetToken.php
@@ -10,7 +10,7 @@ $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['sign-in/reset-password',
 
 Dear <?php echo Html::encode($user->publicIdentity) ?>,<br>
 <br>
-You have requested to reset your password for your Arcadia Music Academy account.<br>
+You have requested to reset your password for your Arcadia Academy of Music account.<br>
 <br>
 If you follow the link below you will be able to reset your password:
 <?php echo Html::a(Html::encode($resetLink), $resetLink) ?><br>
@@ -20,4 +20,4 @@ If you follow the link below you will be able to reset your password:
 If you didn't make this request then you can safely ignore this email.<br>
 <br>
 Thank you<br>
-Arcadia Music Academy Team.<br>
+Arcadia Academy of Music Team.<br>
