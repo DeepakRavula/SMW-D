7b8b567e551f67b6509f36f19df59f44a06a3ddd
unknown <SenG>
2018-02-20 21:23:28 +0530

    adding payment lists in file

--- backend/views/invoice/payment/_index.php
+++ backend/views/invoice/payment/_index.php
@@ -1,7 +1,6 @@
 <?php
 use common\models\Payment;
 use common\models\Invoice;
-use yii\grid\GridView;
 use yii\bootstrap\Modal;
 use yii\helpers\Html;
 use insolita\wgadminlte\LteBox;
@@ -21,7 +20,11 @@ Modal::end(); ?>
 
  <?php Pjax::Begin(['id' => 'invoice-view-payment-tab', 'timeout' => 6000]); ?> 
 <?php $boxTools = null;?>
+<?php if (!$print): ?>
 <?php $boxTools = '<i title="Add" class="fa fa-plus add-payment m-r-10"></i>' ?>
+<?php $attributeAmount='amount';?>
+<?php else : $attributeAmount='';?>
+<?php endif; ?>
 <?php
     LteBox::begin([
         'type' => LteConst::TYPE_DEFAULT,
@@ -31,70 +34,19 @@ Modal::end(); ?>
     ])
     ?>
 
-
+<?php if (!$print): ?>
 <div style="margin-bottom: 10px">   
 <?= Html::a(Yii::t('backend', 'Apply Credit'), ['#'], ['class' => 'btn btn-primary btn-sm apply-credit']);?>
 </div>
- 
-<?php
-$columns = [
-      [
-        'contentOptions' => ['class' => 'text-left','style' => 'min-width:60px;max-width:60px;'],
-        'label' => 'Date',
-        'value' => function ($data) {
-            return Yii::$app->formatter->asDate($data->date);
-        },
-        ],
-    [
-        'contentOptions' => ['class' => 'text-left','style' => 'min-width:60px;max-width:60px;'],
-        'label' => 'Type',
-        'value' => function ($data) {
-            return $data->paymentMethod->name;
-        },
-        ],
-    [
-        'contentOptions' => ['class' => 'text-left', 'style' => 'min-width:60px;max-width:60px;'],
-        'label' => 'Ref',
-        'value' => function ($data) {
-            return $data->reference;
-        },
-        ],
-    [
-        'contentOptions' => ['class' => 'text-left','style' => 'min-width:155px;max-width:155px;overflow: auto; word-wrap: break-word;'],
-        'label' => 'Notes',
-        'value' => function ($data) {
-            return $data->notes;
-
-        },
-        ],
-        [
-            'label'=>'Amount',
-            'format' => 'currency',
-            'headerOptions' => ['class' => 'text-right'],
-            'contentOptions' => ['class' => 'text-right','style' => 'min-width:60px;max-width:60px;'],
-            'value' => function ($data) {
-                return Yii::$app->formatter->asDecimal($data->amount);
-            },
-        ],
-    ]; ?>
-
+ <?php endif; ?>
 <div>
-	<?php yii\widgets\Pjax::begin([
-        'id' => 'invoice-payment-listing',
-        'timeout' => 6000,
-    ]) ?>
-	<?= GridView::widget([
-        'id' => 'payment-grid',
-        'dataProvider' => $invoicePaymentsDataProvider,
-        'columns' => $columns,
-    'summary' => false,
-        'emptyText' => false,
-        'options' => ['class' => 'col-md-12'],
-    'tableOptions' => ['class' => 'table table-condensed'],
-    'headerRowOptions' => ['class' => 'bg-light-gray'],
-    ]);
-    ?>
-<?php \yii\widgets\Pjax::end(); ?>	
+    		<?=
+        $this->render('/invoice/payment/_payment-list', [
+            'model' => $model,
+            'invoicePaymentsDataProvider' => $invoicePaymentsDataProvider,
+        ]);
+        ?>
+    
 </div>
 <?php
     $amount = 0;
