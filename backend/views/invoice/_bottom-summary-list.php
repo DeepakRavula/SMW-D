7268a703b034ab3ee93c919030360ccd24c22bab
unknown <SenG>
2018-02-20 21:10:24 +0530

    adding bottom summary

--- backend/views/invoice/_bottom-summary-list.php
+++ backend/views/invoice/_bottom-summary-list.php
@@ -1,34 +1,46 @@
 
 <dl class="dl-invoice-summary">
-	<dt id="invoice-discount">Discounts</dt>
-	<dd><?= Yii::$app->formatter->format($model->totalDiscount, ['currency', 'USD', [
-        \NumberFormatter::MIN_FRACTION_DIGITS => 2,
-        \NumberFormatter::MAX_FRACTION_DIGITS => 2,
-    ]]); ?>
-	<dt>SubTotal</dt>
-	<dd><?= Yii::$app->formatter->format($model->subTotal, ['currency', 'USD', [
-        \NumberFormatter::MIN_FRACTION_DIGITS => 2,
-        \NumberFormatter::MAX_FRACTION_DIGITS => 2,
-    ]]); ?></dd>
-	<dt>Tax</dt>
-	<dd><?= Yii::$app->formatter->format($model->tax, ['currency', 'USD', [
-        \NumberFormatter::MIN_FRACTION_DIGITS => 2,
-        \NumberFormatter::MAX_FRACTION_DIGITS => 2,
-    ]]); ?></dd>
-	<dt>Total</dt>
-	<dd><?= Yii::$app->formatter->format($model->total, ['currency', 'USD', [
-        \NumberFormatter::MIN_FRACTION_DIGITS => 2,
-        \NumberFormatter::MAX_FRACTION_DIGITS => 2,
-    ]]); ?></dd>
-	<dt>Paid</dt>
-	<dd> <?php $paymentTotal = !empty($model->invoicePaymentTotal)? $model->invoicePaymentTotal : 0; ?> 
-		<?= Yii::$app->formatter->format($paymentTotal, ['currency', 'USD', [
-        \NumberFormatter::MIN_FRACTION_DIGITS => 2,
-        \NumberFormatter::MAX_FRACTION_DIGITS => 2,
-    ]]); ?>
-	<dt>Balance</dt>
-	<dd> <?= Yii::$app->formatter->format($model->balance, ['currency', 'USD', [
-        \NumberFormatter::MIN_FRACTION_DIGITS => 2,
-        \NumberFormatter::MAX_FRACTION_DIGITS => 2,
-    ]]); ?></dd>
+    <dt id="invoice-discount">Discounts</dt>
+    <dd><?=
+        Yii::$app->formatter->format($model->totalDiscount, ['currency', 'USD', [
+                \NumberFormatter::MIN_FRACTION_DIGITS => 2,
+                \NumberFormatter::MAX_FRACTION_DIGITS => 2,
+        ]]);
+        ?>
+    <dt>SubTotal</dt>
+    <dd><?=
+        Yii::$app->formatter->format($model->subTotal, ['currency', 'USD', [
+                \NumberFormatter::MIN_FRACTION_DIGITS => 2,
+                \NumberFormatter::MAX_FRACTION_DIGITS => 2,
+        ]]);
+        ?></dd>
+    <dt>Tax</dt>
+    <dd><?=
+        Yii::$app->formatter->format($model->tax, ['currency', 'USD', [
+                \NumberFormatter::MIN_FRACTION_DIGITS => 2,
+                \NumberFormatter::MAX_FRACTION_DIGITS => 2,
+        ]]);
+        ?></dd>
+    <dt>Total</dt>
+    <dd><?=
+        Yii::$app->formatter->format($model->total, ['currency', 'USD', [
+                \NumberFormatter::MIN_FRACTION_DIGITS => 2,
+                \NumberFormatter::MAX_FRACTION_DIGITS => 2,
+        ]]);
+        ?></dd>
+    <dt>Paid</dt>
+    <dd> <?php $paymentTotal = !empty($model->invoicePaymentTotal) ? $model->invoicePaymentTotal : 0; ?> 
+        <?=
+        Yii::$app->formatter->format($paymentTotal, ['currency', 'USD', [
+                \NumberFormatter::MIN_FRACTION_DIGITS => 2,
+                \NumberFormatter::MAX_FRACTION_DIGITS => 2,
+        ]]);
+        ?>
+    <dt>Balance</dt>
+    <dd> <?=
+        Yii::$app->formatter->format($model->balance, ['currency', 'USD', [
+                \NumberFormatter::MIN_FRACTION_DIGITS => 2,
+                \NumberFormatter::MAX_FRACTION_DIGITS => 2,
+        ]]);
+        ?></dd>
 </dl>
