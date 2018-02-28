13e14e0c9b1fd183f9bb48fa2b0917a151089397
SenG <senguttuvang@gmail.com>
2018-03-01 00:59:44 +0530

    adds migration for new field

---
+++ common/migrations/db/m180228_192405_add_balance_student_csv.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180228_192405_add_balance_student_csv
+ */
+class m180228_192405_add_balance_student_csv extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->addColumn('student_csv', 'balance', $this->decimal(10,4)->notNull()->after('notes'));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180228_192405_add_balance_student_csv cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180228_192405_add_balance_student_csv cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
