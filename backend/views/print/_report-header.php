bab88b1bb0679a4c7e915417a5f712e9d0252bad
SenG <senguttuvang@gmail.com>
2018-03-08 11:57:01 +0530

    print time voucher

---
+++ backend/views/print/_report-header.php
@@ -0,0 +1,30 @@
+<div class="invoice-view">
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="page-header">
+                <span class="logo-lg"><img class="login-logo-img" src="<?= Yii::$app->request->baseUrl ?>/img/logo.png" /></span>   
+                <span class="pull-right invoice-address right-align">
+                    <small>
+                    <b>Arcadia Music Academy ( <?= $locationModel->name; ?> )</b><br>
+                    <?php if (!empty($locationModel->address)): ?>
+                        <?= $locationModel->address; ?>
+                    <?php endif; ?>
+                    <br/>
+                    <?php if (!empty($locationModel->city_id)): ?>
+                        <?= $locationModel->city->name; ?>,
+                    <?php endif; ?>
+                    <?php if (!empty($locationModel->province_id)): ?>
+                        <?= $locationModel->province->name; ?>
+                    <?php endif; ?>
+                    <br/>
+                    <?php if (!empty($locationModel->postal_code)): ?>
+                        <?= $locationModel->postal_code; ?>
+                    <?php endif; ?>
+                    <br/>
+                    </small>
+                </span>
+                </h2>
+        </div>
+    </div>
+    
+    </div>
--- backend/views/user/teacher/_print.php
+++ backend/views/user/teacher/_print.php
@@ -7,29 +7,40 @@ use common\models\Qualification;
 ?>
     <!-- title row -->
     <?php
-   echo $this->render('/print/_header', [
+   echo $this->render('/print/_report-header', [
        'userModel'=>$model,
        'locationModel'=>$model->userLocation->location,
 ]);
    ?>
+<style>
+        @media print{
+            .report-h1{
+                font-size:36px;
+            }
+            .report-h2{
+                font-size:32px;
+            }
+        }
+    </style>
       <!-- /.col -->
-     <div class="col-sm-4 invoice-col">
-        <b>Lessons</b><br>
-        <br>
-        <b><?php echo  $fromDate->format('F jS, Y') . ' to ' . $toDate->format('F jS, Y');?></b>
-      </div>
+      <div class="col-md-12">
+          <div class="report-h1"> Time Voucher For  <?php echo $model->publicIdentity.'     '; ?></div>
+          
+          <div class="report-h2"><?php echo  $fromDate->format('F jS, Y') . '  to   ' . $toDate->format('F jS, Y');?> </div>
       <!-- /.col -->
-    </div>
     <!-- /.row -->
-
+      </div>
     <!-- Table row -->
     <div class="row">
-      <div class="col-xs-12 table-responsive">
-       <div class="report-grid">
-<?php echo $this->render('_time-voucher-content',['searchModel'=>$searchModel,'teacherLessonDataProvider' => $teacherLessonDataProvider]); ?>
-      </div>
-      <!-- /.col -->
-    </div>
+	<div class="col-xs-12 table-responsive">
+	    <div class="report-grid">
+		<?php echo $this->render('_time-voucher-content',[
+		    'searchModel' => $searchModel,
+		    'teacherLessonDataProvider' => $teacherLessonDataProvider
+		]); ?>
+	    </div>
+	    <!-- /.col -->
+	</div>
     <!-- /.row -->
 	<div class="boxed col-md-12 pull-right">
 <div class="sign">
