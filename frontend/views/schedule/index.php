e6e56a8f270cd83be0077db099b4f708485ca549
SenG <senguttuvang@gmail.com>
2018-03-13 15:51:48 +0530

    teacher-frontend-login-fix

--- frontend/views/schedule/index.php
+++ frontend/views/schedule/index.php
@@ -5,7 +5,7 @@ use yii\helpers\Url;
 use yii\bootstrap\Tabs;
 use common\models\CalendarEventColor;
 
-$this->title = 'Schedule for ' .(new \DateTime())->format('l, F jS, Y');
+$this->title = 'Schedule for ' . (new \DateTime())->format('l, F jS, Y');
 ?>
 <link type="text/css" href="/plugins/fullcalendar-scheduler/lib/fullcalendar.min.css" rel='stylesheet' />
 <link type="text/css" href="/plugins/fullcalendar-scheduler/lib/fullcalendar.print.min.css" rel='stylesheet' media='print' />
@@ -23,90 +23,98 @@ $this->title = 'Schedule for ' .(new \DateTime())->format('l, F jS, Y');
 <link type="text/css" href="/plugins/poshytip/tip-yellowsimple/tip-yellowsimple.css" rel='stylesheet' />
 
 <style>
-@media (max-width: 768px){
- #calendar .fc-view .fc-time-grid-container,#calendar .fc-day-grid-container{
-	 overflow-x:auto !important;
-	 overflow-y: hidden !important;
-	 min-height:220px !important;
-	 height: auto !important;
- }
- #calendar .fc-center h2{
-	     font-size: 16px;
-    margin-top: 10px;
- }
- .wrap > .container {
-    padding: 47px 7px 20px;
-}
- #calendar .fc-header-toolbar .fc-left button{
-	 padding:0 .4em;
- }
- #calendar .fc-toolbar{
-	     padding: 10px 0;
- }
-}
+    @media (max-width: 768px){
+	#calendar .fc-view .fc-time-grid-container,#calendar .fc-day-grid-container{
+	    overflow-x:auto !important;
+	    overflow-y: hidden !important;
+	    min-height:220px !important;
+	    height: auto !important;
+	}
+	#calendar .fc-center h2{
+	    font-size: 16px;
+	    margin-top: 10px;
+	}
+	.wrap > .container {
+	    padding: 47px 7px 20px;
+	}
+	#calendar .fc-header-toolbar .fc-left button{
+	    padding:0 .4em;
+	}
+	#calendar .fc-toolbar{
+	    padding: 10px 0;
+	}
+    }
 
 </style>
 <?php $this->render('_color-code'); ?>
 <div class="clearfix"></div>
 <div id='calendar'></div>
-<?php $userId = Yii::$app->user->id;?>
+<?php $userId = Yii::$app->user->id; ?>
 <script type="text/javascript">
-var locationAvailabilities   = <?php echo Json::encode($locationAvailabilities); ?>;
-var userId = '<?php echo $userId; ?>';
-$(document).ready(function() {	
-    var params = $.param({ userId: userId });
-    var from_time = <?= $from_time?>
-    var to_time = <?= $to_time?>
-    var view='agendaDay';
-    loadCalendar(from_time,to_time,view);
-    function loadCalendar(from_time,to_time,view)
-    {
-	    $('#calendar').fullCalendar({
-        nowIndicator: true,
-        schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
-        header: {
-			left: 'prev,next today',
-			center: 'title',
-			right: 'month,agendaWeek,agendaDay'
-		},
-        titleFormat: 'DD-MMM-YYYY, dddd',
-        defaultView: view,
-        minTime: from_time,
-        maxTime: to_time,
-        slotDuration: "00:15:00",
-        editable: false,
-        droppable: false,
-        events: {
-            url: '<?= Url::to(['schedule/render-day-events']) ?>?' + params,
-            type: 'GET',
-            error: function() {
-                $("#calendar").fullCalendar("refetchEvents");
+        var locationAvailabilities = <?php echo Json::encode($locationAvailabilities); ?>;
+        var userId = '<?php echo $userId; ?>';
+        $(document).ready(function () {
+            var params = $.param({userId: userId});
+            function loadCalendar(from_time, to_time, view)
+            {
+                $('#calendar').unbind().removeData().fullCalendar({
+                    nowIndicator: true,
+                    schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'month,agendaWeek,agendaDay'
+                    },
+                    titleFormat: 'DD-MMM-YYYY, dddd',
+                    defaultView: view,
+                    minTime: from_time,
+                    maxTime: to_time,
+                    slotDuration: "00:15:00",
+                    editable: false,
+                    droppable: false,
+                    events: {
+                        url: '<?= Url::to(['schedule/render-day-events']) ?>?' + params,
+                        type: 'GET',
+                        error: function () {
+                            $("#calendar").fullCalendar("refetchEvents");
+                        }
+                    },
+                    eventRender: function (event, element) {
+                        element.poshytip({
+                            className: 'tip-yellowsimple',
+                            alignTo: 'cursor',
+                            alignX: 'center',
+                            alignY: 'top',
+                            offsetY: 5,
+                            followCursor: false,
+                            slide: false,
+                            content: function (updateCallback) {
+                                return event.description;
+                            }
+                        });
+                    },
+                    allDaySlot: false,
+                });
+
             }
-        },
-		eventRender: function(event, element) {
-			element.poshytip({
-				className: 'tip-yellowsimple',
-				alignTo: 'cursor',
-				alignX: 'center',
-				alignY : 'top',
-				offsetY: 5,
-				followCursor: false,
-				slide: false,
-				content : function(updateCallback) {
-					return event.description;
-				}
-			});
-		},
-		allDaySlot:false,
-    });
-	    
-    }
-    $(document).on('click', '.fc-agendaWeek-button', function () {
-		var from_time = <?= $week_from_time?>
-		var to_time = <?= $week_to_time?>
-		var view='agendaWeek';
-		loadCalendar(from_time,to_time,view);
-			
-	});
-});
+            var from_time = '<?= $from_time ?>';
+            var to_time = '<?= $to_time ?>';
+            var view = 'agendaDay';
+            loadCalendar(from_time, to_time, view);
+            $(document).on('click', '.fc-agendaWeek-button', function () {
+                var from_time = '<?= $week_from_time ?>';
+                var to_time = '<?= $week_to_time ?>';
+                var view = 'agendaWeek';
+                loadCalendar(from_time, to_time, view);
+
+            });
+            $(document).on('click', '.fc-agendaDay-button', function () {
+                var from_time = '<?= $from_time ?>';
+                var to_time = '<?= $to_time ?>';
+                var view = 'agendaDay';
+                loadCalendar(from_time, to_time, view);
+
+            });
+        });
+
 </script>
