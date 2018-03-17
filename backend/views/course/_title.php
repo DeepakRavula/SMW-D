7fb05353df053583f73298e242b5ae0ac585aee8
SenG <senguttuvang@gmail.com>
2018-03-17 15:07:48 +0530

    fixes group course view title.

--- backend/views/course/_title.php
+++ backend/views/course/_title.php
@@ -2,6 +2,7 @@
 use yii\helpers\Url;
 use common\models\Lesson;
 
+$this->title = $model->program->name;
 ?>
 <a href="<?= Url::to(['lesson/index', 'LessonSearch[type]' => Lesson::TYPE_GROUP_LESSON]);?>">Group Lessons</a>  /
 <?= $model->program->name;?>
