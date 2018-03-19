a0064315ceff817683871edce2d22d3ffeee9f30
SenG <senguttuvang@gmail.com>
2018-03-19 20:31:47 +0530

    changes

---
+++ tests/codeception/common/unit/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace tests\codeception\common;
+
+
+class ExampleTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \tests\codeception\common\UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
