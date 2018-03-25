8bb9972cf0b5d45dc58ed095dd76ce284bfce596
SenG <senguttuvang@gmail.com>
2018-03-25 14:54:20 +0530

    gets user from env file.

--- deploy.php
+++ deploy.php
@@ -4,17 +4,29 @@ namespace Deployer;
 require 'recipe/yii2-app-advanced.php';
 require 'recipe/slack.php';
 
+set('user', function () {
+    return getenv('DEP_USER');
+});
+$user = getenv('DEP_HOST_USER');
 // Hosts
-host('pt')
-    ->user('poonkodi')
+host('smw')
+    ->user($user)
     ->port(22)
     ->configFile('~/.ssh/config')
-    ->identityFile('~/.ssh/github')
+    ->identityFile('~/.ssh/id_rsa')
     ->forwardAgent(true)
-	->set('deploy_path', '/srv/smw')
+	->set('deploy_path', '/home/arcadia/smw-dev')
 	->set('branch', 'master')
-	->set('user', 'Seng')
 	->stage('development');
+
+set('slack_webhook', 'https://hooks.slack.com/services/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');
+set('slack_title', 'Studio Manager Web');
+set('slack_text', '{{user}} deploying {{branch}} to {{deploy_path}}');
+set('slack_success_text', 'Deploy to {{deploy_path}} successful');
+set('slack_failure_text', 'Deploy to {{deploy_path}} failure');
+set('slack_color', 'blue');
+set('slack_success_color', 'green');
+set('slack_failure_color', 'red');
 
 set('default_stage', 'development');
 
@@ -22,13 +34,13 @@ set('default_stage', 'development');
 set('application', 'Studio Manager Web');
 
 // Project repository
-set('repository', 'git@github.com:senguttuvang/smw.git');
+set('repository', 'smw-git:kristin-green-and-associates/smw.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
 
 // Shared files/dirs between deploys 
-add('shared_files', []);
+add('shared_files', ['.env']);
 add('shared_dirs', []);
 
 // Writable dirs by web server 
@@ -73,14 +85,17 @@ task('deploy:one-off', function() {
     writeln('<info>One off migration is done.</info>');
 });
 
+before('deploy', 'slack:notify');
+
 task('deploy', [
     'deploy:prepare',
     'deploy:latest_code',
-    'deploy:composer',
+    //'deploy:composer',
     'deploy:migration',
     'deploy:one-off',
 ]);
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
-
+after('success', 'slack:notify:success');
+after('deploy:failed', 'slack:notify:failure');
