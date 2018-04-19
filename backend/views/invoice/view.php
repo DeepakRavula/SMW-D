461cc7722f9359c09e51414df79f3ffdfcb08053
SenG <senguttuvang@gmail.com>
2018-04-19 16:53:35 +0530

    add-validation-for-apply-credit

--- backend/views/invoice/view.php
+++ backend/views/invoice/view.php
@@ -335,7 +335,8 @@ Modal::begin([
                         $('#popup-modal').find('.modal-header').html('<h4 class="m-0">Apply Credit</h4>');
                         $('.modal-save').text('Pay now')
                     } else {
-                        $('#invoice-error-notification').html('No credits available!').fadeIn().delay(5000).fadeOut();
+                        alert(response.message);
+                        $('#invoice-error-notification').html(response.message).fadeIn().delay(5000).fadeOut();
                     }
                 }
             });
