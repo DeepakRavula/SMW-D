05f87e03c205edb235393f56c3cfb213d0f1f392
SenG <senguttuvang@gmail.com>
2018-04-20 14:20:33 +0530

    forgot password email

--- common/behaviors/CatchAllEmailBehavior.php
+++ common/behaviors/CatchAllEmailBehavior.php
@@ -6,6 +6,7 @@ use yii\base\Behavior;
 use yii\mail\BaseMailer;
 use common\models\TestEmail;
 use backend\models\EmailForm;
+use common\models\PasswordResetRequestForm;
 
 /**
  * Class CatchAllMailBehavior
@@ -25,13 +26,17 @@ class CatchAllEmailBehavior extends Behavior
     }
 
     public function addTestEmail($event) {
-	    $model = new EmailForm();
+		$model = new EmailForm();
+		$passwordResetModel = new PasswordResetRequestForm();
 		if (env('YII_ENV') === 'dev') {
 			$email = TestEmail::find()->one()->email;
 		} else {
 			$model->load(Yii::$app->request->post());
 			$email = $model->to;
+			$passwordResetModel->load(Yii::$app->request->post());
+			$email = $passwordResetModel->email;
 		}
 		return $event->message->setTo($email);
 	}
+
 }
