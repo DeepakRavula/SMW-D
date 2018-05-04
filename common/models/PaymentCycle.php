c4360b4761512f3c3e8783710e2e4e11d7c5849e
SenG <senguttuvang@gmail.com>
2018-05-04 12:25:43 +0530

    fixes payment cycle delete

--- common/models/PaymentCycle.php
+++ common/models/PaymentCycle.php
@@ -93,6 +93,18 @@ class PaymentCycle extends \yii\db\ActiveRecord
                 ->onCondition(['lesson.isDeleted' => false]);
     }
 
+    public function beforeSoftDelete()
+    {
+        if ($this->proFormaInvoice && !$this->proFormaInvoice->hasPayments()) {
+            $this->proFormaInvoice->trigger(Invoice::EVENT_DELETE);
+            $this->proFormaInvoice->delete();
+        }
+        foreach ($this->paymentCycleLessons as $payemntCycleLesson) {
+            $payemntCycleLesson->delete();
+        }
+        return true;
+    }
+
     public function getInvoiceItemPaymentCycleLessons()
     {
         return $this->hasMany(InvoiceItemPaymentCycleLesson::className(), ['paymentCycleLessonId' => 'id'])
@@ -129,15 +141,6 @@ class PaymentCycle extends \yii\db\ActiveRecord
     public function hasProFormaInvoice()
     {
         return !empty($this->proFormaInvoice);
-    }
-
-    public function beforeDelete()
-    {
-        if ($this->proFormaInvoice && !$this->proFormaInvoice->hasPayments()) {
-            $this->proFormaInvoice->trigger(Invoice::EVENT_DELETE);
-            $this->proFormaInvoice->delete();
-        }
-        return parent::beforeDelete();
     }
 
     public function beforeSave($insert)
