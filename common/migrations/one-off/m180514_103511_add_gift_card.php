0dd02bb0af305f237b508b8d6b7dfc215eb9a276
SenG <senguttuvang@gmail.com>
2018-05-14 20:27:45 +0530

    add gift card payment

---
+++ common/migrations/one-off/m180514_103511_add_gift_card.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180514_103511_add_gift_card
+ */
+class m180514_103511_add_gift_card extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert('payment_method',array(
+            'name'=>'Gift Card',
+            'active' => 1,
+            'displayed' => 1,
+            'sortOrder' => 8
+     ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180514_103511_add_gift_card cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180514_103511_add_gift_card cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
