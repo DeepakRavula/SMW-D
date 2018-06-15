d1624bbc130998969e686d64376530ac108b7fd9
SenG <senguttuvang@gmail.com>
2018-06-15 17:48:50 +0530

    pfi-list-index

--- backend/views/layouts/_left-menu.php
+++ backend/views/layouts/_left-menu.php
@@ -86,18 +86,18 @@ echo Menu::widget([
             'icon' => '<i class="fa fa-music"></i>',
 	    'active' => (Yii::$app->controller->id === 'unscheduled-lesson') ? true : false,
         ],
-            [
-            'label' => Yii::t('backend', 'Proforma Invoices'),
-            'icon' => '<i class="fa  fa-dollar"></i>',
-            'url' => ['/invoice/index', 'InvoiceSearch[type]' => Invoice::TYPE_PRO_FORMA_INVOICE],
-            'visible' => Yii::$app->user->can('managePfi'),
-            'active' => (isset(Yii::$app->request->queryParams['InvoiceSearch']['type']) && Yii::$app->request->queryParams['InvoiceSearch']['type'] == Invoice::TYPE_PRO_FORMA_INVOICE) ? true : false,
-            'badge' => Invoice::pfiCount(),
-            'badgeBgClass' => 'label-default'
-        ],
+        //     [
+        //     'label' => Yii::t('backend', 'Proforma Invoices'),
+        //     'icon' => '<i class="fa  fa-dollar"></i>',
+        //     'url' => ['/invoice/index', 'InvoiceSearch[type]' => Invoice::TYPE_PRO_FORMA_INVOICE],
+        //     'visible' => Yii::$app->user->can('managePfi'),
+        //     'active' => (isset(Yii::$app->request->queryParams['InvoiceSearch']['type']) && Yii::$app->request->queryParams['InvoiceSearch']['type'] == Invoice::TYPE_PRO_FORMA_INVOICE) ? true : false,
+        //     'badge' => Invoice::pfiCount(),
+        //     'badgeBgClass' => 'label-default'
+        // ],
         
         [
-            'label' => Yii::t('backend', 'New Proforma Invoices'),
+            'label' => Yii::t('backend', 'Proforma Invoices'),
             'icon' => '<i class="fa  fa-dollar"></i>',
             'url' => ['/proforma-invoice/index'],
             'visible' => Yii::$app->user->can('managePfi'),
