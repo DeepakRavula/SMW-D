18248a9148a15b3980d3af591293ee1b1efd5152
SenG <senguttuvang@gmail.com>
2018-06-19 21:48:33 +0530

    add mail in viewPfi

--- backend/views/proforma-invoice/_buttons.php
+++ backend/views/proforma-invoice/_buttons.php
@@ -12,7 +12,6 @@ use yii\widgets\Pjax;
 <?= Html::a('<i title="Mail" class="fa fa-envelope-o"></i>', '#', [
     'id' => 'proforma-invoice-mail-button',
     'class' => 'm-r-10 btn btn-box-tool']) ?>
-<?= Html::a('<i class="fa fa-print m-r-10"></i>', ['#'], ['class' => 'm-r-10 btn btn-box-tool','id'=>'print-btn']) ?>
 <?= Yii::$app->formatter->format($model->getTotal($model->id), ['currency', 'USD', [
     \NumberFormatter::MIN_FRACTION_DIGITS => 2,
     \NumberFormatter::MAX_FRACTION_DIGITS => 2,
