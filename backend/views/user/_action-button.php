40193890d253c6c412b4af1f7d212908cdeff79b
SenG <senguttuvang@gmail.com>
2018-07-15 22:08:03 +0530

    remove receive payment option in admin,teacher

--- backend/views/user/_action-button.php
+++ backend/views/user/_action-button.php
@@ -9,7 +9,9 @@ $user = User::findOne($model->id);
 <div class="btn-group-sm">
     <button class="btn dropdown-toggle" data-toggle="dropdown">More Action&nbsp;&nbsp;<span class="caret"></span></button>
     <ul class="dropdown-menu dropdown-menu-right">
+    <?php if ($user->isCustomer()) : ?>
 	<li><a id="receive-payments" href="#">Receive Payment</a></li>
+    <?php endif ; ?>
 	<?php if(($loggedUser->isAdmin()) || ($loggedUser->isOwner() && $user->isManagableByOwner()) || ($loggedUser->isStaff() && $user->isManagableByStaff())) { ?>
 	<li><a class="user-delete-button" href="<?= Url::to(['delete', 'id' => $model->id]);?>">Delete</a></li>
     </ul>
