d38269f9106798994924e7951c7ffc42883d7c1e
SenG <senguttuvang@gmail.com>
2018-07-17 09:40:36 +0530

    add-price-details

--- backend/views/lesson/_discount-action-menu.php
+++ backend/views/lesson/_discount-action-menu.php
@@ -1,5 +1,11 @@
+<?php use common\models\User; ?>
+
 <i class="fa fa-angle-down fa-lg dropdown-toggle" data-toggle="dropdown"></i>
     <ul class="dropdown-menu dropdown-menu-right" id="menu-shown">
         <li><a id="lesson-discount" href="#">Edit Discount...</a></li>
         <li><a id="lesson-tax" href="#">Edit Tax...</a></li>
+<?php $loggedUser = User::findOne(Yii::$app->user->id); ?>
+<?php if ($loggedUser->isAdmin()) : ?>
+        <li><a id="lesson-price" href="#">Edit Price...</a></li>
+<?php endif; ?>
     </ul>
