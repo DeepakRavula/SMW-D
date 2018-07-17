b740955b3711228ff681d65f1dcfcfb31d97e63a
SenG <senguttuvang@gmail.com>
2018-07-17 15:57:11 +0530

    fixes rounding error

--- common/models/query/InvoiceQuery.php
+++ common/models/query/InvoiceQuery.php
@@ -107,7 +107,7 @@ class InvoiceQuery extends \yii\db\ActiveQuery
     public function invoiceCredit($userId)
     {
         return $this->andWhere(['user_id' => $userId]) 
-            ->andWhere(['<', 'balance', 0.00]);
+            ->andWhere(['<=', 'balance', 0.01]);
     }
 
     public function proFormaCredit($lessonId)
