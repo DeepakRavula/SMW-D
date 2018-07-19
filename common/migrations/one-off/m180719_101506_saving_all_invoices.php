f532b659c5f8f6ee4d905e47013ff20708107dfd
SenG <senguttuvang@gmail.com>
2018-07-19 15:42:18 +0530

    saving all invoices using migration

---
+++ common/migrations/one-off/m180719_101506_saving_all_invoices.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\db\Migration;
+use common\models\Invoice;
+
+/**
+ * Class m180719_101506_saving_all_invoices
+ */
+class m180719_101506_saving_all_invoices extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $invoices = Invoice::find()
+        ->notDeleted()
+        ->location([14, 15])
+        ->all();
+
+    foreach ($invoices as $invoice) {
+        $invoice->save();
+    }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180719_101506_saving_all_invoices cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180719_101506_saving_all_invoices cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
