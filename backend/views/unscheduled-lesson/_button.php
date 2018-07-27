888716eeec561cdae0b8e37eadd30644a86e8d8e
SenG <senguttuvang@gmail.com>
2018-07-27 13:25:27 +0530

    fix-alignment

--- backend/views/unscheduled-lesson/_button.php
+++ backend/views/unscheduled-lesson/_button.php
@@ -17,11 +17,11 @@ $form = ActiveForm::begin([
 ?>
 <?php yii\widgets\Pjax::begin() ?>
 <div class="show-all-top">
-<div class="checkbox">
-<div id="show-all" class="checkbox-btn">
-<?= $form->field($searchModel, 'showAll')->checkbox(['data-pjax' => true]); ?>
-</div>
-</div>
+    <div class="checkbox">
+        <div id="show-all" class="checkbox-btn">
+            <?= $form->field($searchModel, 'showAll')->checkbox(['data-pjax' => true]); ?>
+        </div>
+    </div>
 </div>
 <?php \yii\widgets\Pjax::end(); ?>
 <?php ActiveForm::end(); ?>
