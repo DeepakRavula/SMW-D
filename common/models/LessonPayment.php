0946afeb6536a6c2548683f90ebb379791dcb8ef
SenG <senguttuvang@gmail.com>
2018-07-27 11:10:02 +0530

    lesson-invoice-payment-date

--- common/models/LessonPayment.php
+++ common/models/LessonPayment.php
@@ -82,6 +82,9 @@ class LessonPayment extends \yii\db\ActiveRecord
     {
         if ($insert) {
             $this->isDeleted = false;
+            if (!$this->date) {
+                $this->date = (new \DateTime($this->date))->format('Y-m-d H:i:s');
+            }
         }
         if (round($this->amount, 2) == 0.00) {
             $this->isDeleted = true;
@@ -116,11 +119,7 @@ class LessonPayment extends \yii\db\ActiveRecord
                 }
                 $this->payment->updateAttributes(['amount' => $this->amount]);
             }
-        } else {
-            if (!$this->date) {
-                $this->date = (new \DateTime($this->date))->format('Y-m-d H:i:s');
-            }
-        }
+        } 
         foreach ($this->lesson->paymentRequests as $paymentRequest) {
             $paymentRequest->save();
         }
