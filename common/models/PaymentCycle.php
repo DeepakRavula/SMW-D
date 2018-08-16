61b83c781e66bfb4cce4e86bb84d886d6c84a077
SenG <senguttuvang@gmail.com>
2018-08-16 10:34:01 +0530

    fixes payment cycle paid status

--- common/models/PaymentCycle.php
+++ common/models/PaymentCycle.php
@@ -162,7 +162,7 @@ class PaymentCycle extends \yii\db\ActiveRecord
             ->between($fromDate, $toDate)
             ->all();
         foreach ($lessons as $lesson) {
-            if ($lesson->hasPayment()) {
+            if ($lesson->invoice || $lesson->hasPayment()) {
                 $status = true;
                 break;
             }
@@ -183,7 +183,7 @@ class PaymentCycle extends \yii\db\ActiveRecord
             ->between($fromDate, $toDate)
             ->all();
         foreach ($lessons as $lesson) {
-            if (!$lesson->hasPayment()) {
+            if (!$lesson->invoice && !$lesson->hasPayment()) {
                 $status = true;
                 break;
             }
