da61f06b76374c98c552dd24ba49266d013e721c
SenG <senguttuvang@gmail.com>
2018-08-26 23:35:04 +0530

    report-print-date-format-cahnge

--- backend/views/report/tax-collected/_print.php
+++ backend/views/report/tax-collected/_print.php
@@ -3,7 +3,7 @@
 /* @var $model common\models\Invoice */
 
 use common\models\Location;
-
+use Carbon\Carbon;
 ?>
 <?php $model = Location::findOne(['id' => Location::findOne(['slug' => \Yii::$app->location])->id]); ?>
 <?php
@@ -13,7 +13,7 @@ use common\models\Location;
    ?>
 <div>
     <h3><strong>Tax Collected Report </strong></h3></div>
-<div><h3><?= (new \DateTime($searchModel->fromDate))->format('F jS, Y') . ' to ' . (new \DateTime($searchModel->toDate))->format('F jS, Y'); ?></h3></div>
+<div><h3><?= Carbon::parse($searchModel->fromDate)->format('M d, Y') . ' to ' . Carbon::parse($searchModel->toDate)->format('M d, Y'); ?></h3></div>
 <?php echo $this->render('_taxcollected', [
     'searchModel' => $searchModel, 
     'taxDataProvider' => $taxDataProvider,
