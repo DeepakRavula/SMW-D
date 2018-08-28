7e3cfee65f9043b2b9001a86843f280c0ec263ce
SenG <senguttuvang@gmail.com>
2018-08-28 22:30:30 +0530

    enrolment-list-ui-feedback

--- backend/views/enrolment/index.php
+++ backend/views/enrolment/index.php
@@ -35,6 +35,7 @@ $this->params['show-all'] = $this->render('_button', [
         'value' => function ($data) {
             return $data->course->program->name;
         },
+        'contentOptions' => ['style' => 'width:17%'],
     ],
     [
         'attribute' => 'student',
@@ -42,6 +43,7 @@ $this->params['show-all'] = $this->render('_button', [
         'value' => function ($data) {
             return $data->student->fullName;
         },
+        'contentOptions' => ['style' => 'width:17%'],
     ],
     [
         'attribute' => 'teacher',
@@ -49,6 +51,7 @@ $this->params['show-all'] = $this->render('_button', [
         'value' => function ($data) {
             return $data->course->teacher->publicIdentity;
         },
+        'contentOptions' => ['style' => 'width:17%'],
     ],
     [
         'attribute' => 'startdate',
@@ -56,7 +59,7 @@ $this->params['show-all'] = $this->render('_button', [
         'value' => function ($data) {
             return Yii::$app->formatter->asDate($data->course->startDate);
         },
-        'contentOptions' => ['style' => 'width:200px'],
+        'contentOptions' => ['style' => 'width:17%'],
         'filterType' => KartikGridView::FILTER_DATE_RANGE,
         'filterWidgetOptions' => [
             'id' => 'enrolment-startdate-search',
@@ -123,7 +126,7 @@ $this->params['show-all'] = $this->render('_button', [
         'summary' => false,
         'emptyText' => false,
         'filterModel' => $searchModel,
-        'tableOptions' => ['class' => 'table table-bordered'],
+        'tableOptions' => ['class' => 'table table-bordered table-condensed'],
         'headerRowOptions' => ['class' => 'bg-light-gray'],
         'rowOptions' => function ($model, $key, $index, $grid) use ($searchModel) {
             $url = Url::to(['enrolment/view', 'id' => $model->id]);
