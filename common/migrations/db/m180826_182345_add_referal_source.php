8530cf5cfd3820508623e43709ae35624ecade57
SenG <senguttuvang@gmail.com>
2018-08-29 22:19:38 +0530

    yii2-audit-log-plugin

--- common/migrations/db/m180826_182345_add_referal_source.php
+++ common/migrations/db/m180826_182345_add_referal_source.php
@@ -38,7 +38,6 @@ class m180826_182345_add_referal_source extends Migration
             foreach ($sourceNames as $sourceName) {
                 $referralSource       = new ReferralSource();
                 $referralSource->name = $sourceName;
-                $referralSource->isDeleted = false;
                 $referralSource->save();
             }
 
