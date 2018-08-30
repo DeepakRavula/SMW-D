9b6dd71b939ec06542874946f0d8605bdfbae41c
SenG <senguttuvang@gmail.com>
2018-08-30 12:03:17 +0530

    changing course_schedule into course_details

---
+++ common/migrations/db/m180830_053038_changing_course_schedule_details.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180830_053038_changing_course_schedule_details
+ */
+class m180830_053038_changing_course_schedule_details extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->renameTable('course_schedule','course_details');
+        $this->addColumn('course_details', 'startDate', $this->timeStamp()->notNull());
+        $this->addColumn('course_details', 'endDate', $this->timeStamp()->notNull());
+        $this->addColumn('course_details', 'teacherId', $this->integer()->notNull());
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180830_053038_changing_course_schedule_details cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180830_053038_changing_course_schedule_details cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
