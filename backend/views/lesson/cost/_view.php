495ca0aa7af544fc7ea8c89e7fc80fedffbb62d0
SenG <senguttuvang@gmail.com>
2018-08-30 21:56:03 +0530

    teacher-collapsed-box

--- backend/views/lesson/cost/_view.php
+++ backend/views/lesson/cost/_view.php
@@ -8,7 +8,8 @@ use yii\widgets\Pjax;
 <?php Pjax::Begin(['id' => 'lesson-cost'])?>
 <div class="box box-default collapsed-box">
             <div class="box-header with-border">
-            <i class="fa fa-plus pull-left" data-widget="collapse"></i>
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+            </button>
               <h3 class="box-title">Cost</h3>
                 <div class="box-tools pull-right">
                 <i title="Edit" class="fa fa-pencil edit-cost"></i>                
--- backend/views/user/teacher/_private-qualification.php
+++ backend/views/user/teacher/_private-qualification.php
@@ -10,7 +10,8 @@ use yii\widgets\Pjax;
 <?php Pjax::Begin(['id' => 'private-grid'])?>
 <div class="box box-default collapsed-box">
             <div class="box-header with-border">
-            <i class="fa fa-plus pull-left" data-widget="collapse"></i>
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+            </button>
               <h3 class="box-title">Private Qualifications</h3>
                 <div class="box-tools pull-right">
                 <i title="Add" class="fa fa-plus add-new-qualification"></i>                
