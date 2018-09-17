9c9e2cbed32eb82420e31f8d833b20e5ae3bc39e
SenG <senguttuvang@gmail.com>
2018-09-17 21:58:22 +0530

    fix-lesson-cost

---
+++ common/migrations/one-off/m180917_151132_fix_lessons_cost.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\db\Migration;
+use common\models\Lesson;
+use common\models\Qualification;
+
+/**
+ * Class m180917_151132_fix_lessons_cost
+ */
+class m180917_151132_fix_lessons_cost extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $lessons = Lesson::find()
+            ->notDeleted()
+            ->leaf()
+            ->notCanceled()
+            ->one();
+            print_r($lessons);die;
+            foreach ($lessons as $lesson) {
+                if ($lesson->hasRootLesson()) {
+                    if ($lesson->teacherId != $lesson->rootLesson->teacherId) {
+                        $qualification = Qualification::findOne(['teacher_id' => $lesson->teacherId,
+                        'program_id' => $lesson->course->program->id]);
+            $lesson->updateAttributes(['teacherRate' => $qualification->rate ?? 0]);
+                    }
+                }
+                
+            }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180917_151132_fix_lessons_cost cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180917_151132_fix_lessons_cost cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
