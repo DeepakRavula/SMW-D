0bb47f2a2b210f2231ffe29b5e010bc436e1f27d
SenG <senguttuvang@gmail.com>
2018-09-18 13:15:51 +0530

    adding warning in preview of customer merge

--- backend/views/user/customer/_merge-preview.php
+++ backend/views/user/customer/_merge-preview.php
@@ -5,8 +5,8 @@ use yii\helpers\Url;
 use yii\bootstrap\ActiveForm;
 ?>
 
-<div id="success-notification" style="display:none;" class="alert-success alert fade in"></div>
-
+<div id="error-notification" style="display:none;" class="alert-danger alert fade in"></div>
+<div id="warning-notification" style="display:none;" class="alert-warning alert fade in"></div>
 <div class="grid-row-open">
 <?php yii\widgets\Pjax::begin([
     'timeout' => 6000,
@@ -48,6 +48,7 @@ echo GridView::widget([
     $(document).ready(function() {
         $('#popup-modal').find('.modal-header').html('<h4 class="m-0">Customer Merge Preview</h4>');
         $('#popup-modal .modal-dialog').css({'width': '800px'});
+        $('#modal-popup-warning-notification').html('Merging another customer will delete all of their contact data. This can not be undone.').fadeIn();
         $('#modal-apply').text('Confirm');
         $('#modal-apply').show();  
     });
