57d91b0e5e88c778247b99b2364cf5eef6e82b7b
SenG <senguttuvang@gmail.com>
2018-10-17 10:40:33 +0530

    lesson discount only for maple

--- common/migrations/one-off/m181011_081034_fix_extend_enrolment_discount.php
+++ common/migrations/one-off/m181011_081034_fix_extend_enrolment_discount.php
@@ -30,7 +30,7 @@ class m181011_081034_fix_extend_enrolment_discount extends Migration
                     ->joinWith(['enrolmentDiscount' => function ($query) {
                         $query->andWhere(['NOT', [ 'OR', ['enrolment_discount.id' => null], ['enrolment_discount.discount' => NULL]]]);
                     }])
-                    ->location([15])
+                    ->location([14])
                     ->notDeleted()
                     ->isConfirmed()
                    ->all();
