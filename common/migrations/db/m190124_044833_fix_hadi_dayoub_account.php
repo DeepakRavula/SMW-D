8cdc2496c9d1a8064fa8e89b22a774f028cc74a4
SenG <senguttuvang@gmail.com>
2019-01-26 12:42:42 +0530

    migration fix hadidayoubaccount

--- common/migrations/db/m190124_044833_fix_hadi_dayoub_account.php
+++ common/migrations/db/m190124_044833_fix_hadi_dayoub_account.php
@@ -44,9 +44,30 @@ class m190124_044833_fix_hadi_dayoub_account extends Migration
                 }
              }
           }
-          $lesson->delete();
+          if($lesson->invoice) {        
+            $this->execute ("DELETE  iv, ip, ili, iil, iipcl, iie, n3, pfpf, t2, ilid, ir FROM invoice iv
+            LEFT JOIN note n3 ON n3.`instanceId`= iv.`id` AND n3.`instanceType` = 4
+            LEFT JOIN proforma_payment_frequency pfpf ON pfpf.`invoiceId`= iv.`id`
+            LEFT JOIN invoice_line_item ili ON ili.`invoice_id`= iv.`id`
+            LEFT JOIN invoice_item_lesson iil ON iil.`invoiceLineItemId`= ili.`id`
+            LEFT JOIN invoice_item_enrolment iie ON iie.`invoiceLineItemId`= ili.`id`
+            LEFT JOIN invoice_item_payment_cycle_lesson iipcl ON iipcl.`invoiceLineItemId`= ili.`id`
+            LEFT JOIN invoice_line_item_discount ilid ON ilid.`invoiceLineItemId`= ili.`id`
+            LEFT JOIN invoice_payment ip ON ip.`invoice_id`= iv.`id`
+            LEFT JOIN invoice_reverse ir ON ir.`invoiceId`= iv.`id`
+            LEFT JOIN transaction t2 ON t2.`id`= iv.`transactionId`
+            where iv.id =".$lesson->invoice->id);
+              }
+         
+          $this->execute("DELETE FROM bulk_reschedule_lesson where lessonId =".$lesson->id);
+          $this->execute("DELETE FROM invoice_item_lesson where lessonId =".$lesson->id);
+          $this->execute("DELETE FROM lesson_split_usage where lessonId =".$lesson->id);
+          $this->execute("DELETE FROM  private_lesson where lessonId =".$lesson->id);
+          $this->execute("DELETE FROM lesson where lesson.id = ".$lesson->id);
           $this->execute("DELETE FROM lesson_hierarchy where lesson_hierarchy.lessonId = ".$immediateRootLesson->id." AND lesson_hierarchy.childLessonId =".$lesson->id);
-          $this->execute("DELETE FROM lesson_hierarchy where lesson_hierarchy.lessonId = ".$lesson->id." AND lesson_hierarchy.childLessonId =".$lesson->id);            
+          $this->execute("DELETE FROM lesson_hierarchy where lesson_hierarchy.lessonId = ".$lesson->id." AND lesson_hierarchy.childLessonId =".$lesson->id);     
+
+//          
           
         }     
     }
