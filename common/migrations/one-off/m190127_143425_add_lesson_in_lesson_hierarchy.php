fcd6a454ab245335c09e2d67a543111216f12521
SenG <senguttuvang@gmail.com>
2019-01-28 15:53:50 +0530

    add-lesson-in-lesson-hierarchy

--- common/migrations/one-off/m190127_143425_add_lesson_in_lesson_hierarchy.php
+++ common/migrations/one-off/m190127_143425_add_lesson_in_lesson_hierarchy.php
@@ -2,8 +2,6 @@
 
 use yii\db\Migration;
 use common\models\Lesson;
-use common\models\LessonHierarchy;
-use common\models\PrivateLesson;
 /**
  * Class m190127_143425_add_lesson_in_lesson_hierarchy
  */
@@ -18,19 +16,7 @@ class m190127_143425_add_lesson_in_lesson_hierarchy extends Migration
                 ->andWhere(['id' => [199429,199430]])
                 ->all();
         foreach ($lessons as $lesson) {
-            $lessonHierarchy = new LessonHierarchy();
-            $lessonHierarchy->lessonId = $lesson->id;
-            $lessonHierarchy->childLessonId = $lesson->id;
-            $lessonHierarchy->depth = 0;
-            $lessonHierarchy->save();
-            $privateLesson = new PrivateLesson();
-            $privateLesson->lessonId = $lesson->id;
-            if ($lesson->id == 199429) {
-                $privateLesson->expiryDate = '2019-06-7 10:00:00';
-            } else {
-                $privateLesson->expiryDate = '2019-06-14 10:00:00';
-            }
-            $privateLesson->save();
+            $lesson->makeAsRoot();
         }
     }
 
