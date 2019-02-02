f69ada50a93538ed77c9795a4b620cb64ea57a1f
SenG <senguttuvang@gmail.com>
2019-02-02 14:50:32 +0530

    feedback

--- common/models/CourseReschedule.php
+++ common/models/CourseReschedule.php
@@ -95,7 +95,7 @@ class CourseReschedule extends Model
             ->andWhere(['courseId' => $this->courseId])
             ->regular()
             ->notDeleted()
-            ->statusScheduledOrUnscheduledOrRescheduled()
+            ->notCanceled()
             ->isConfirmed()
             ->andWhere(['>=', 'DATE(lesson.date)', (new \DateTime($this->dateToChangeSchedule))->format('Y-m-d')])
             ->orderBy(['lesson.date' => SORT_ASC])
