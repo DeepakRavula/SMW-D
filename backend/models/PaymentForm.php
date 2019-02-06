9a89c65ea9d73d2aca9148009249aeeaf12240c4
SenG <senguttuvang@gmail.com>
2019-02-06 16:06:18 +0530

    rverting code receive payment

--- backend/models/PaymentForm.php
+++ backend/models/PaymentForm.php
@@ -64,6 +64,8 @@ class PaymentForm extends Model
         return [
             ['amount', 'validateAmount'],
             ['amount', 'match', 'pattern' => '^[0-9]\d*(\.\d+)?$^'],
+            [['canUsePaymentCredits', 'paymentId', 'paymentCredits', 'invoiceCredits', 'reference', 'paymentCreditIds', 'prId',
+              'receiptId', 'payment_method_id', 'notes'], 'Unsafe'],
             [['date', 'amountNeeded', 'canUseInvoiceCredits', 'selectedCreditValue', 'canUsePaymentCredits',
                 'invoiceCreditIds', 'amount', 'userId', 'amountToDistribute', 'invoicePayments', 'lessonPayments',
                 'paymentId', 'paymentCredits', 'invoiceCredits', 'reference', 'paymentCreditIds', 'prId',
