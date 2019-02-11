bc9e4ed963dfb1c196567ec10d1ce3ef98a4b1fa
SenG <senguttuvang@gmail.com>
2019-02-11 16:05:06 +0530

    add-reports-privileage

--- common/migrations/one-off/m190208_071615_add_reports_to_privilege.php
+++ common/migrations/one-off/m190208_071615_add_reports_to_privilege.php
@@ -49,13 +49,46 @@ class m190208_071615_add_reports_to_privilege extends Migration
                 'description' => 'Manage tax collected report'
             ],
         ];
-        $locations = Location::find()->all();
         foreach ($permissions as $permission) {
             $loginToBackend = $auth->getPermission($permission['permission']);
             if ($loginToBackend) {
                 $this->execute("UPDATE rbac_auth_item set isLocationSpecific = true where type = 2 and description = '" .$permission['description']."'");
             }
         }
+        $admin = $auth->getRole(User::ROLE_ADMINISTRATOR);
+        $owner = $auth->getRole(User::ROLE_OWNER);
+        $newPermissions = [
+            [
+                'permission' => 'managePaymentsReport',
+                'description' => 'Manage Payment report'
+            ],
+            [
+                'permission' => 'manageSalesAndPayment',
+                'description' => 'Manage sales and payment'
+            ],            
+        ];
+        $locations = Location::find()->all();
+        foreach ($newPermissions as $newPermission) {
+            $loginToBackend = $auth->getPermission($newPermission['permission']);
+            if (!$loginToBackend) {
+                $loginToBackend = $auth->createPermission($newPermission['permission']);
+                $loginToBackend->description = $newPermission['description'];
+                $loginToBackend->isLocationSpecific = true;
+                $auth->add($loginToBackend);
+            }
+            foreach ($locations as $location) {
+                $locationId = $location->id;
+                $adminItem = $auth->getChildrenWithLocation(User::ROLE_ADMINISTRATOR, $locationId);
+                $ownerItem = $auth->getChildrenWithLocation(User::ROLE_OWNER, $locationId);
+                if (empty($adminItem[$newPermission['permission']])) {
+                    $auth->addChildWithLocation($admin, $loginToBackend, $locationId);
+                }
+                if (empty($ownerItem[$newPermission['permission']])) {
+                    $auth->addChildWithLocation($owner, $loginToBackend, $locationId);
+                }
+            }
+        }
+
 
     }
 
