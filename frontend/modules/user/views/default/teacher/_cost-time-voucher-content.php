19dbaead7acba3d9c726874459aca87766dbdeda
SenG <senguttuvang@gmail.com>
2019-02-23 12:33:02 +0530

    invoiced-lesson-feedback

--- frontend/modules/user/views/default/teacher/_cost-time-voucher-content.php
+++ frontend/modules/user/views/default/teacher/_cost-time-voucher-content.php
@@ -151,7 +151,7 @@ td.kv-group-odd {
                 'pluginOptions' => [
                     'autoApply' => true,
                     'ranges' => [
-                        //Yii::t('kvdrp', 'Today') => ["moment().startOf('day')", "moment()"],
+                        Yii::t('kvdrp', 'Today') => ["moment().startOf('day')", "moment()"],
                         Yii::t('kvdrp', 'Tomorrow') => ["moment().startOf('day').add(1,'days')", "moment().endOf('day').add(1,'days')"],
                         Yii::t('kvdrp', 'Next {n} Days', ['n' => 7]) => ["moment().startOf('day')", "moment().endOf('day').add(6, 'days')"],
                         Yii::t('kvdrp', 'Next {n} Days', ['n' => 30]) => ["moment().startOf('day')", "moment().endOf('day').add(29, 'days')"],
