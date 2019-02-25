755a63d3eda223b6ab893d543b7bacd71f7f44ad
SenG <senguttuvang@gmail.com>
2019-02-25 15:51:21 +0530

    change-kartik-daterange

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -499,7 +499,7 @@ class UserController extends BaseController
         $lessonSearchModel->dateRange=(new\DateTime())->format('M d,Y').' - '.(new\DateTime())->format('M d,Y');
         $lessonSearchModel->load($request->get());
         $invoiceSearchModel = new InvoiceSearch();
-        $invoiceSearchModel->dateRange = (new\DateTime())->format('M d,Y') . ' - ' . (new\DateTime())->format('M d,Y');
+        $invoiceSearchModel->dateRange = (new \DateTime('previous week monday'))->format('M d,Y') . ' - ' . (new \DateTime('previous week saturday'))->format('M d,Y');
         if ($invoiceSearchModel->load($request->get())) {
             list($invoiceSearchModel->fromDate, $invoiceSearchModel->toDate) = explode(' - ', $invoiceSearchModel->dateRange);
         }
