6f4705440422a5d1616da626544e541fb3e2510b
SenG <senguttuvang@gmail.com>
2019-02-28 20:53:13 +0530

    blocking negative payment delete

--- common/models/Payment.php
+++ common/models/Payment.php
@@ -86,11 +86,18 @@ class Payment extends ActiveRecord
    
     public function validateOnDelete($attributes)
     {
+        if ($this->isNegativePayment()) {	
+            $this->addError($attributes, "Negative Payments Cannot be deleted");	
+        }
         if ($this->hasInvoicePayments()) {
             foreach ($this->invoicePayments as $invoicePayment) {
                 if (!$invoicePayment->invoice->isInvoice()) {
                     $this->addError($attributes, "Used PFI's payments can't be deleted!");
                     break;
+                } else {	         
+                    if ($invoicePayment->invoice->isPaymentCreditInvoice() && !$this->isNegativePayment()) {	
+                        $this->addError($attributes, "Refunded payments cannot be deleted! ");	
+                    }
                 }
  
             }
