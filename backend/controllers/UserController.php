185dda94e87d6142160b326d67669dfd31a77bae
SenG <senguttuvang@gmail.com>
2019-02-28 10:36:45 +0530

    teachers-invoiced-lesson-feedback

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -418,7 +418,6 @@ class UserController extends BaseController
 
     protected function getTimeVoucherDataProvider($id, $fromDate, $toDate, $summariseReport)
     {
-        
         $timeVoucher = InvoiceLineItem::find()
             ->notDeleted()
             ->joinWith(['invoice' => function ($query) use ($fromDate,$toDate) {
@@ -500,10 +499,11 @@ class UserController extends BaseController
         $lessonSearchModel->load($request->get());
         $invoiceSearchModel = new InvoiceSearch();
         $invoiceSearchModel->dateRange = (new \DateTime('previous week monday'))->format('M d,Y') . ' - ' . (new \DateTime('previous week saturday'))->format('M d,Y');
+        $invoiceSearchModel->fromDate = (new \DateTime('previous week monday'))->format('M d,Y');
+        $invoiceSearchModel->toDate = (new \DateTime('previous week saturday'))->format('M d,Y');
         if ($invoiceSearchModel->load($request->get())) {
             list($invoiceSearchModel->fromDate, $invoiceSearchModel->toDate) = explode(' - ', $invoiceSearchModel->dateRange);
         }
-
         return $this->render('view', [
             'isCustomerView' => $searchModel->accountView,
             'minTime' => $minTime,
