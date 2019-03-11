4c811ec7cfdf74f0d6a2f5c3ae41a744f30fde94
SenG <senguttuvang@gmail.com>
2019-03-11 16:18:42 +0530

    migration for adding extra lesson duedate to current date

---
+++ common/migrations/db/m190311_104000_extra_lesson_duedate_migration.php
@@ -0,0 +1,57 @@
+<?php
+
+use yii\db\Migration;
+use common\models\Lesson;
+use Carbon\Carbon;
+use yii\helpers\Console;
+/**
+ * Class m190311_104000_extra_lesson_duedate_migration
+ */
+class m190311_104000_extra_lesson_duedate_migration extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $extraLessons = Lesson::find()
+        ->isConfirmed()
+        ->notDeleted()
+        ->extra()
+        ->location([4,9,14,15,16,17,18,19,20,21])
+        ->activePrivateLessons()
+        ->notCanceled()
+        ->all();
+    foreach ($extraLessons as $extraLesson) {
+        $dueDate = Carbon::parse()->format('Y-m-d');
+        $extraLesson->updateAttributes(['dueDate' => $dueDate]);
+        Console::output("processing: " . $extraLesson->id . 'added due date', Console::FG_GREEN, Console::BOLD);
+    }
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190311_104000_extra_lesson_duedate_migration cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190311_104000_extra_lesson_duedate_migration cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
