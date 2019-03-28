362b713cb5758ca8b72bd398e03e8cabfa038662
SenG <senguttuvang@gmail.com>
2019-03-28 12:30:18 +0530

    adding due date in private lesson table

--- console/controllers/PrivateLessonController.php
+++ console/controllers/PrivateLessonController.php
@@ -112,4 +112,47 @@ class PrivateLessonController extends Controller
         Console::output("done.", Console::FG_GREEN, Console::BOLD);
         return true;
     }
+
+    public function actionSetDueDate()
+    {
+        set_time_limit(0);
+        ini_set('memory_limit', '-1');
+
+        $lessons = Lesson::find()
+            ->notDeleted()
+            ->notCanceled()
+            ->isConfirmed()
+            ->location($this->locationId)
+            ->privateLessons()
+            ->all();
+        Console::startProgress(0, 'adding due date in private lessons...');
+        foreach ($lessons as $lesson) {
+            if ($lesson->privateLesson) {
+                $lesson->privateLesson->updateAttributes([
+                    'dueDate' => $lesson->dueDate,
+                ]);
+                Console::output("processing: " . $lesson->id . 'added due date in private lesson table', Console::FG_GREEN, Console::BOLD);
+            } else {
+                print_r($lesson->id);die('coming');
+               // if ($lesson->rootLesson) {
+                //     $expiryDate = new \DateTime($lesson->rootLesson->privateLesson->expiryDate);
+                //     $date       = new \DateTime($lesson->date);
+                //     if ($date >= $expiryDate) {
+                //         $expiryDate = $date->modify('1 day');
+                //     }
+                // } else {
+                //     $date = new \DateTime($lesson->date);
+                //     $expiryDate = $date->modify('90 days');
+                // }
+                // $privateLessonModel = new PrivateLesson();
+                // $privateLessonModel->lessonId = $lesson->id;
+                // $privateLessonModel->expiryDate = $expiryDate->format('Y-m-d H:i:s');
+                // $privateLessonModel->save();
+               // Console::output("processing: " . $lesson->id . 'added new private lesson', Console::FG_GREEN, Console::BOLD);
+            }
+        }
+        Console::endProgress(true);
+        Console::output("done.", Console::FG_GREEN, Console::BOLD);
+        return true;
+    }
 }
