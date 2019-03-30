c32ff5a4f46795ebb94f747dda8ce6b5aa40bb89
SenG <senguttuvang@gmail.com>
2019-03-30 10:58:28 +0530

    adding schema design for customer recurring payment enrolment table

---
+++ common/models/CustomerRecurringPaymentEnrolment.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\behaviors\BlameableBehavior;
+use asinfotrack\yii2\audittrail\behaviors\AuditTrailBehavior;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * This is the model class for table "customer_recurring_payment_enrolment".
+ *
+ * @property int $id
+ * @property int $enrolmentId
+ * @property int $customerRecurringPaymentId
+ * @property string $createdOn
+ * @property string $updatedOn
+ * @property int $createdByUserId
+ * @property int $updatedByUserId
+ */
+class CustomerRecurringPaymentEnrolment extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'customer_recurring_payment_enrolment';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'createdAtAttribute' => 'createdOn',
+                'updatedAtAttribute' => 'updatedOn',
+                'value' => (new \DateTime())->format('Y-m-d H:i:s'),
+            ],
+            [
+                'class' => BlameableBehavior::className(),
+                'createdByAttribute' => 'createdByUserId',
+                'updatedByAttribute' => 'updatedByUserId'
+            ],
+            'audittrail' => [
+                'class' => AuditTrailBehavior::className(), 
+                'consoleUserId' => self::CONSOLE_USER_ID, 
+                'attributeOutput' => [
+                    'last_checked' => 'datetime',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['enrolmentId', 'customerRecurringPaymentId', 'createdByUserId', 'updatedByUserId'], 'required'],
+            [['enrolmentId', 'customerRecurringPaymentId', 'createdByUserId', 'updatedByUserId'], 'integer'],
+            [['createdOn', 'updatedOn'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'enrolmentId' => 'Enrolment ID',
+            'customerRecurringPaymentId' => 'Customer Recurring Payment ID',
+            'createdOn' => 'Created On',
+            'updatedOn' => 'Updated On',
+            'createdByUserId' => 'Created By User ID',
+            'updatedByUserId' => 'Updated By User ID',
+        ];
+    }
+}
