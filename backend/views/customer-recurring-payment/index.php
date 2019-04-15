3d668f4a2c7ebf245c62bf311d0a6b508f72e266
SenG <senguttuvang@gmail.com>
2019-04-15 13:11:21 +0530

    add-list-of-recurring-payments

--- backend/views/customer-recurring-payment/index.php
+++ backend/views/customer-recurring-payment/index.php
@@ -10,7 +10,7 @@ use yii\widgets\Pjax;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Recurring Payment';
+$this->title = 'Recurring Payments';
 $this->params['breadcrumbs'][] = $this->title;
 ?> 
 <div class="recurring-payment-index">  
