b14e4938b081a7a5bfb9eea6d30ca11436840d9f
SenG <senguttuvang@gmail.com>
2019-06-13 17:47:11 +0530

    fix date name

--- common/models/User.php
+++ common/models/User.php
@@ -1132,7 +1132,7 @@ class User extends ActiveRecord implements IdentityInterface
                 ->andWhere(['>', 'invoice.balance', 0.09])
                 ->notDeleted();
                 if ($days > 90) {       
-                     $invoicesBalanceTotal->andWhere(['<', 'invoice.', Carbon::now()->modify('- 90 days')->format('Y-m-d')]);
+                     $invoicesBalanceTotal->andWhere(['<', 'invoice.date', Carbon::now()->modify('- 90 days')->format('Y-m-d')]);
                 } else {
                     $invoicesBalanceTotal->andWhere(['between', 'invoice.date', $fromDate->format('Y-m-d'), $toDate->format('Y-m-d')]);
                 }
