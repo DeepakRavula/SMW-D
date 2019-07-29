76859c744df15959b014395e2f8726887ced46a5
SenG <senguttuvang@gmail.com>
2019-07-29 13:38:53 +0530

    fix-explode-lesson-damage

--- common/migrations/one-off/m190726_060034_fix_explode_lesson_with_program_rate.php
+++ common/migrations/one-off/m190726_060034_fix_explode_lesson_with_program_rate.php
@@ -2,7 +2,7 @@
 
 use yii\db\Migration;
 use common\models\Lesson;
-
+use common\models\User;
 /**
  * Class m190726_060034_fix_explode_lesson_with_program_rate
  */
@@ -11,6 +11,14 @@ class m190726_060034_fix_explode_lesson_with_program_rate extends Migration
     /**
      * {@inheritdoc}
      */
+    public function init()
+    {
+        parent::init();
+        $user = User::findByRole(User::ROLE_BOT);
+        $botUser = end($user);
+        Yii::$app->user->setIdentity(User::findOne(['id' => $botUser->id]));
+    }
+
     public function safeUp()
     {
         $locationIds = [1,4,5,6,9,13,14,15,16,17,18,19,20,21];
@@ -22,9 +30,10 @@ class m190726_060034_fix_explode_lesson_with_program_rate extends Migration
         ->all();
         $lessonCount = 0;
         foreach ($explodedLessons as $explodedLesson){
-            if ($explodedLesson->programRate != $explodedLesson->rootLesson->programRate && $explodedLesson->programRate > $explodedLesson->rootLesson->programRate ) {
-                $explodedLesson->updateAttributes(['programRate' => $explodedLesson->rootLesson->programRate]);
+            if ($explodedLesson->programRate != $explodedLesson->rootLesson->programRate && ($explodedLesson->programRate > $explodedLesson->rootLesson->programRate || ($explodedLesson->rootLesson->programRate - $explodedLesson->programRate ) > 0.09 )) {
                 print_r("\n".$explodedLesson->id."\t".$explodedLesson->course->location->id."\t changed from ".$explodedLesson->rootLesson->programRate." to ".$explodedLesson->programRate);
+                $explodedLesson->programRate = $explodedLesson->rootLesson->programRate;
+                $explodedLesson->save();
                 $lessonCount++;
             }
         }
