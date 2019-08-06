6030920d9eebe1fba1116ed66ba9a61d796ab843
SenG <senguttuvang@gmail.com>
2019-08-06 16:54:51 +0530

    adding migration to change duedate to fifteen

--- console/controllers/EnrolmentController.php
+++ console/controllers/EnrolmentController.php
@@ -91,44 +91,4 @@ class EnrolmentController extends Controller
         return $model->deleteWithTransactionalData();
     }
 
-    public function actionSetLessonDueDate()
-    {
-        set_time_limit(0);
-        ini_set('memory_limit', '-1');
-        Console::startProgress(0, 'Processing Enrolments');
-        $enrolments = Enrolment::find()
-                       ->location($this->id) 
-                       ->isConfirmed()
-                       ->notDeleted()
-                       ->isRegular()
-                       ->privateProgram()
-                       ->all();
-        foreach ($enrolments as $enrolment){
-            Console::output("processing: " . $enrolment->id . 'processing', Console::FG_GREEN, Console::BOLD);
-           $this->setDueDate($enrolment->id);
-        }  
-        Console::endProgress(true);
-        Console::output("done.", Console::FG_GREEN, Console::BOLD);             
-        return true;
-    }
-
-    public function setDueDate($enrolmentId)
-    {
-        $lessons = Lesson::find()
-            ->enrolment($enrolmentId)
-            ->isConfirmed()
-            ->notCanceled()
-            ->notDeleted()
-            ->all();
-        foreach ($lessons as $lesson){
-            if ($lesson->paymentCycle) {
-                $dueDateMonth = Carbon::parse($lesson->dueDate)->format('m');
-                $dueDateYear = Carbon::parse($lesson->dueDate)->format('Y');
-                $formatedDate = 15 . '-' . $dueDateMonth . '-' . $dueDateYear;
-                $dueDate = Carbon::parse($formatedDate)->format('Y-m-d');
-                $lesson->updateAttributes(['dueDate' => $dueDate]); 
-            }
-        }
-        return true;
-    }
 }
