4c5b0a8228f71695fa17bdf8df7bc2a10542853c
SenG <senguttuvang@gmail.com>
2019-08-19 13:12:39 +0530

    fix issue in schedule events

--- common/models/query/TeacherUnavailabilityQuery.php
+++ common/models/query/TeacherUnavailabilityQuery.php
@@ -16,17 +16,17 @@ class TeacherUnavailabilityQuery extends ActiveQuery
         return $this->andWhere(['OR', 
             ['AND',
                 [
-                    '<', 'DATE(fromDate)', $date->format('Y-m-d')
+                    '<', 'DATE(fromDateTime)', $date->format('Y-m-d H:i:s')
                 ],
                 [
-                    '>', 'DATE(toDate)', $date->format('Y-m-d')
+                    '>', 'DATE(toDateTime)', $date->format('Y-m-d H:i:s')
                 ]
             ], 
             [
-                'DATE(fromDate)' => $date->format('Y-m-d')
+                'DATE(fromDateTime)' => $date->format('Y-m-d H:i:s')
             ],
             [
-                'DATE(toDate)' => $date->format('Y-m-d')
+                'DATE(toDateTime)' => $date->format('Y-m-d H:i:s')
             ]
         ]);
     }
