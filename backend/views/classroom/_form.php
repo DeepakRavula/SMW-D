ddbdcb5954d4c8c1e82c4eeff92c1b76873af4f3
SenG <senguttuvang@gmail.com>
2019-09-14 16:59:24 +0530

    adding-classroom-form

--- backend/views/classroom/_form.php
+++ backend/views/classroom/_form.php
@@ -22,7 +22,7 @@ use yii\helpers\Url;
     ]); ?>
 
 	<div class="row">
-    	<?php echo $form->field($model, 'name')->label() ?>
+    	<?php echo $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
     	<?php echo $form->field($model, 'description')->textArea(['maxlength' => true]) ?>
 	</div>
     <?php ActiveForm::end(); ?>
