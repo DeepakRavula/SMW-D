088f97ec384dfb02ddf4bce948d848381f972173
SenG <senguttuvang@gmail.com>
2019-10-19 13:28:33 +0530

    add-new-private-lesson-listing

--- backend/views/lesson/_show-all-button.php
+++ backend/views/lesson/_show-all-button.php
@@ -16,20 +16,11 @@ $form = ActiveForm::begin([
     ]);
 ?>
 <?php yii\widgets\Pjax::begin() ?>
-<div id="bulk-action-menu" class="m-b-10 pull-right">
-<div class="btn-group">
-<button class="btn btn-default dropdown-toggle" data-toggle="dropdown"><i class="fa fa-filter fa-1x"></i>&nbsp;&nbsp;<span class="caret"></span></button>
-<ul class="dropdown-menu dropdown-menu-right">
-<li>
-    <div class="show-all-top m-r-20">
-    <div class="checkbox">
-    <div id="show-all" class="checkbox-btn">
-        <?= $form->field($searchModel, 'showAll')->checkbox(['data-pjax' => true]); ?>
-    </div>
-    </div>
-    </div>
-<li>
-</ul>
+<div class="show-all-top m-r-20">
+<div class="checkbox">
+<div id="show-all" class="checkbox-btn">
+<?= $form->field($searchModel, 'showAll')->checkbox(['data-pjax' => true]); ?>
+</div>
 </div>
 </div>
 <?php \yii\widgets\Pjax::end(); ?>
