1de188011077fb1cc8f4f270c23386de5fcaf10c
SenG <senguttuvang@gmail.com>
2020-02-07 17:35:35 +0530

    adding save

--- backend/controllers/EnrolmentController.php
+++ backend/controllers/EnrolmentController.php
@@ -805,9 +805,7 @@ class EnrolmentController extends BaseController
                         $courseSchedule->fromTime = $model->course->recentCourseSchedule->fromTime;
                         $courseSchedule->duration = $model->course->recentCourseSchedule->duration;
                         $courseSchedule->paymentFrequency = $model->paymentFrequencyId;
-                        if (!$courseSchedule->save()) {
-                            print_r($courseSchedule->getErrors());
-                        }
+                        $courseSchedule->save();
                     }
                     $model->setStatus();
                     if ($message) {
