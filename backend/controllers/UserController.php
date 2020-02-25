4160a8b8a933973b226eccc7567fef580619f530
SenG <senguttuvang@gmail.com>
2020-02-25 12:53:11 +0530

    fixing-group-enrolment-lists

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -236,6 +236,7 @@ class UserController extends BaseController
             ->notDeleted()
             ->isConfirmed()
             ->isRegular()
+            ->andWhere(['>=', 'DATE(enrolment.endDateTime)', (new \DateTime())->format('Y-m-d')])
             ->groupBy(['enrolment.id'])
             ->activeAndfutureEnrolments();
 
