5507253ff02faf93cbc4a0ddd29765a1618def74
VeeraSanthanaLakshmi <veera@pearlthoughts.com>
2021-04-05 22:14:03 +0530

    adding date format in m, dy format

--- common/models/CustomerRecurringPayment.php
+++ common/models/CustomerRecurringPayment.php
@@ -79,7 +79,7 @@ class CustomerRecurringPayment extends \yii\db\ActiveRecord
             [['customerId', 'entryDay', 'paymentDay', 'paymentMethodId', 'paymentFrequencyId',  'amount'], 'required'],
             [['customerId', 'paymentMethodId', 'paymentFrequencyId', 'createdByUserId', 'updatedByUserId'], 'integer'],
             [['entryDay', 'paymentDay', 'expiryMonth', 'expiryYear', 'expiryDate', 'createdOn', 'updatedOn', 'amount', 'createdByUserId', 'updatedByUserId', 'isRecurringPaymentEnabled', 'startDate', 'isDeleted', 'nextEntryDay'], 'safe'],
-            [['startDate'], 'date'],
+            [['startDate'], 'date', 'format' => 'M d,Y', 'message' => 'Date format shoule be in M d,Y format',],
         ];
     }
 
