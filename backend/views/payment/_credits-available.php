92a685ae22bf1f327593b22fb9ecac5cdbc71a25
VeeraSanthanaLakshmi <veera@pearlthoughts.com>
2021-08-22 21:11:05 +0530

    add-payment-reference-invoice-popup

--- backend/views/payment/_credits-available.php
+++ backend/views/payment/_credits-available.php
@@ -6,6 +6,7 @@ use yii\bootstrap\Html;
 use common\models\Invoice;
 use common\models\Payment;
 use yii\bootstrap\ActiveForm;
+$reference = $model->notes ? $model->notes : $model->reference ? $model->reference : "";
 ?>
 <div id="lesson-grid" class="col-md-12">
 <table class="table table-striped table-bordered">
@@ -19,7 +20,7 @@ use yii\bootstrap\ActiveForm;
     </thead>
     <tbody>
         <tr class="line-items-value lesson-line-items" data-key="1">
-            <td class="text-left"> <?= $model->notes ? $model->notes : $model->reference ? $model->reference : "" ?> </td>
+            <td class="text-left"> <a href=""> <?= $reference ?> </a> </td>
             <td class="text-left"> <?= (new DateTime($model->date))->format('M d, Y'); ?> </td>
             <td class="text-left"> <?= $model->paymentMethod->name ?> </td>
             <td class="text-right"><?= Yii::$app->formatter->asCurrency(round($model->amount, 2)); ?></td>
